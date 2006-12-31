240d3c43f7f76a30dc5b05ca4269c644d0bc1010
wolfgang-ch <wolfgang-ch@5a91af82-4d22-0410-8bcb-a41d7c89129f>
2006-12-31 21:09:32 +0000

    git-svn-id: https://mytourbook.svn.sourceforge.net/svnroot/mytourbook/trunk@4 5a91af82-4d22-0410-8bcb-a41d7c89129f

---
+++ UI.java
@@ -0,0 +1,153 @@
+/*******************************************************************************
+ * Copyright (C) 2006  Wolfgang Schramm
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation; either version 2 of the License, or (at your option) any later 
+ * version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA    
+ *******************************************************************************/
+package net.tourbook.ui;
+
+import net.tourbook.util.PixelConverter;
+
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.viewers.ColumnPixelData;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.events.VerifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.IMemento;
+
+public class UI {
+
+	public static ColumnPixelData getColumnPixelWidth(PixelConverter pixelConverter, int width) {
+		return new ColumnPixelData(pixelConverter.convertWidthInCharsToPixels(width), false);
+	}
+
+	/**
+	 * Restore the sash weight from a memento
+	 * 
+	 * @param sash
+	 * @param fMemento
+	 * @param weightKey
+	 * @param sashDefaultWeight
+	 */
+	public static void restoreSashWeight(	SashForm sash,
+											IMemento fMemento,
+											String weightKey,
+											int[] sashDefaultWeight) {
+
+		int[] sashWeights = sash.getWeights();
+		int[] newWeights = new int[sashWeights.length];
+
+		for (int weightIndex = 0; weightIndex < sashWeights.length; weightIndex++) {
+
+			Integer mementoWeight = fMemento.getInteger(weightKey + Integer.toString(weightIndex));
+
+			if (mementoWeight == null) {
+				try {
+					newWeights[weightIndex] = sashDefaultWeight[weightIndex];
+
+				} catch (ArrayIndexOutOfBoundsException e) {
+					newWeights[weightIndex] = 100;
+				}
+			} else {
+				newWeights[weightIndex] = mementoWeight;
+			}
+		}
+
+		sash.setWeights(newWeights);
+	}
+
+	/**
+	 * Store the weights for the sash in a memento
+	 * 
+	 * @param sash
+	 * @param memento
+	 * @param weightKey
+	 */
+	public static void saveSashWeight(SashForm sash, IMemento memento, String weightKey) {
+
+		int[] weights = sash.getWeights();
+
+		for (int weightIndex = 0; weightIndex < weights.length; weightIndex++) {
+			memento.putInteger(weightKey + Integer.toString(weightIndex), weights[weightIndex]);
+		}
+	}
+
+	/**
+	 * Set grid layout with no margins for a composite
+	 * 
+	 * @param composite
+	 */
+	public static void set0GridLayout(Composite composite) {
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.marginHeight = 0;
+		gridLayout.marginWidth = 0;
+		gridLayout.verticalSpacing = 0;
+		composite.setLayout(gridLayout);
+	}
+
+	public static GridData setFieldWidth(Composite parent, StringFieldEditor field, int width) {
+		GridData gd = new GridData();
+		gd.widthHint = width;
+		field.getTextControl(parent).setLayoutData(gd);
+		return gd;
+	}
+
+	public static void setHorizontalSpacer(Composite parent, int columns) {
+		Label label = new Label(parent, SWT.NONE);
+		GridData gd = new GridData();
+		gd.horizontalSpan = columns;
+		label.setLayoutData(gd);
+	}
+
+	public static GridData setWidth(Control control, int width) {
+		GridData gd = new GridData();
+		gd.widthHint = width;
+		control.setLayoutData(gd);
+		return gd;
+	}
+
+	public static VerifyListener verifyListenerTypeLong() {
+
+		return new VerifyListener() {
+			public void verifyText(VerifyEvent e) {
+				if (e.text.equals("")) {
+					return;
+				}
+				try {
+					Long.parseLong(e.text);
+				} catch (NumberFormatException e1) {
+					e.doit = false;
+				}
+			}
+		};
+	}
+
+	public static void setDefaultColor(Control control) {
+		control.setForeground(null);
+		control.setBackground(null);
+	}
+
+	public static void setErrorColor(Text control) {
+		control.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_RED));
+		control.setForeground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+	}
+
+}
