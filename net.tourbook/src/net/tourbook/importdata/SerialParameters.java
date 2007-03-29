e0d02081ae05f813934ca2e4154c5bc9373f2b5e
wolfgang-ch <wolfgang-ch@5a91af82-4d22-0410-8bcb-a41d7c89129f>
2007-03-29 06:33:40 +0000

    fixed copyrights
    git-svn-id: https://mytourbook.svn.sourceforge.net/svnroot/mytourbook/trunk@157 5a91af82-4d22-0410-8bcb-a41d7c89129f

--- net.tourbook/src/net/tourbook/importdata/SerialParameters.java
+++ net.tourbook/src/net/tourbook/importdata/SerialParameters.java
@@ -1,33 +1,18 @@
-/* @(#)SerialParameters.java	1.5 98/07/17 SMI
- *
- * Copyright (c) 1998 Sun Microsystems, Inc. All Rights Reserved.
- *
- * Sun grants you ("Licensee") a non-exclusive, royalty free, license
- * to use, modify and redistribute this software in source and binary
- * code form, provided that i) this copyright notice and license appear
- * on all copies of the software; and ii) Licensee does not utilize the
- * software in a manner which is disparaging to Sun.
- *
- * This software is provided "AS IS," without a warranty of any kind.
- * ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
- * INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN AND
- * ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY
- * LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THE
- * SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN OR ITS LICENSORS
- * BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT,
- * INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES,
- * HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING
- * OUT OF THE USE OF OR INABILITY TO USE SOFTWARE, EVEN IF SUN HAS BEEN
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
- *
- * This software is not designed or intended for use in on-line control
- * of aircraft, air traffic, aircraft navigation or aircraft
- * communications; or in the design, construction, operation or
- * maintenance of any nuclear facility. Licensee represents and
- * warrants that it will not use or redistribute the Software for such
- * purposes.
- */
+/*******************************************************************************
+ * Copyright (C) 2005, 2007  Wolfgang Schramm
+ *  
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation version 2 of the License.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA    
+ *******************************************************************************/
 
 package net.tourbook.importdata;
 
--- net.tourbook/src/net/tourbook/views/tourBook/TVITourBookMonth.java
+++ net.tourbook/src/net/tourbook/views/tourBook/TVITourBookMonth.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2006, 2007  Wolfgang Schramm
+ * Copyright (C) 2005, 2007  Wolfgang Schramm
  *  
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software 
