17de40ac1821d56a10db6af99af63aae4fb61bea
wolfgang-ch <wolfgang-ch@5a91af82-4d22-0410-8bcb-a41d7c89129f>
2007-04-01 09:33:06 +0000

    old ejb3 version
    git-svn-id: https://mytourbook.svn.sourceforge.net/svnroot/mytourbook/trunk@170 5a91af82-4d22-0410-8bcb-a41d7c89129f

--- net.tourbook.ext.ejb3/src/org/hibernate/ejb/HibernateQuery.java
+++
@@ -1,8 +0,0 @@
-//$Id: HibernateQuery.java 9796 2006-04-26 06:46:52Z epbernard $
-package org.hibernate.ejb;
-
-import javax.persistence.Query;
-
-public interface HibernateQuery extends Query {
-	public org.hibernate.Query getHibernateQuery();
-}
--- net.tourbook.ext.ejb3/src/org/hibernate/ejb/event/CallbackHandlerConsumer.java
+++
@@ -1,9 +0,0 @@
-//$Id: CallbackHandlerConsumer.java 8717 2005-11-30 14:37:38Z epbernard $
-package org.hibernate.ejb.event;
-
-/**
- * @author Emmanuel Bernard
- */
-public interface CallbackHandlerConsumer {
-	void setCallbackHandler(EntityCallbackHandler callbackHandler);
-}
