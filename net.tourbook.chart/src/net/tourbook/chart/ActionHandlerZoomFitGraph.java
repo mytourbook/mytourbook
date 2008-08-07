7aae30f9324e87d2fc571a284f8d82a09a43625c
wolfgang-ch <wolfgang-ch@5a91af82-4d22-0410-8bcb-a41d7c89129f>
2008-08-07 12:17:59 +0000

    eleminate gradient peaks
    git-svn-id: https://mytourbook.svn.sourceforge.net/svnroot/mytourbook/trunk@619 5a91af82-4d22-0410-8bcb-a41d7c89129f

--- net.tourbook.chart/src/net/tourbook/chart/ActionHandlerZoomFitGraph.java
+++ net.tourbook.chart/src/net/tourbook/chart/ActionHandlerZoomFitGraph.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2007  Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2008  Wolfgang Schramm and Contributors
  *  
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software 
@@ -18,9 +18,9 @@ package net.tourbook.chart;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-public class ActionHandlerFitGraph extends ActionHandler {
+public class ActionHandlerZoomFitGraph extends ActionHandler {
 
-	public ActionHandlerFitGraph() {
+	public ActionHandlerZoomFitGraph() {
 		fCommandId = Chart.COMMAND_ID_FIT_GRAPH;
 	}
 
