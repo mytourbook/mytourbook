3c1582eeed5b402e9176c2ff4eb57ae17062f621
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2011-01-20 10:41:14 +0100

    testing mailing list

--- net.tourbook/src/net/tourbook/colors/GraphColorProvider.java
+++ net.tourbook/src/net/tourbook/colors/GraphColorProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2010  Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2011  Wolfgang Schramm and Contributors
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -42,7 +42,6 @@ import org.eclipse.swt.graphics.RGB;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.WorkbenchException;
 import org.eclipse.ui.XMLMemento;
-import org.osgi.framework.Bundle;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
