5bd01f6d6a451db7add048936d9b27ad92aa1aa6
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2011-08-06 16:38:34 +0200

    adjusted version number

--- net.tourbook.device.daum.ergobike/src/net/tourbook/device/daum/ergobike/Activator.java
+++ net.tourbook.device.daum.ergobike/src/net/tourbook/device/daum/ergobike/Activator.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2008  Wolfgang Schramm and Contributors
- *  
+ * Copyright (C) 2005, 2011  Wolfgang Schramm and Contributors
+ * 
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software 
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with 
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA    
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  *******************************************************************************/
 package net.tourbook.device.daum.ergobike;
 
@@ -24,25 +24,25 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String	PLUGIN_ID	= "net.tourbook.device.daum_ergobike";	//$NON-NLS-1$
+	public static final String	PLUGIN_ID	= "net.tourbook.device.daum.ergobike";	//$NON-NLS-1$
 
 	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+	private static Activator	plugin;
 
 	/**
 	 * The constructor
 	 */
 	public Activator() {
 		plugin = this;
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
 	}
 
 	/*
