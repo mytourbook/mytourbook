3e5bcec5665f88923a4e18b0dd24836a54939a51
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2011-08-06 14:16:58 +0200

    11.8.1 osx installer

---
+++ INSTALL-UPDATES/installer-net.tourbook/mytourbook-osx-package.maker/repackage-mt-11.8.1.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+#  repackage mytourbook
+
+./EclipseOSXRepackager --force /Users/wschramm/mytourbook-PRODUCT/11.8.1/mytourbook_11.8.1.macosx.cocoa.x86_64/mytourbook /Users/wschramm/mytourbook-PRODUCT/11.8.1/mytourbook_11.8.1.macosx.cocoa.x86_64/MyTourbook.app
