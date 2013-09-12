8a9580b0b57f74bff1d75b407dd6402b7c965347
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2013-09-12 19:19:29 +0200

    fixed problem for 3D french default layers, because the layer id (name) is translated for this language

--- net.tourbook/src/net/tourbook/map3/view/Map3Manager.java
+++ net.tourbook/src/net/tourbook/map3/view/Map3Manager.java
@@ -123,7 +123,7 @@ public class Map3Manager {
 	 */
 	private static Map3LayerView					_map3LayerView;
 
-	private static LayerList						_wwDefaultLayers;
+	private static LayerList						_wwDefaultLocaleLayers;
 
 	/**
 	 * Contains custom (none default) layers, key is layerId.
@@ -208,7 +208,13 @@ public class Map3Manager {
 		wwModel.setShowTessellationBoundingVolumes(false);
 
 		// get default layer
-		_wwDefaultLayers = wwModel.getLayers();
+		_wwDefaultLocaleLayers = wwModel.getLayers();
+
+//		System.out.println(UI.timeStampNano() + " Map3 layer");
+//		for (final Layer layer : _wwDefaultLocaleLayers) {
+//			System.out.println(UI.timeStampNano() + "\t" + layer);
+//		}
+//		// TODO remove SYSTEM.OUT.PRINTLN
 
 		// create custom layer BEFORE state is applied and xml file is read which references these layers
 		createCustomLayer_TourTracks();
@@ -966,9 +972,11 @@ public class Map3Manager {
 
 						if (isDefaultLayer) {
 
-							// layer is a default layer
+							// this is a default layer
 
-							final Layer wwLayer = _wwDefaultLayers.getLayerByName(xmlChildId);
+							final String localeLayerId = MapDefaultLayer.getLocaleLayerId(xmlChildId);
+
+							final Layer wwLayer = _wwDefaultLocaleLayers.getLayerByName(localeLayerId);
 
 							// check if xml layer is a default ww layer
 							if (wwLayer == null) {
@@ -993,7 +1001,7 @@ public class Map3Manager {
 
 						} else {
 
-							// NO default layer
+							// this is NO default layer
 
 							tviLayer = _customLayers.get(xmlChildId);
 
