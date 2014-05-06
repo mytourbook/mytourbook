0a9761df044117c9e9c66274318330c14e7d2dc6
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2014-05-06 18:26:06 +0200

    updated to FIT SDK 10.20

--- net.tourbook.device.garmin.fit/src/net/tourbook/device/garmin/fit/listeners/DeviceInfoMesgListenerImpl.java
+++ net.tourbook.device.garmin.fit/src/net/tourbook/device/garmin/fit/listeners/DeviceInfoMesgListenerImpl.java
@@ -2,36 +2,41 @@ package net.tourbook.device.garmin.fit.listeners;
 
 import net.tourbook.device.garmin.fit.FitActivityContext;
 
+import com.garmin.fit.AntplusDeviceType;
 import com.garmin.fit.DeviceInfoMesg;
 import com.garmin.fit.DeviceInfoMesgListener;
-import com.garmin.fit.DeviceType;
 
 public class DeviceInfoMesgListenerImpl extends AbstractMesgListener implements DeviceInfoMesgListener {
 
-	public DeviceInfoMesgListenerImpl(FitActivityContext context) {
+	public DeviceInfoMesgListenerImpl(final FitActivityContext context) {
 		super(context);
 	}
 
+	private boolean hasHeartRateSensor(final Short deviceType) {
+		return deviceType.equals(AntplusDeviceType.HEART_RATE);
+	}
+
+	private boolean hasPowerSensor(final Short deviceType) {
+		return deviceType.equals(AntplusDeviceType.BIKE_POWER);
+	}
+
+	private boolean hasSpeedSensor(final Short deviceType) {
+
+		return deviceType.equals(AntplusDeviceType.BIKE_SPEED)
+				|| deviceType.equals(AntplusDeviceType.BIKE_SPEED_CADENCE);
+	}
+
 	@Override
-	public void onMesg(DeviceInfoMesg mesg) {
-		Short deviceType = mesg.getDeviceType();
+	public void onMesg(final DeviceInfoMesg mesg) {
+
+		final Short deviceType = mesg.getDeviceType();
+
 		if (deviceType != null) {
+
 			context.setSpeedSensorPresent(hasSpeedSensor(deviceType));
 			context.setHeartRateSensorPresent(hasHeartRateSensor(deviceType));
 			context.setPowerSensorPresent(hasPowerSensor(deviceType));
 		}
-	}
-
-	private boolean hasSpeedSensor(Short deviceType) {
-		return deviceType.equals(DeviceType.BIKE_SPEED) || deviceType.equals(DeviceType.BIKE_SPEED_CADENCE);
-	}
-
-	private boolean hasHeartRateSensor(Short deviceType) {
-		return deviceType.equals(DeviceType.HEART_RATE);
-	}
-
-	private boolean hasPowerSensor(Short deviceType) {
-		return deviceType.equals(DeviceType.BIKE_POWER);
 	}
 
 }
