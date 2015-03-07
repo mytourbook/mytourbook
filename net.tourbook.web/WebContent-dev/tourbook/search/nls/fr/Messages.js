3534af2aef7f3c7510183ea2011567af3c842f2d
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2015-03-07 12:05:12 +0100

    french translation

--- net.tourbook.web/WebContent-dev/tourbook/search/nls/fr/Messages.js
+++ net.tourbook.web/WebContent-dev/tourbook/search/nls/fr/Messages.js
@@ -1,4 +1,30 @@
 // created with net.tourbook.ant.I18ToDojo
 define(//							
 {									
+		Search_App_Action_EditMarker                       : 'Editer Marqueur',
+		Search_App_Action_EditTour                         : 'Editer Itinéraire',
+		Search_App_Action_SearchOptions_Tooltip            : 'Options de Recherche.',
+		Search_App_Action_StartSearch_Tooltip              : 'Démarrer la recherche.',
+		Search_App_Label_Status_Tooltip                    : 'Nombre de résultats - Temps de recherche',
+		Search_App_Text_Search_PlaceHolder                 : 'Recherche d\'Itinéraires, Marquers et Waypoints',
+		Search_App_Tooltip                                 : '<b>Jokers de recherche</b><br>' + 'Une recherche peut être effectuée avec les jokers * or ? et d\'autres possibilités, voir les détails dans la <a href=\'{0}\'>Description du langage de requête Lucene.</a><br><br>' + '' + 'Lucene est le moteur de recherche interne utilisé.',
+		Search_Grid_Label_NoDataMessage                    : 'Aucun résultat trouvé.',
+		Search_Options_Action_RestoreDefaults              : 'Restorer les options par défaut',
+		Search_Options_Checkbox_EaseSearching              : 'Utiliser la recherche facile',
+		Search_Options_Checkbox_EaseSearching_Tooltip      : '<b>* Jokers de recherche</b><br>' + 'Quand <i>Utiliser la recherche facile</i> est sélectionné, un koker * est ajouté au texte de recherche, le caractère * n\'est pas visible .<br><br>' + '' + '<b>Recherche étendue</b><br>' + 'I peut être utile de désactiver <i>Utiliser la recherche facile</i> quand on effectue une recherche avec d\'autres options du parser Lucene.',
+		Search_Options_Checkbox_ShowContentAll             : 'Tout',
+		Search_Options_Checkbox_ShowContentMarker          : 'Marqueurs',
+		Search_Options_Checkbox_ShowContentTour            : 'Itinéraires',
+		Search_Options_Checkbox_ShowContentWaypoint        : 'Waypoints',
+		Search_Options_Checkbox_ShowDateTime               : 'Montrer date/heure',
+		Search_Options_Checkbox_ShowItemNumber             : 'Montrer le numéro de l\'article',
+		Search_Options_Checkbox_ShowLuceneDocId            : 'Montrer le doc ID Lucene',
+		Search_Options_Dialog_Header                       : 'Options de Recherche',
+		Search_Options_Group_Content                       : 'Que faut-il rechercher ?',
+		Search_Options_Group_Result                        : 'Résultats',
+		Search_Options_Group_Sorting                       : 'Tri',
+		Search_Options_Label_SortAscending                 : 'Par Date',
+		Search_Options_Radio_SortAscending                 : 'Ascendant',
+		Search_Options_Radio_SortDescending                : 'Descendant',
+		Search_Validation_SearchFilter                     : 'Aucun résultat de trouvé avec ces options de recherche.'
 });		
