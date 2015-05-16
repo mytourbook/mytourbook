707f805f7cdf8f80f41a7e6e38fb6c7889f058d2
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2015-05-16 09:17:27 +0200

    15.3.2 spanish translation including 15.6 image file texts

--- net.tourbook.web/WebContent-dev/tourbook/search/nls/es/Messages.js
+++ net.tourbook.web/WebContent-dev/tourbook/search/nls/es/Messages.js
@@ -1,4 +1,30 @@
 // created with net.tourbook.ant.I18ToDojo
 define(//							
 {									
+		Search_App_Action_EditMarker                       : 'Editar Marcador',
+		Search_App_Action_EditTour                         : 'Editar Recorrido',
+		Search_App_Action_SearchOptions_Tooltip            : 'Buscar Opciones.',
+		Search_App_Action_StartSearch_Tooltip              : 'Comenzar búsqueda.',
+		Search_App_Label_Status_Tooltip                    : 'Número de resultados - Tiempo de búsqueda',
+		Search_App_Text_Search_PlaceHolder                 : 'Buscar Recorridos, Marcadores y Puntos de recorrido',
+		Search_App_Tooltip                                 : '<b>Buscador con expresiones regulares</b><br>' + 'Una búsqueda puede también ser realizada con las expresiones regulares * o ? y otras opciones. Puedes encontrar más detalles en la <a href=\'{0}\'>Descripción de consultas de Lucene.</a><br><br>' + '' + 'Lucene es el motor de búsqueda interno utilizado.',
+		Search_Grid_Label_NoDataMessage                    : 'No se encontraron resultados.',
+		Search_Options_Action_RestoreDefaults              : 'Restaurar Valores Por Defecto',
+		Search_Options_Checkbox_EaseSearching              : 'Utilizar búsqueda simple',
+		Search_Options_Checkbox_EaseSearching_Tooltip      : '<b>Buscador con expresiones regulares</b><br>' + 'Cuando la <i>Utilizar búsqueda simple</i> está seleccionada, un * es añadido automáticamente al texto buscado.<br><br>' + '' + '<b>Búsqueda extendida</b><br>' + 'Puede ser de ayuda desactivar <i>Utilizar búsqueda simple</i> cuando se utilizan las expresiones regulares para Lucene.',
+		Search_Options_Checkbox_ShowContentAll             : 'Todo',
+		Search_Options_Checkbox_ShowContentMarker          : 'Marcadores',
+		Search_Options_Checkbox_ShowContentTour            : 'Recorridos',
+		Search_Options_Checkbox_ShowContentWaypoint        : 'Puntos de Ruta',
+		Search_Options_Checkbox_ShowDateTime               : 'Mostrar fecha/hora',
+		Search_Options_Checkbox_ShowItemNumber             : 'Mostrar numeración',
+		Search_Options_Checkbox_ShowLuceneDocId            : 'Mostrar ID del documento Lucene',
+		Search_Options_Dialog_Header                       : 'Opciones de Búsqueda',
+		Search_Options_Group_Content                       : '¿Qué debe ser buscado?',
+		Search_Options_Group_Result                        : 'Resultado',
+		Search_Options_Group_Sorting                       : 'Orden',
+		Search_Options_Label_SortAscending                 : 'Por Fecha',
+		Search_Options_Radio_SortAscending                 : 'Ascendente',
+		Search_Options_Radio_SortDescending                : 'Descendente',
+		Search_Validation_SearchFilter                     : 'No se puede encontrar nada con estas opciones de búsqueda.'
 });		
