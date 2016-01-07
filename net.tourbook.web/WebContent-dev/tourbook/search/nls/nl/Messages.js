a4c4098006e5a68d5d1c1ba7c141df1de1195609
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2016-01-07 16:52:57 +0100

    16.1 web resources + NL js translations

--- net.tourbook.web/WebContent-dev/tourbook/search/nls/nl/Messages.js
+++ net.tourbook.web/WebContent-dev/tourbook/search/nls/nl/Messages.js
@@ -1,4 +1,30 @@
 // created with net.tourbook.ant.I18ToDojo
 define(//							
 {									
+		Search_App_Action_EditMarker                       : 'Marker aanpassen',
+		Search_App_Action_EditTour                         : 'Tocht aanpassen',
+		Search_App_Action_SearchOptions_Tooltip            : 'Zoek opties.',
+		Search_App_Action_StartSearch_Tooltip              : 'Start het zoeken.',
+		Search_App_Label_Status_Tooltip                    : 'Aantal zoekresultaten - Zoek tijd',
+		Search_App_Text_Search_PlaceHolder                 : 'Zoek tochten, markers en waypoints',
+		Search_App_Tooltip                                 : '<b>Wildcard zoeken</b><br>' + 'Zoeken kan met * of ? wildcards en andere mogelijkheden, meer info kan u hier lezen <a href=\'{0}\'>Lucene query parser omschrijving.</a><br><br>' + '' + 'Lucene wordt intern gebruikt als zoekmotor.',
+		Search_Grid_Label_NoDataMessage                    : 'Geen resultaten gevonden.',
+		Search_Options_Action_RestoreDefaults              : 'Herstel standaardwaarden',
+		Search_Options_Checkbox_EaseSearching              : 'Gebruik gemakkelijk zoeken',
+		Search_Options_Checkbox_EaseSearching_Tooltip      : '<b>* Wildcard zoeken</b><br>' + 'Wanneer <i>gemakkelijk zoeken</i> is geselecteerd, zal automatisch, intern een * toegevoegd worden aan de zoektermen.<br><br>' + '' + '<b>Uitgebreid zoeken</b><br>' + 'Als u de Lucene query parser optie gebruikt  zet u <i>gemakkelijk zoeken</i> best uit.',
+		Search_Options_Checkbox_ShowContentAll             : 'Alles',
+		Search_Options_Checkbox_ShowContentMarker          : 'Markers',
+		Search_Options_Checkbox_ShowContentTour            : 'Tochten',
+		Search_Options_Checkbox_ShowContentWaypoint        : 'Waypoints',
+		Search_Options_Checkbox_ShowDateTime               : 'Toon datum/tijd',
+		Search_Options_Checkbox_ShowItemNumber             : 'Toon item nummer',
+		Search_Options_Checkbox_ShowLuceneDocId            : 'Toon Lucene doc ID',
+		Search_Options_Dialog_Header                       : 'Zoek opties',
+		Search_Options_Group_Content                       : 'Waarop moet gezocht worden?',
+		Search_Options_Group_Result                        : 'Resultaat',
+		Search_Options_Group_Sorting                       : 'Sortering',
+		Search_Options_Label_SortAscending                 : 'Op datum',
+		Search_Options_Radio_SortAscending                 : 'Oplopend',
+		Search_Options_Radio_SortDescending                : 'Aflopend',
+		Search_Validation_SearchFilter                     : 'Er kan niets gevonden worden met deze zoek opties.'
 });		
