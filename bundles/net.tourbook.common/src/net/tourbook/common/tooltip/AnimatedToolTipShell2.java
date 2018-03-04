e2e8e4e95529134e362aba2a2130c2d2454ac55f
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2018-03-04 18:49:37 +0100

    fixed NPE

--- bundles/net.tourbook.common/src/net/tourbook/common/tooltip/AnimatedToolTipShell2.java
+++ bundles/net.tourbook.common/src/net/tourbook/common/tooltip/AnimatedToolTipShell2.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2015 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2018 Wolfgang Schramm and Contributors
  * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -1178,6 +1178,10 @@ public abstract class AnimatedToolTipShell2 {
 
 				@Override
 				public void run() {
+
+					if (_currentShell == null || _currentShell.isDisposed()) {
+						return;
+					}
 
 					// hide tooltip when another shell is activated
 
