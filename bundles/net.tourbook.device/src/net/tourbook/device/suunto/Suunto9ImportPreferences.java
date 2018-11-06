19db3358794a847737bbaa2969d0b0641c7b4a48
FJBDev <freemovin@free.fr>
2018-11-06 01:02:18 +0000

    Preference page. WIP. Can't get rid of the botton preference otherwise it removes everything.

--- bundles/net.tourbook.device/src/net/tourbook/device/suunto/Suunto9ImportPreferences.java
+++ bundles/net.tourbook.device/src/net/tourbook/device/suunto/Suunto9ImportPreferences.java
@@ -1,53 +1,69 @@
 package net.tourbook.device.suunto;
 
+import java.util.ArrayList;
+
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.jface.layout.PixelConverter;
-import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.jface.util.PropertyChangeEvent;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-import net.tourbook.common.preferences.BooleanFieldEditor2;
+import net.tourbook.application.TourbookPlugin;
+import net.tourbook.preferences.ITourbookPreferences;
 
 public class Suunto9ImportPreferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
-	public static final String	IMPORT_INTO_TITLE_TRUNCATED	= "truncated";	//$NON-NLS-1$
-	public static final String	IMPORT_INTO_TITLE_ALL			= "all";			//$NON-NLS-1$
+	ArrayList<String>						AltitudeData	= new ArrayList<String>() {
+																		{
+																			//TODO Use Messages.
+																			add("GPS");
+																			add("Altitude");
+																		}
+																	};
+	ArrayList<String>						DistanceData	= new ArrayList<String>() {
+																		{
+																			//TODO Use Messages.
+																			add("GPS");
+																			add("Provided values");
+																		}
+																	};
+	private SelectionAdapter			_defaultSelectionListener;
 
-	private SelectionAdapter	_defaultSelectionListener;
+	private final IPreferenceStore	_prefStore		= TourbookPlugin.getDefault().getPreferenceStore();
 
-	private PixelConverter		_pc;
+	private PixelConverter				_pc;
 
 	/*
 	 * UI controls
 	 */
-	private Composite					_containerCharacter;
+	private Group					_groupData;
 
-	private Group						_groupNotesImport;
+	private Combo					_comboAlgorithm;
+	private Combo					_comboDistance;
 
-	private BooleanFieldEditor2	_editBool_IgnoreSpeedValues;
-	private BooleanFieldEditor		_editBool_ImportIntoDescription;
-	private BooleanFieldEditor2	_editBool_ImportIntoTitle;
+	private StringFieldEditor	_txtDecimalSep;
 
-	private IntegerFieldEditor		_editInt_TruncatedNotes;
-
-	private Label						_lblIgnoreSpeed;
-
-	private Button						_chkIgnoreSpeed;
-	private Button						_rdoImportAll;
-	private Button						_rdoImportTruncated;
+	private Label					_lblExample;
+	private Label					_lblExampleValue;
 
 	@Override
 	protected void createFieldEditors() {
 
 		createUI();
+
+		setupUI();
 
 	}
 
@@ -56,14 +72,108 @@ public class Suunto9ImportPreferences extends FieldEditorPreferencePage implemen
 		final Composite parent = getFieldEditorParent();
 		GridLayoutFactory.fillDefaults().applyTo(parent);
 
-		initUI(parent);
+		/*
+		 * Data
+		 */
+		_groupData = new Group(parent, SWT.NONE);
+		_groupData.setText(Messages.pref_data);
+		GridDataFactory.fillDefaults().grab(true, false).applyTo(_groupData);
+		{
+			// label: value example
+			_lblExample = new Label(_groupData, SWT.NONE);
+			_lblExample.setText("ddd");
+			/*
+			 * combo: smoothing algorithm
+			 */
+			_comboAlgorithm = new Combo(_groupData, SWT.READ_ONLY | SWT.BORDER);
+			_comboAlgorithm.setVisibleItemCount(10);
+			// _comboAlgorithm.addFocusListener(_keepOpenListener);
+			_comboAlgorithm.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(final SelectionEvent e) {
+					// if (_isUpdateUI) {
+					return;
+				}
+				// onSelectSmoothingAlgo();
+			});
 
+			_lblExampleValue = new Label(_groupData, SWT.NONE);
+			_lblExampleValue.setText("Distance");
+
+			/*
+			 * combo: smoothing algorithm
+			 */
+			_comboDistance = new Combo(_groupData, SWT.READ_ONLY | SWT.BORDER);
+			_comboDistance.setVisibleItemCount(10);
+			// _comboAlgorithm.addFocusListener(_keepOpenListener);
+			_comboDistance.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(final SelectionEvent e) {
+					// if (_isUpdateUI) {
+					return;
+				}
+				// onSelectSmoothingAlgo();
+			});
+
+			// text: decimal separator
+			_txtDecimalSep = new StringFieldEditor(
+					ITourbookPreferences.REGIONAL_DECIMAL_SEPARATOR,
+					"dewfreg",
+					_groupData);
+			GridDataFactory
+					.swtDefaults()
+					.hint(15, SWT.DEFAULT)
+					.applyTo(_txtDecimalSep.getTextControl(_groupData));
+			_txtDecimalSep.setTextLimit(1);
+			_txtDecimalSep.setPreferenceStore(_prefStore);
+			_txtDecimalSep.load();
+			_txtDecimalSep.setPropertyChangeListener(new IPropertyChangeListener() {
+				public void propertyChange(final PropertyChangeEvent event) {}
+			});
+
+		}
+
+		// add layout to the group
+		//final GridLayout regionalLayout = (GridLayout) _groupData.getLayout();
+		//	regionalLayout.marginWidth = 5;
+		//regionalLayout.marginHeight = 5;
+
+		enableControls();
+
+	}
+
+	private void setupUI() {
+
+		/*
+		 * fillup algorithm combo
+		 */
+		for (int index = 0; index < AltitudeData.size(); ++index) {
+			_comboAlgorithm.add(AltitudeData.get(index));
+		}
+		_comboAlgorithm.select(0);
+
+		/*
+		 * fillup algorithm combo
+		 */
+		for (String distanceChoice : DistanceData) {
+			_comboDistance.add(distanceChoice);
+		}
+		_comboDistance.select(0);
+	}
+
+	private void enableControls() {
+
+		// final boolean isUseCustomFormat = _chkUseCustomFormat.getBooleanValue();
+
+		// _txtDecimalSep.setEnabled(isUseCustomFormat, _groupData);
+		// _txtGroupSep.setEnabled(isUseCustomFormat, _groupData);
+
+		// _lblExample.setEnabled(isUseCustomFormat);
+		// _lblExampleValue.setEnabled(isUseCustomFormat);
 	}
 
 	@Override
-	public void init(final IWorkbench workbench) {
-		//setPreferenceStore(_prefStore);
-	}
+	public void init(final IWorkbench workbench) {}
 
 	private void initUI(final Composite parent) {
 
@@ -78,18 +188,15 @@ public class Suunto9ImportPreferences extends FieldEditorPreferencePage implemen
 	@Override
 	protected void performDefaults() {
 
-		_editBool_IgnoreSpeedValues.loadDefault();
-		_editBool_ImportIntoDescription.loadDefault();
-		_editBool_ImportIntoTitle.loadDefault();
-
-		_editInt_TruncatedNotes.loadDefault();
-
+		_comboAlgorithm.select(0);
+		_comboDistance.select(0);
 	}
 
 	@Override
 	public boolean performOk() {
 
-		//_prefStore.setValue(IPreferences.IS_TITLE_IMPORT_ALL, _rdoImportAll.getSelection());
+		// _prefStore.setValue(IPreferences.IS_TITLE_IMPORT_ALL,
+		// _rdoImportAll.getSelection());
 
 		return super.performOk();
 	}
