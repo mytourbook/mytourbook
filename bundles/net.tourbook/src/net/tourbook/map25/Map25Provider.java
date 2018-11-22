d31d3f68df807b3c3e361373707bcd2b6f4d3fd9
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2018-11-22 17:30:04 +0100

    fixed problem that new map providers cannot be edited

--- bundles/net.tourbook/src/net/tourbook/map25/Map25Provider.java
+++ bundles/net.tourbook/src/net/tourbook/map25/Map25Provider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2017 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2018 Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -21,114 +21,114 @@ import de.byteholder.geoclipse.map.UI;
 
 public class Map25Provider implements Cloneable {
 
-	private String		_id;
-	private UUID		_uuid;
+   private String      _id;
+   private UUID        _uuid;
 
-	public boolean		isEnabled;
-	public boolean		isDefault;
+   public boolean      isEnabled;
+   public boolean      isDefault;
 
-	public String		name			= UI.EMPTY_STRING;
-	public String		description		= UI.EMPTY_STRING;
+   public String       name         = UI.EMPTY_STRING;
+   public String       description  = UI.EMPTY_STRING;
 
-	public String		url				= UI.EMPTY_STRING;
-	public String		tilePath		= UI.EMPTY_STRING;
+   public String       url          = UI.EMPTY_STRING;
+   public String       tilePath     = UI.EMPTY_STRING;
 
-	public String		apiKey			= UI.EMPTY_STRING;
+   public String       apiKey       = UI.EMPTY_STRING;
 
-	public TileEncoding	tileEncoding	= TileEncoding.MVT;
+   public TileEncoding tileEncoding = TileEncoding.MVT;
 
-	public Map25Provider() {
+   public Map25Provider() {
 
-		_uuid = UUID.randomUUID();
-		_id = _uuid.toString();
-	}
+      _uuid = UUID.randomUUID();
+      _id = _uuid.toString();
+   }
 
-	/**
-	 * @param notCheckedUUID
-	 *            Contains a UUID string but it can be invalid.
-	 */
-	public Map25Provider(final String notCheckedUUID) {
+   /**
+    * @param notCheckedUUID
+    *           Contains a UUID string but it can be invalid.
+    */
+   public Map25Provider(final String notCheckedUUID) {
 
-		UUID uuid;
-		try {
-			uuid = UUID.fromString(notCheckedUUID);
-		} catch (final Exception e) {
-			uuid = UUID.randomUUID();
-		}
+      UUID uuid;
+      try {
+         uuid = UUID.fromString(notCheckedUUID);
+      } catch (final Exception e) {
+         uuid = UUID.randomUUID();
+      }
 
-		_uuid = uuid;
-		_id = _uuid.toString();
-	}
+      _uuid = uuid;
+      _id = _uuid.toString();
+   }
 
-	@Override
-	public Object clone() {
+   @Override
+   public Object clone() {
 
-		try {
+      try {
 
-			final Map25Provider clonedProvider = (Map25Provider) super.clone();
+         final Map25Provider clonedProvider = (Map25Provider) super.clone();
 
-			return clonedProvider;
+         return clonedProvider;
 
-		} catch (final CloneNotSupportedException e) {
+      } catch (final CloneNotSupportedException e) {
 
-			// this shouldn't happen, since we are Cloneable
-			throw new InternalError(e);
-		}
-	}
+         // this shouldn't happen, since we are Cloneable
+         throw new InternalError(e);
+      }
+   }
 
-	@Override
-	public boolean equals(final Object obj) {
+   @Override
+   public boolean equals(final Object obj) {
 
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (getClass() != obj.getClass()) {
+         return false;
+      }
 
-		final Map25Provider other = (Map25Provider) obj;
-		if (_uuid == null) {
-			if (other._uuid != null) {
-				return false;
-			}
-		} else if (!_uuid.equals(other._uuid)) {
-			return false;
-		}
+      final Map25Provider other = (Map25Provider) obj;
+      if (_uuid == null) {
+         if (other._uuid != null) {
+            return false;
+         }
+      } else if (!_uuid.equals(other._uuid)) {
+         return false;
+      }
 
-		return true;
-	}
+      return true;
+   }
 
-	/**
-	 * @return Returns the map provider {@link UUID} as string.
-	 */
-	public String getId() {
-		return _id;
-	}
+   /**
+    * @return Returns the map provider {@link UUID} as string.
+    */
+   public String getId() {
+      return _id;
+   }
 
-	public UUID getUuid() {
-		return _uuid;
-	}
+   public UUID getUuid() {
+      return _uuid;
+   }
 
-	@Override
-	public int hashCode() {
+   @Override
+   public int hashCode() {
 
-		return _uuid.hashCode();
-	}
+      return _uuid.hashCode();
+   }
 
-	@Override
-	public String toString() {
-		return "Map25Provider [" //$NON-NLS-1$
-//				+ "id=" + id + ", "
-				+ "name=" + name + ", " //$NON-NLS-1$ //$NON-NLS-2$
-				//				+ "description=" + description + ", "
-				//				+ "offlineFolder=" + offlineFolder + ", "
-				//				+ "url=" + url + ", "
-				//				+ "tilePath=" + tilePath + ", "
-				//				+ "apiKey=" + apiKey
-				+ "]"; //$NON-NLS-1$
-	}
+   @Override
+   public String toString() {
+      return "Map25Provider [" //$NON-NLS-1$
+//          + "id=" + id + ", "
+            + "name=" + name + ", " //$NON-NLS-1$ //$NON-NLS-2$
+            //          + "description=" + description + ", "
+            //          + "offlineFolder=" + offlineFolder + ", "
+            //          + "url=" + url + ", "
+            //          + "tilePath=" + tilePath + ", "
+            //          + "apiKey=" + apiKey
+            + "]"; //$NON-NLS-1$
+   }
 
 }
