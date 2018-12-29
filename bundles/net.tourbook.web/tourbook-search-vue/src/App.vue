37c4234f1f0f3e569d722fa24b748af22c1a40a3
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2018-12-29 17:11:59 +0100

    disable line animation in input field

--- bundles/net.tourbook.web/tourbook-search-vue/src/App.vue
+++ bundles/net.tourbook.web/tourbook-search-vue/src/App.vue
@@ -6,26 +6,30 @@
          <tr>
             <td style="width:24px;">
                <v-tooltip bottom>
-                  <div slot="activator" id="domInfo" class="actionIcon iconPhotoTooltip" tabindex="0">&nbsp;</div>
-                  <span>Tooltip</span>
+                  <div slot="activator" id="domInfo" class="actionIcon iconPhotoTooltip" tabindex="1">&nbsp;</div>
+                  <div>some text 2</div>
                </v-tooltip>
             </td>
             <td style="">
-               <v-autocomplete
-                  label="Search Tours, Marker and Waypoints"
-                  :items="items"
-                  v-model="value"
-               ></v-autocomplete>
+               <v-autocomplete 
+                  :label="Messages.Search_App_Text_Search_PlaceHolder"
+                  :items="searchItems"
+                  item-text="itemText"
+                  autofocus
+
+                  class="no-transition"
+                  tabindex="2"
+               />
                <div id="domSearchInput"></div>
             </td>
             <td style="width:24px;">
-               <div slot="activator" id="domActionSearch" class="actionIcon iconSearch" tabindex="10">&nbsp;</div>
+               <div slot="activator" id="domActionSearch" class="actionIcon iconSearch" tabindex="3">&nbsp;</div>
             </td>
             <td style="width:80px;">
                <div id="domAppStatus" style="padding-left: 0.2em;">&nbsp;</div>
             </td>
             <td style="width:24px;">
-               <div slot="activator" id="domAction_Options" class="actionIcon iconOptions" tabindex="20">&nbsp;</div>
+               <div slot="activator" id="domAction_Options" class="actionIcon iconOptions" tabindex="4">&nbsp;</div>
             </td>
          </tr>
       </table>
@@ -36,24 +40,34 @@
 
 <script>
 
-import HelloWorld from "./components/HelloWorld";
 
 export default {
 	
   name: "App",
   components: {
-    HelloWorld
+
   },
   
-  data() {
-    return {
-      //
-    };
-  }
+  data: () => ({
+
+       value:'this is a value',
+
+       searchItems: [
+
+          {itemText: 'abc 1', id: 1},
+          {itemText: 'bcd 2', id: 2},
+          {itemText: 'cde 3', id: 3},
+          {itemText: 'def 4', id: 4},
+       ],
+
+       Messages: {
+          Search_App_Text_Search_PlaceHolder : 'Search Tours, Marker and Waypoints'
+       }
+    })
 };
 </script>
 
-<style> 
+<style>
 
 @import './assets/search.css'; 
 
