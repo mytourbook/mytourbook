92b276040a0e6dc3068fce0c3bdd4faa85fa60f8
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2018-12-29 17:12:32 +0100

    added axios and lodash

--- bundles/net.tourbook.web/tourbook-search-vue/src/main.js
+++ bundles/net.tourbook.web/tourbook-search-vue/src/main.js
@@ -8,7 +8,19 @@ import 'typeface-roboto'
 // material design icons
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
-import Vue from "vue";
+import Vue from 'vue'
+
+// ajax
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+// lodash
+import VueLodash from 'vue-lodash'
+// const options = { name: 'lodash' } // customize the way you want to call it
+Vue.use(VueLodash/*, options*/) // options is optional
+
+// vuetify
 import './plugins/vuetify'
 import App from "./App.vue";
 
