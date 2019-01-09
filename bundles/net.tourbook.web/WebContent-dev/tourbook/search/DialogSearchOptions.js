ea8a11f53daf028097ec0bc051b03dbbc371d687
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2019-01-09 08:32:14 +0100

    replaced crlf->lf

--- bundles/net.tourbook.web/WebContent-dev/tourbook/search/DialogSearchOptions.js
+++ bundles/net.tourbook.web/WebContent-dev/tourbook/search/DialogSearchOptions.js
@@ -1,20 +1,20 @@
 define(
 [
-	'dojo/_base/declare',
-	"dojo/_base/lang",
-	'dojo/dom',
-	'dojo/on',
-	"dojo/request/xhr",
+ 'dojo/_base/declare',
+ "dojo/_base/lang",
+ 'dojo/dom',
+ 'dojo/on',
+ "dojo/request/xhr",
 
-	'dijit/form/NumberSpinner',
-	'dijit/form/RadioButton',
-	'dijit/TitlePane',
+ 'dijit/form/NumberSpinner',
+ 'dijit/form/RadioButton',
+ 'dijit/TitlePane',
 
-	'../widget/BaseDialog',
-	'./SearchMgr',
+ '../widget/BaseDialog',
+ './SearchMgr',
 
-	'dojo/text!./DialogSearchOptions.html',
-	'dojo/i18n!./nls/Messages'
+ 'dojo/text!./DialogSearchOptions.html',
+ 'dojo/i18n!./nls/Messages'
 
 ], function(//
 //	
@@ -37,231 +37,236 @@ Messages //
 //
 ) {
 
-	var dlgSearchOptions = declare('tourbook.search.DialogSearchOptions',
-	[ BaseDialog
-	], {
+   var dlgSearchOptions = declare('tourbook.search.DialogSearchOptions',
+   [ BaseDialog
+   ],
+   {
 
-		templateString : template,
+      templateString : template,
 
-		// create messages field which is needed that messages can be accessed in the template
-		messages : Messages,
+      // create messages field which is needed that messages can be accessed in the template
+      messages : Messages,
 
-		constructor : function(args) {
-			this._searchApp = args.searchApp;
-		},
+      constructor : function(args) {
+         this._searchApp = args.searchApp;
+      },
 
-		postCreate : function() {
+      postCreate : function() {
 
-			var dlg = this;
+         var dlg = this;
 
-			this.inherited(arguments);
+         this.inherited(arguments);
 
-			// hide dialog when mouse has leaved it
-			on(this.domNode, "mouseleave", lang.hitch(this, "hideDialog"));
+         // hide dialog when mouse has leaved it
+         on(this.domNode, "mouseleave", lang.hitch(this, "hideDialog"));
 
-			/*
-			 * Tooltips with html tags must be defined in the js code, otherwise the TAGs do not work.
-			 */
-			this.apChk_EaseSearching_Tooltip.label = Messages.Search_Options_Checkbox_EaseSearching_Tooltip;
+         /*
+          * Tooltips with html tags must be defined in the js code, otherwise the TAGs do not work.
+          */
+         this.apChk_EaseSearching_Tooltip.label = Messages.Search_Options_Checkbox_EaseSearching_Tooltip;
 
-			this.apChk_EaseSearching_Tooltip.onShow = function() {
-				dlg.openedTooltips++;
+         this.apChk_EaseSearching_Tooltip.onShow = function() {
+            dlg.openedTooltips++;
 //				console.log("onShow: " + dlg.openedTooltips);
-			};
+         };
 
-			this.apChk_EaseSearching_Tooltip.onHide = function() {
-				dlg.openedTooltips--;
+         this.apChk_EaseSearching_Tooltip.onHide = function() {
+            dlg.openedTooltips--;
 //				console.log("onHide: " + dlg.openedTooltips);
-			};
-		},
+         };
+      },
 
-		/**
-		 * Overwrite BaseDialog.showDialog and restore the state of the UI.
-		 */
-		showDialog : function showDialog(args) {
+      /**
+       * Overwrite BaseDialog.showDialog and restore the state of the UI.
+       */
+      showDialog : function showDialog(args) {
 
-			this.inherited(arguments);
+         this.inherited(arguments);
 
-			this._restoreState();
-		},
+         this._restoreState();
+      },
 
-		apActionRestoreDefaults : function apActionRestoreDefaults() {
+      apActionRestoreDefaults : function apActionRestoreDefaults() {
 
-			this._setSearchOptions(//
-			{
-				isRestoreDefaults : true
-			});
-		},
+         this._setSearchOptions(
+         //
+         {
+            isRestoreDefaults : true
+         });
+      },
 
-		/**
-		 * Search all checkbox
-		 */
-		apSearchAll : function apSearchAll() {
+      /**
+       * Search all checkbox
+       */
+      apSearchAll : function apSearchAll() {
 
-			this._enableControls();
+         this._enableControls();
 
-			// fire selection
-			this.apSelection();
-		},
+         // fire selection
+         this.apSelection();
+      },
 
-		/**
-		 * Selection is from an attach point.
-		 */
-		apSelection : function apSelection() {
+      /**
+       * Selection is from an attach point.
+       */
+      apSelection : function apSelection() {
 
-			if (this._isValid()) {
+         if (this._isValid()) {
 
-				var searchOptions = //
-				{
-					isEaseSearching : this.apChkEaseSearching.get('checked'),
+            var searchOptions =
+            //
+            {
+               isEaseSearching : this.apChkEaseSearching.get('checked'),
 
-					isShowContentAll : this.apChkShowContentAll.get('checked'),
-					isShowContentTour : this.apChkShowContentTour.get('checked'),
-					isShowContentMarker : this.apChkShowContentMarker.get('checked'),
-					isShowContentWaypoint : this.apChkShowContentWaypoint.get('checked'),
+               isShowContentAll : this.apChkShowContentAll.get('checked'),
+               isShowContentTour : this.apChkShowContentTour.get('checked'),
+               isShowContentMarker : this.apChkShowContentMarker.get('checked'),
+               isShowContentWaypoint : this.apChkShowContentWaypoint.get('checked'),
 
-					isSortByDateAscending : this.apSortByDateAscending.get('checked'),
+               isSortByDateAscending : this.apSortByDateAscending.get('checked'),
 
-					isShowDate : this.apChkShowDate.get('checked'),
-					isShowTime : this.apChkShowTime.get('checked'),
-					isShowDescription : this.apChkShowDescription.get('checked'),
-					isShowItemNumber : this.apChkShowItemNumber.get('checked'),
-					isShowLuceneID : this.apChkShowLuceneID.get('checked')
-				};
+               isShowDate : this.apChkShowDate.get('checked'),
+               isShowTime : this.apChkShowTime.get('checked'),
+               isShowDescription : this.apChkShowDescription.get('checked'),
+               isShowItemNumber : this.apChkShowItemNumber.get('checked'),
+               isShowLuceneID : this.apChkShowLuceneID.get('checked')
+            };
 
-				this._setSearchOptions(searchOptions);
-			}
-		},
+            this._setSearchOptions(searchOptions);
+         }
+      },
 
-		_isValid : function _isValid() {
+      _isValid : function _isValid() {
 
-			var //
-			statusText = '', //
-			isValid = true, //
+         var //
+         statusText = '', //
+         isValid = true, //
 
-			isShowContentAll = this.apChkShowContentAll.get('checked'), //
-			isShowContentTour = this.apChkShowContentTour.get('checked'), //
-			isShowContentMarker = this.apChkShowContentMarker.get('checked'), //
-			isShowContentWaypoint = this.apChkShowContentWaypoint.get('checked');
+         isShowContentAll = this.apChkShowContentAll.get('checked'), //
+         isShowContentTour = this.apChkShowContentTour.get('checked'), //
+         isShowContentMarker = this.apChkShowContentMarker.get('checked'), //
+         isShowContentWaypoint = this.apChkShowContentWaypoint.get('checked');
 
-			if (isShowContentAll) {
+         if (isShowContentAll) {
 
-				// content is valid
+            // content is valid
 
-			} else {
+         } else {
 
-				// at least one content must be checked
+            // at least one content must be checked
 
-				if (isShowContentTour == false //
-					&& isShowContentMarker == false //
-					&& isShowContentWaypoint == false) {
+            if (isShowContentTour == false //
+               && isShowContentMarker == false //
+               && isShowContentWaypoint == false) {
 
-					statusText = Messages.Search_Validation_SearchFilter;
-					isValid = false;
-				}
-			}
+               statusText = Messages.Search_Validation_SearchFilter;
+               isValid = false;
+            }
+         }
 
-			// update status
-			dom.byId('domSearchStatus').innerHTML = statusText;
+         // update status
+         dom.byId('domSearchStatus').innerHTML = statusText;
 
-			// resize dialog because status text has changed and can be too long 
-			this._dialog.resize();
+         // resize dialog because status text has changed and can be too long 
+         this._dialog.resize();
 
-			return isValid;
-		},
+         return isValid;
+      },
 
-		_enableControls : function _enableControls() {
+      _enableControls : function _enableControls() {
 
-			var isShowContentAll = this.apChkShowContentAll.get('checked');
+         var isShowContentAll = this.apChkShowContentAll.get('checked');
 
-			this.apChkShowContentTour.set('disabled', isShowContentAll);
-			this.apChkShowContentMarker.set('disabled', isShowContentAll);
-			this.apChkShowContentWaypoint.set('disabled', isShowContentAll);
-		},
+         this.apChkShowContentTour.set('disabled', isShowContentAll);
+         this.apChkShowContentMarker.set('disabled', isShowContentAll);
+         this.apChkShowContentWaypoint.set('disabled', isShowContentAll);
+      },
 
-		/**
-		 * 
-		 */
-		_restoreState : function _restoreState(callBack) {
+      /**
+       * 
+       */
+      _restoreState : function _restoreState(callBack) {
 
-			var _this = this;
+         var _this = this;
 
-			var xhrQuery = {};
-			xhrQuery[SearchMgr.XHR_PARAM_ACTION] = SearchMgr.XHR_ACTION_GET_SEARCH_OPTIONS;
+         var xhrQuery = {};
+         xhrQuery[SearchMgr.XHR_PARAM_ACTION] = SearchMgr.XHR_ACTION_GET_SEARCH_OPTIONS;
 
-			xhr(SearchMgr.XHR_SEARCH_HANDLER, {
+         xhr(SearchMgr.XHR_SEARCH_HANDLER,
+         {
 
-				handleAs : 'json',
-				preventCache : true,
-				timeout : SearchMgr.XHR_TIMEOUT,
+            handleAs : 'json',
+            preventCache : true,
+            timeout : SearchMgr.XHR_TIMEOUT,
 
-				query : xhrQuery
+            query : xhrQuery
 
-			}).then(function(xhrData) {
+         }).then(function(xhrData) {
 
-				_this._updateUI_FromState(_this, xhrData);
-			});
-		},
+            _this._updateUI_FromState(_this, xhrData);
+         });
+      },
 
-		/**
-		 * Set search options in the backend and reload current search with new search options.
-		 */
-		_setSearchOptions : function _setSearchOptions(searchOptions) {
+      /**
+       * Set search options in the backend and reload current search with new search options.
+       */
+      _setSearchOptions : function _setSearchOptions(searchOptions) {
 
-			var _this = this;
+         var _this = this;
 
-			var jsonSearchOptions = JSON.stringify(searchOptions);
+         var jsonSearchOptions = JSON.stringify(searchOptions);
 
-			var xhrQuery = {};
-			xhrQuery[SearchMgr.XHR_PARAM_ACTION] = SearchMgr.XHR_ACTION_SET_SEARCH_OPTIONS;
-			xhrQuery[SearchMgr.XHR_PARAM_SEARCH_OPTIONS] = encodeURIComponent(jsonSearchOptions);
+         var xhrQuery = {};
+         xhrQuery[SearchMgr.XHR_PARAM_ACTION] = SearchMgr.XHR_ACTION_SET_SEARCH_OPTIONS;
+         xhrQuery[SearchMgr.XHR_PARAM_SEARCH_OPTIONS] = encodeURIComponent(jsonSearchOptions);
 
-			xhr(SearchMgr.XHR_SEARCH_HANDLER, {
+         xhr(SearchMgr.XHR_SEARCH_HANDLER,
+         {
 
-				handleAs : 'json',
-				preventCache : true,
-				timeout : SearchMgr.XHR_TIMEOUT,
+            handleAs : 'json',
+            preventCache : true,
+            timeout : SearchMgr.XHR_TIMEOUT,
 
-				query : xhrQuery
+            query : xhrQuery
 
-			}).then(function(xhrData) {
+         }).then(function(xhrData) {
 
-				if (xhrData.isSearchOptionsDefault) {
+            if (xhrData.isSearchOptionsDefault) {
 
-					// set defaults in the UI
-					_this._updateUI_FromState(_this, xhrData);
-				}
+               // set defaults in the UI
+               _this._updateUI_FromState(_this, xhrData);
+            }
 
-				// repeat previous search
+            // repeat previous search
 
-				_this._searchApp._searchInput.startSearch(true);
-			});
-		},
+            _this._searchApp._searchInput.startSearch(true);
+         });
+      },
 
-		_updateUI_FromState : function _updateUI_FromState(dialog, xhrData) {
+      _updateUI_FromState : function _updateUI_FromState(dialog, xhrData) {
 
-			dialog.apChkEaseSearching.set('checked', xhrData.isEaseSearching);
+         dialog.apChkEaseSearching.set('checked', xhrData.isEaseSearching);
 
-			dialog.apChkShowContentAll.set('checked', xhrData.isShowContentAll);
-			dialog.apChkShowContentTour.set('checked', xhrData.isShowContentTour);
-			dialog.apChkShowContentMarker.set('checked', xhrData.isShowContentMarker);
-			dialog.apChkShowContentWaypoint.set('checked', xhrData.isShowContentWaypoint);
+         dialog.apChkShowContentAll.set('checked', xhrData.isShowContentAll);
+         dialog.apChkShowContentTour.set('checked', xhrData.isShowContentTour);
+         dialog.apChkShowContentMarker.set('checked', xhrData.isShowContentMarker);
+         dialog.apChkShowContentWaypoint.set('checked', xhrData.isShowContentWaypoint);
 
-			dialog.apSortByDateAscending.set('checked', xhrData.isSortByDateAscending);
-			dialog.apSortByDateDescending.set('checked', !xhrData.isSortByDateAscending);
+         dialog.apSortByDateAscending.set('checked', xhrData.isSortByDateAscending);
+         dialog.apSortByDateDescending.set('checked', !xhrData.isSortByDateAscending);
 
-			dialog.apChkShowDate.set('checked', xhrData.isShowDate);
-			dialog.apChkShowTime.set('checked', xhrData.isShowTime);
-			dialog.apChkShowDescription.set('checked', xhrData.isShowDescription);
-			dialog.apChkShowItemNumber.set('checked', xhrData.isShowItemNumber);
-			dialog.apChkShowLuceneID.set('checked', xhrData.isShowLuceneID);
+         dialog.apChkShowDate.set('checked', xhrData.isShowDate);
+         dialog.apChkShowTime.set('checked', xhrData.isShowTime);
+         dialog.apChkShowDescription.set('checked', xhrData.isShowDescription);
+         dialog.apChkShowItemNumber.set('checked', xhrData.isShowItemNumber);
+         dialog.apChkShowLuceneID.set('checked', xhrData.isShowLuceneID);
 
-			dialog._enableControls();
-			dialog._isValid();
-		}
+         dialog._enableControls();
+         dialog._isValid();
+      }
 
-	});
+   });
 
-	return dlgSearchOptions;
+   return dlgSearchOptions;
 
 });
