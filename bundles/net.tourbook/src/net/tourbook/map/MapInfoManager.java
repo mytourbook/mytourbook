5b1bd42bfaf7ac65d40db9fee2d4205e2b47f850
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2019-01-27 10:17:34 +0100

    reduce digits for lat/lon in map location (statusline)

--- bundles/net.tourbook/src/net/tourbook/map/MapInfoManager.java
+++ bundles/net.tourbook/src/net/tourbook/map/MapInfoManager.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2017 Wolfgang Schramm and Contributors
- * 
+ * Copyright (C) 2005, 2019 Wolfgang Schramm and Contributors
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
@@ -17,88 +17,146 @@ package net.tourbook.map;
 
 import java.text.NumberFormat;
 
-import net.tourbook.map2.Messages;
-
 import org.eclipse.osgi.util.NLS;
+
+import net.tourbook.map2.Messages;
 
 public class MapInfoManager {
 
-	private static MapInfoManager	_instance;
+   private static MapInfoManager _instance;
 
-	private double					_latitude		= Double.MIN_VALUE;
-	private double					_longitude;
+   private double                _latitude     = Double.MIN_VALUE;
+   private double                _longitude;
 
-	private int						_mapZoomLevel	= -1;
+   private int                   _mapZoomLevel = -1;
 
-	private final NumberFormat		_nf6			= NumberFormat.getNumberInstance();
-	{
-		_nf6.setMinimumFractionDigits(6);
-		_nf6.setMaximumFractionDigits(6);
-	}
+   private final NumberFormat    _nf0          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf1          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf2          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf3          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf4          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf5          = NumberFormat.getNumberInstance();
+   private final NumberFormat    _nf6          = NumberFormat.getNumberInstance();
 
-	private MapInfoControl _infoWidget;
+   {
+      _nf0.setMinimumFractionDigits(0);
+      _nf0.setMaximumFractionDigits(0);
+      _nf1.setMinimumFractionDigits(1);
+      _nf1.setMaximumFractionDigits(1);
+      _nf2.setMinimumFractionDigits(2);
+      _nf2.setMaximumFractionDigits(2);
+      _nf3.setMinimumFractionDigits(3);
+      _nf3.setMaximumFractionDigits(3);
+      _nf4.setMinimumFractionDigits(4);
+      _nf4.setMaximumFractionDigits(4);
+      _nf5.setMinimumFractionDigits(5);
+      _nf5.setMaximumFractionDigits(5);
+      _nf6.setMinimumFractionDigits(6);
+      _nf6.setMaximumFractionDigits(6);
+   }
 
-	public static MapInfoManager getInstance() {
+   private MapInfoControl _infoWidget;
 
-		if (_instance == null) {
-			_instance = new MapInfoManager();
-		}
+   public static MapInfoManager getInstance() {
 
-		return _instance;
-	}
+      if (_instance == null) {
+         _instance = new MapInfoManager();
+      }
 
-	public void resetInfo() {
+      return _instance;
+   }
 
-		_latitude = Double.MIN_VALUE;
+   public void resetInfo() {
 
-		updateUI();
-	}
+      _latitude = Double.MIN_VALUE;
 
-	void setInfoWidget(final MapInfoControl infoWidget) {
+      updateUI();
+   }
 
-		_infoWidget = infoWidget;
+   void setInfoWidget(final MapInfoControl infoWidget) {
 
-		updateUI();
-	}
+      _infoWidget = infoWidget;
 
-	public void setMapPosition(final double latitude, final double longitude, final int zoomLevel) {
+      updateUI();
+   }
 
-		_latitude = latitude;
-		_longitude = longitude;
-		_mapZoomLevel = zoomLevel;
+   public void setMapPosition(final double latitude, final double longitude, final int zoomLevel) {
 
-		updateUI();
-	}
+      _latitude = latitude;
+      _longitude = longitude;
+      _mapZoomLevel = zoomLevel;
 
-	private void updateUI() {
+      updateUI();
+   }
 
-		// check widget
-		if ((_infoWidget == null) || _infoWidget.isDisposed()) {
-			return;
-		}
+   private void updateUI() {
 
-		// check data
-		if ((_latitude == Double.MIN_VALUE) || (_mapZoomLevel == -1)) {
+      // check widget
+      if ((_infoWidget == null) || _infoWidget.isDisposed()) {
+         return;
+      }
 
-			_infoWidget.setText(Messages.statusLine_mapInfo_defaultText);
+      // check data
+      if ((_latitude == Double.MIN_VALUE) || (_mapZoomLevel == -1)) {
 
-		} else {
+         _infoWidget.setText(Messages.statusLine_mapInfo_defaultText);
 
-			double lon = _longitude % 360;
-			lon = lon > 180 ? //
-					lon - 360
-					: lon < -180 ? //
-							lon + 360
-							: lon;
+      } else {
 
-			_infoWidget.setText(
-					NLS.bind(
-							Messages.statusLine_mapInfo_data,
-							new Object[] {
-									_nf6.format(_latitude),
-									_nf6.format(lon),
-									Integer.toString(_mapZoomLevel + 1) }));
-		}
-	}
+         double lon = _longitude % 360;
+         lon = lon > 180 ? //
+               lon - 360
+               : lon < -180 ? //
+                     lon + 360
+                     : lon;
+
+         final int minZoomLevel_Digit_1 = 1;
+         final int minZoomLevel_Digit_2 = minZoomLevel_Digit_1 + 3;
+         final int minZoomLevel_Digit_3 = minZoomLevel_Digit_2 + 3;
+         final int minZoomLevel_Digit_4 = minZoomLevel_Digit_3 + 3;
+         final int minZoomLevel_Digit_5 = minZoomLevel_Digit_4 + 3;
+         final int minZoomLevel_Digit_6 = minZoomLevel_Digit_5 + 3;
+
+         final String latText =
+
+               _mapZoomLevel > minZoomLevel_Digit_6 ? _nf6.format(_latitude) : //
+
+                     _mapZoomLevel > minZoomLevel_Digit_5 ? _nf5.format(_latitude) : //
+
+                           _mapZoomLevel > minZoomLevel_Digit_4 ? _nf4.format(_latitude) : //
+
+                                 _mapZoomLevel > minZoomLevel_Digit_3 ? _nf3.format(_latitude) : //
+
+                                       _mapZoomLevel > minZoomLevel_Digit_2 ? _nf2.format(_latitude) : //
+
+                                             _mapZoomLevel > minZoomLevel_Digit_1 ? _nf1.format(_latitude) : //
+
+                                                   _nf0.format(_latitude);
+
+         final String lonText =
+
+               _mapZoomLevel > minZoomLevel_Digit_6 ? _nf6.format(lon) : //
+
+                     _mapZoomLevel > minZoomLevel_Digit_5 ? _nf5.format(lon) : //
+
+                           _mapZoomLevel > minZoomLevel_Digit_4 ? _nf4.format(lon) : //
+
+                                 _mapZoomLevel > minZoomLevel_Digit_3 ? _nf3.format(lon) : //
+
+                                       _mapZoomLevel > minZoomLevel_Digit_2 ? _nf2.format(lon) : //
+
+                                             _mapZoomLevel > minZoomLevel_Digit_1 ? _nf1.format(lon) : //
+
+                                                   _nf0.format(lon);
+
+         _infoWidget.setText(
+               NLS.bind(
+                     Messages.statusLine_mapInfo_data,
+                     new Object[] {
+                           latText,
+                           lonText,
+                           Integer.toString(_mapZoomLevel + 1) }));
+      }
+   }
 
 }
