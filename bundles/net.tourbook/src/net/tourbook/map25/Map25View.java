31ad982388e73aa556920e1772eaf8cc189039d8
telemaxx <6745th@web.de>
2019-05-04 10:25:21 +0200

    trying avoid "NullPointerException" for mapBookmark in savestate https://sourceforge.net/p/mytourbook/discussion/622811/thread/92d53186ff/?limit=25#67c0

--- bundles/net.tourbook/src/net/tourbook/map25/Map25View.java
+++ bundles/net.tourbook/src/net/tourbook/map25/Map25View.java
@@ -32,6 +32,7 @@ import net.tourbook.chart.Chart;
 import net.tourbook.chart.ChartDataModel;
 import net.tourbook.chart.SelectionChartInfo;
 import net.tourbook.chart.SelectionChartXSliderPosition;
+import net.tourbook.common.UI;
 import net.tourbook.common.tooltip.ActionToolbarSlideout;
 import net.tourbook.common.tooltip.ICloseOpenedDialogs;
 import net.tourbook.common.tooltip.IOpeningDialog;
@@ -1424,7 +1425,14 @@ public class Map25View extends ViewPart implements IMapBookmarks, ICloseOpenedDi
       // other layers
       _mapApp.getLayer_BaseMap().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_BASE_MAP_VISIBLE, true));
       _mapApp.getLayer_Building().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_BUILDING_VISIBLE, true));
-      _mapApp.getLayer_MapBookmark().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_BOOKMARK_VISIBLE, true));
+      
+      // mapbookmarks need extra attention to avoid "NullPointerException". see https://sourceforge.net/p/mytourbook/discussion/622811/thread/92d53186ff/?limit=25#67c0
+      if (_mapApp.getLayer_MapBookmark() != null) {
+         _mapApp.getLayer_MapBookmark().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_BOOKMARK_VISIBLE, true));
+      } else { //should never happen
+         System.out.println(UI.timeStamp() + " ### Map25View restoreState(): _mapApp.getLayer_MapBookmark() returns null. should not happen");//$NON-NLS-1$
+      }
+      
       //_mapApp.getLayer_S3DB().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_S3DB_VISIBLE, true));
 
       _mapApp.getLayer_Label().setEnabled(Util.getStateBoolean(_state, STATE_IS_LAYER_LABEL_VISIBLE, true));
@@ -1449,7 +1457,7 @@ public class Map25View extends ViewPart implements IMapBookmarks, ICloseOpenedDi
 
       _state.put(STATE_IS_LAYER_BASE_MAP_VISIBLE, _mapApp.getLayer_BaseMap().isEnabled());
       _state.put(STATE_IS_LAYER_BUILDING_VISIBLE, _mapApp.getLayer_Building().isEnabled());
-      _state.put(STATE_IS_LAYER_BOOKMARK_VISIBLE, _mapApp.getLayer_MapBookmark().isEnabled());
+      //_state.put(STATE_IS_LAYER_BOOKMARK_VISIBLE, _mapApp.getLayer_MapBookmark().isEnabled());
       //_state.put(STATE_IS_LAYER_S3DB_VISIBLE, _mapApp.getLayer_S3DB().isEnabled());
       _state.put(STATE_IS_LAYER_LABEL_VISIBLE, _mapApp.getLayer_Label().isEnabled());
       _state.put(STATE_IS_LAYER_MARKER_VISIBLE, _mapApp.getLayer_TourMarker().isEnabled());
@@ -1457,6 +1465,14 @@ public class Map25View extends ViewPart implements IMapBookmarks, ICloseOpenedDi
       _state.put(STATE_IS_LAYER_TOUR_VISIBLE, _mapApp.getLayer_Tour().isEnabled());
       _state.put(STATE_IS_LAYER_SCALE_BAR_VISIBLE, _mapApp.getLayer_ScaleBar().isEnabled());
 
+      // mapbookmarks need extra attention to avoid "NullPointerException". see https://sourceforge.net/p/mytourbook/discussion/622811/thread/92d53186ff/?limit=25#67c0
+      if (_mapApp.getLayer_MapBookmark() != null) {
+         _state.put(STATE_IS_LAYER_BOOKMARK_VISIBLE, _mapApp.getLayer_MapBookmark().isEnabled());
+      } else {  // should never happen
+         _state.put(STATE_IS_LAYER_BOOKMARK_VISIBLE, false);
+         System.out.println(UI.timeStamp() + " ### Map25View saveState(): _mapApp.getLayer_MapBookmark() returns null. should not happen");//$NON-NLS-1$
+      }
+           
       // hillshading layer
       _state.put(STATE_IS_LAYER_HILLSHADING_VISIBLE, _mapApp.getLayer_HillShading().isEnabled());
       _state.put(STATE_LAYER_HILLSHADING_OPACITY, _mapApp.getLayer_HillShading_Opacity());
