26d99e5e11054ec89970c618595008f879dd1666
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-08-05 23:47:15 +0000

    Better comments

--- bundles/net.tourbook.device.garmin.fit/src/net/tourbook/device/garmin/fit/listeners/MesgListener_Record.java
+++ bundles/net.tourbook.device.garmin.fit/src/net/tourbook/device/garmin/fit/listeners/MesgListener_Record.java
@@ -266,13 +266,13 @@ public class MesgListener_Record extends AbstractMesgListener implements RecordM
 
       /**
        * Running dynamics data <code>
-      
+
       //	|| fieldName.equals("stance_time") //				     253.0  ms
       //	|| fieldName.equals("stance_time_balance") //		   51.31 percent
       //	|| fieldName.equals("step_length") //				    1526.0  mm
       // || fieldName.equals("vertical_oscillation") //       105.2  mm          //$NON-NLS-1$
       // || fieldName.equals("vertical_ratio") //               8.96 percent     //$NON-NLS-1$
-      
+
        * </code>
        */
       final Float stanceTime = mesg.getStanceTime();
@@ -352,8 +352,8 @@ public class MesgListener_Record extends AbstractMesgListener implements RecordM
          case DEV_FIELD_NAME__POWER:
          case DEV_FIELD_NAME__RP_POWER:
 
-            //If both Stryd ("Power") and Garmin RD Pod Power data "RP_Power"
-            //are present, we only keep the Stryd data
+            //If both Stryd ("Power") and Garmin RD Pod Power data ("RP_Power")
+            //are present, we skip the Garmin data and only keep the Stryd data
             if (devField.getName().equals(DEV_FIELD_NAME__RP_POWER) &&
                   StreamSupport.stream(mesg.getDeveloperFields().spliterator(), false)
                         .anyMatch(developerField -> developerField.getName().equals(DEV_FIELD_NAME__POWER))) {
