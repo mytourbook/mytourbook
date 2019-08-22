da8a882d17f8ea576b267a28ac322d93ca10c2ee
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-08-22 20:18:15 +0000

    WIP

--- bundles/net.tourbook/src/net/tourbook/tour/ActionDeleteMarkerDialog.java
+++ bundles/net.tourbook/src/net/tourbook/tour/ActionDeleteMarkerDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2014  Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2019  Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -30,44 +30,49 @@ import org.eclipse.jface.action.Action;
 
 public class ActionDeleteMarkerDialog extends Action {
 
-	private ITourProvider	_tourProvider;
-	private TourMarker		_tourMarker;
-	private boolean			_isSaveTour;
+   private ITourProvider _tourProvider;
+   private TourMarker    _tourMarker;
+   private boolean       _isSaveTour;
 
-	/**
-	 * @param tourProvider
-	 * @param isSaveTour
-	 *            when <code>true</code> the tour will be saved when the marker dialog is closed
-	 */
-	public ActionDeleteMarkerDialog(final ITourProvider tourProvider, final boolean isSaveTour) {
+   /**
+    * @param tourProvider
+    * @param isSaveTour
+    *           when <code>true</code> the tour will be saved when the marker dialog is closed
+    */
+   public ActionDeleteMarkerDialog(final ITourProvider tourProvider, final boolean isSaveTour) {
 
-		_tourProvider = tourProvider;
-		_isSaveTour = isSaveTour;
+      _tourProvider = tourProvider;
+      _isSaveTour = isSaveTour;
 
-      setText("");//Messages.app_action_edit_tour_marker);
-		setImageDescriptor(TourbookPlugin.getImageDescriptor(Messages.Image__edit_tour_marker));
-		setDisabledImageDescriptor(TourbookPlugin.getImageDescriptor(Messages.Image__edit_tour_marker_disabled));
+      setText("Delete marker(s)...");//Messages.app_action_edit_tour_marker);
+      setImageDescriptor(TourbookPlugin.getImageDescriptor(Messages.Image__edit_tour_marker));
+      setDisabledImageDescriptor(TourbookPlugin.getImageDescriptor(Messages.Image__edit_tour_marker_disabled));
 
-		setEnabled(false);
-	}
+      setEnabled(false);
+   }
 
-	public static void doAction(final ITourProvider tourProvider,
-								final boolean isSaveTour,
-								final TourMarker selectedTourMarker) {
+   public static void doAction(final ITourProvider tourProvider,
+                               final boolean isSaveTour,
+                               final TourMarker selectedTourMarker) {
 
-		final ArrayList<TourData> selectedTours = tourProvider.getSelectedTours();
+      final ArrayList<TourData> selectedTours = tourProvider.getSelectedTours();
 
-		// check if one tour is selected
-		if (selectedTours == null || selectedTours.size() != 1 || selectedTours.get(0) == null) {
-			return;
-		}
+      // check if one tour is selected
+      if (selectedTours == null || selectedTours.size() != 1 || selectedTours.get(0) == null) {
+         return;
+      }
 
-		final TourData tourData = selectedTours.get(0);
+      //TODO SEVERAL MARKERS CAN BE SELECTED. TAKE CARE OF THAT CASE.
 
-		if (tourData.isManualTour()) {
-			// a manually created tour do not have time slices -> no markers
-			return;
-		}
+      // Put an (s) inthe text. Supprimer le(s) marqueur(s)
+      // or can the translation be changed depending if several markers ????
+
+      final TourData tourData = selectedTours.get(0);
+
+      if (tourData.isManualTour()) {
+         // a manually created tour do not have time slices -> no markers
+         return;
+      }
 
       final Set<TourMarker> _originalTourMarkers = tourData.getTourMarkers();
       final Iterator<TourMarker> it = _originalTourMarkers.iterator();
@@ -86,21 +91,20 @@ public class ActionDeleteMarkerDialog extends Action {
 
       tourData.setTourMarkers(_newTourMarkers);
 
-			if (isSaveTour) {
+      if (isSaveTour) {
 
-				TourManager.saveModifiedTours(selectedTours);
-
+         TourManager.saveModifiedTours(selectedTours);
       }
 
-	}
+   }
 
-	@Override
-	public void run() {
-		doAction(_tourProvider, _isSaveTour, _tourMarker);
-	}
+   @Override
+   public void run() {
+      doAction(_tourProvider, _isSaveTour, _tourMarker);
+   }
 
-	public void setTourMarker(final TourMarker tourMarker) {
-		_tourMarker = tourMarker;
-	}
+   public void setTourMarker(final TourMarker tourMarker) {
+      _tourMarker = tourMarker;
+   }
 
 }
