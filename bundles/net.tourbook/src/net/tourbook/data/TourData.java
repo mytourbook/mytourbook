cca163cfb0880a04e5f994cc4c7d2e8cec050853
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-10-04 20:46:26 +0000

    Reverting what eclipse did

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -143,13 +143,13 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
     *   places   degrees      DMS                  qualitative scale that           N/S or E/W      E/W at         E/W at       E/W at
     *                                              can be identified                at equator      23N/S          45N/S        67N/S
     *
-    *    0       1.0          1Â° 00' 0?       country or large region            111.32   km   102.47   km     78.71  km    43.496  km
-    *    1       0.1          0Â° 06' 0?       large city or district              11.132  km    10.247  km      7.871 km     4.3496 km
-    *    2       0.01         0Â° 00' 36?      town or village                      1.1132 km     1.0247 km    787.1   m    434.96   m
-    *    3       0.001        0Â° 00' 3.6?     neighborhood, street               111.32   m    102.47   m      78.71  m     43.496  m
-    *    4       0.0001       0Â° 00' 0.36?    individual street, land parcel      11.132  m     10.247  m       7.871 m      4.3496 m
-    *    5       0.00001      0Â° 00' 0.036?   individual trees                     1.1132 m      1.0247 m     787.1   mm   434.96   mm
-    *    6       0.000001     0Â° 00' 0.0036?  individual humans                  111.32   mm   102.47   mm     78.71  mm    43.496  mm
+    *    0       1.0          1° 00' 0?       country or large region            111.32   km   102.47   km     78.71  km    43.496  km
+    *    1       0.1          0° 06' 0?       large city or district              11.132  km    10.247  km      7.871 km     4.3496 km
+    *    2       0.01         0° 00' 36?      town or village                      1.1132 km     1.0247 km    787.1   m    434.96   m
+    *    3       0.001        0° 00' 3.6?     neighborhood, street               111.32   m    102.47   m      78.71  m     43.496  m
+    *    4       0.0001       0° 00' 0.36?    individual street, land parcel      11.132  m     10.247  m       7.871 m      4.3496 m
+    *    5       0.00001      0° 00' 0.036?   individual trees                     1.1132 m      1.0247 m     787.1   mm   434.96   mm
+    *    6       0.000001     0° 00' 0.0036?  individual humans                  111.32   mm   102.47   mm     78.71  mm    43.496  mm
     *
     * https://en.wikipedia.org/wiki/Decimal_degrees
     *
@@ -565,7 +565,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
    // ############################################# TRAINING #############################################
 
    /**
-    * Naming variants from JesÃºs PÃ©rez
+    * Naming variants from Jesús Pérez
     * <ul>
     * <li>Impact of training</li>
     * <li>Impact of effort</li>
@@ -1144,7 +1144,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
 
    /**
     * Contains the rough geo parts of the tour or <code>null</code> when geo data are not available. A
-    * grid square is an integer of lat + 90Â° and lon + 180Â° multiplied by 100 (1570 m)
+    * grid square is an integer of lat + 90° and lon + 180° multiplied by 100 (1570 m)
     *
     * <pre>
 
