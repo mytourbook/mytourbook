8dcd58a0cfa8fcd49a561889916b5b744055434c
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-10-13 22:27:35 -0600

    Reverting what Eclipse did

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -134,7 +134,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
    public static final int             DB_LENGTH_WEATHER                 = 1000;
    public static final int             DB_LENGTH_WEATHER_CLOUDS          = 255;
 
-   public static final int             DB_LENGTH_POWER_DATA_SOURCE       = 255;
+    public static final int             DB_LENGTH_POWER_DATA_SOURCE       = 255;
 
    /**
     * <pre>
@@ -143,13 +143,13 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
     *   places   degrees      DMS                  qualitative scale that           N/S or E/W      E/W at         E/W at       E/W at
     *                                              can be identified                at equator      23N/S          45N/S        67N/S
     *
-    *    0       1.0          1� 00' 0?       country or large region            111.32   km   102.47   km     78.71  km    43.496  km
-    *    1       0.1          0� 06' 0?       large city or district              11.132  km    10.247  km      7.871 km     4.3496 km
-    *    2       0.01         0� 00' 36?      town or village                      1.1132 km     1.0247 km    787.1   m    434.96   m
-    *    3       0.001        0� 00' 3.6?     neighborhood, street               111.32   m    102.47   m      78.71  m     43.496  m
-    *    4       0.0001       0� 00' 0.36?    individual street, land parcel      11.132  m     10.247  m       7.871 m      4.3496 m
-    *    5       0.00001      0� 00' 0.036?   individual trees                     1.1132 m      1.0247 m     787.1   mm   434.96   mm
-    *    6       0.000001     0� 00' 0.0036?  individual humans                  111.32   mm   102.47   mm     78.71  mm    43.496  mm
+    *    0       1.0          1° 00' 0?       country or large region            111.32   km   102.47   km     78.71  km    43.496  km
+    *    1       0.1          0° 06' 0?       large city or district              11.132  km    10.247  km      7.871 km     4.3496 km
+    *    2       0.01         0° 00' 36?      town or village                      1.1132 km     1.0247 km    787.1   m    434.96   m
+    *    3       0.001        0° 00' 3.6?     neighborhood, street               111.32   m    102.47   m      78.71  m     43.496  m
+    *    4       0.0001       0° 00' 0.36?    individual street, land parcel      11.132  m     10.247  m       7.871 m      4.3496 m
+    *    5       0.00001      0° 00' 0.036?   individual trees                     1.1132 m      1.0247 m     787.1   mm   434.96   mm
+    *    6       0.000001     0° 00' 0.0036?  individual humans                  111.32   mm   102.47   mm     78.71  mm    43.496  mm
     *
     * https://en.wikipedia.org/wiki/Decimal_degrees
     *
@@ -164,6 +164,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
     * </pre>
     */
    public static final double          MAX_GEO_DIFF                      = 0.0001;
+
 
    public static final double          NORMALIZED_LATITUDE_OFFSET        = 90.0;
    public static final int             NORMALIZED_LATITUDE_OFFSET_E2     = 9000;
@@ -564,7 +565,8 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
    // ############################################# TRAINING #############################################
 
    /**
-    * Naming variants from Jes�s P�rez
+    * Naming variants from Jesús Pérez
+
     * <ul>
     * <li>Impact of training</li>
     * <li>Impact of effort</li>
