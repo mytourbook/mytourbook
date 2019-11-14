5683724949b0d2e461dbef419d67568a9ef97e96
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-11-13 20:45:43 -0700

    Headers

--- bundles/net.tourbook.device/src/net/tourbook/device/suunto/Suunto9DeviceDataReader.java
+++ bundles/net.tourbook.device/src/net/tourbook/device/suunto/Suunto9DeviceDataReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2018, 2019 Frédéric Bard
+ * Copyright (C) 2018, 2019 Frédéric Bard and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
