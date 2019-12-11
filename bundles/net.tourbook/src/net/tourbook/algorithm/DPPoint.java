14349f3e9a04e5b77cb3c101e095953550da1e11
FJBDev <25235665+FJBDev@users.noreply.github.com>
2019-12-11 20:31:31 +0000

    Reverting for less commit "noise"

--- bundles/net.tourbook/src/net/tourbook/algorithm/DPPoint.java
+++ bundles/net.tourbook/src/net/tourbook/algorithm/DPPoint.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2019 Wolfgang Schramm and Contributors
- *
+ * Copyright (C) 2005, 2015 Wolfgang Schramm and Contributors
+ * 
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
@@ -17,59 +17,59 @@ package net.tourbook.algorithm;
 
 public class DPPoint {
 
-   /**
-    * x coordinate
-    */
-   public double x;
+	/**
+	 * x coordinate
+	 */
+	public double	x;
 
-   /**
-    * y coordinate
-    */
-   public double y;
+	/**
+	 * y coordinate
+	 */
+	public double	y;
 
-   /**
-    * Index of this point in the original time serie
-    */
-   public int    serieIndex;
+	/**
+	 * Index of this point in the original time serie
+	 */
+	public int		serieIndex;
 
-   public DPPoint(final double x, final double y, final int serieIndex) {
+	public DPPoint(final double x, final double y, final int serieIndex) {
 
-      this.x = x;
-      this.y = y;
+		this.x = x;
+		this.y = y;
 
-      this.serieIndex = serieIndex;
-   }
+		this.serieIndex = serieIndex;
+	}
 
-   public DPPoint add(final Vector vector) {
-      return new DPPoint(x + vector.x, y + vector.y, serieIndex);
-   }
+	public DPPoint add(final Vector vector) {
+		return new DPPoint(x + vector.x, y + vector.y, serieIndex);
+	}
 
-   /**
-    * Squared Distance
-    *
-    * @param point
-    * @return
-    */
-   public double d2(final DPPoint point) {
-      final Vector difference = diff(point);
-      return difference.dot(difference);
-   }
+	/**
+	 * Squared Distance
+	 * 
+	 * @param point
+	 * @return
+	 */
+	public double d2(final DPPoint point) {
+		final Vector difference = diff(point);
+		return difference.dot(difference);
+	}
 
-   /**
-    * Difference
-    *
-    * @param point
-    * @return
-    */
-   public Vector diff(final DPPoint point) {
-      return new Vector(x - point.x, y - point.y, serieIndex);
-   }
+	/**
+	 * Difference
+	 * 
+	 * @param point
+	 * @return
+	 */
+	public Vector diff(final DPPoint point) {
+		return new Vector(x - point.x, y - point.y, serieIndex);
+	}
 
-   @Override
-   public String toString() {
-      return "DPPoint " //$NON-NLS-1$
-            + String.format("i:%-5d   x=%-5.0f   y=%-5.1f", serieIndex, x, y) //$NON-NLS-1$
-            + "\n"; //$NON-NLS-1$
-   }
+	@Override
+	public String toString() {
+		return "DPPoint " // //$NON-NLS-1$
+				+ String.format("i:%-5d   x=%-5.0f   y=%-5.1f", serieIndex, x, y) //$NON-NLS-1$
+				+ "\n"; //$NON-NLS-1$
+	}
 
 }
