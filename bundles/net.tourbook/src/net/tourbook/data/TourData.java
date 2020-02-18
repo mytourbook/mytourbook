mytourbook/mytourbook

commit 6d3c8694b5b1a25d597dd44e8605fbaed4ed91ca

Tour Marker-Added the following columns Elevation Gain Difference, Elevation Loss Difference, Pace Difference (#10)
* WIP here

* 20.X as of 20200110 (#9)

* French translation

* French typo fix

* fixed issue: column width are reset when tags are saved

* Fixed issue in statistics: Tour type in tooltip is wrong for the first item

* java template update: removed parentheses

* Show tooltip for the bottom bars, the hitbox for the horizontal slider starts now below the horizontal line (for a bar chart)

* reported map2 bug

* Adding the cadence zones time percentages in the weekly summary of the
calendar

* removed useless code

* Unique function to generate the string containing the percentages of
time spent in each cadence zone

* French translation fix

* fixed bikerWeight value in gpx export

had to use getBodyWeight() method because accessing the field didn't work -> very strange

* fixed NPE when importing MT export files

* GPX import: Set default to absolute distance that the defaults for export/import are the same

* readme update

* MTB cheatsheet

* MT cheatsheet

* Revert "MTB cheatsheet"

This reverts commit f624320393a29af5ceeab02335daa66001df13ad.

* Adding TODO for next version (Linux tile saving issue)

* Per review suggestion

* FIT import: Starting marker label from 1 instead of 0

* WIP Issues in the General men values when switching from metric to
implerial and vice versa

* WIP Elevation gain and loss can be wrong at times

* Using a java library but obtaining similar results

* Removing the series reducer

* Clean up

* WIP Fixing a bug where the metric radio buttons disappeared

* Not much progress

* Fixed the UI for the measurement system tab

* Cleanup

* Reverting for less commit "noise"

* Conversion fix

* Typo

* Updated Readme

* dutch translation

* Disabling the "Edit Marker" in the slice editor view if an existing
marker hasn't been selected

bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -2405,6 +2405,29 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
     */
    public boolean computeAltitudeUpDown() {
 
+      if (altitudeSerie == null) {
+         return false;
+      }
+
+      final AltitudeUpDown altiUpDown = computeAltitudeUpDown(0, altitudeSerie.length - 1);
+
+      return altiUpDown != null;
+   }
+
+   public AltitudeUpDown computeAltitudeUpDown(final ArrayList<AltitudeUpDownSegment> segmentSerieIndexParameter,
+                                               final float selectedMinAltiDiff) {
+
+      return computeAltitudeUpDown_30_Algorithm_9_08(segmentSerieIndexParameter, selectedMinAltiDiff);
+   }
+
+   /**
+    * Computes and sets the altitude up/down values into {@link TourData}
+    *
+    * @return Returns an <code>AltitudeUpDown</code> when altitude was computed otherwise
+    *         <code>null</code>
+    */
+   public AltitudeUpDown computeAltitudeUpDown(final int startIndex, final int endIndex) {
+
       float prefDPTolerance;
 
       if (_isImportedMTTour) {
@@ -2419,9 +2442,9 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
 
          // DP needs distance
 
-         altiUpDown = computeAltitudeUpDown_20_Algorithm_DP(prefDPTolerance);
+         altiUpDown = computeAltitudeUpDown_20_Algorithm_DP(prefDPTolerance, startIndex, endIndex - 1);
 
-         // keep this value to see in the UI (toursegmenter) the value and how it is computed
+         // keep this value to see in the UI (tour segmenter) the value and how it is computed
          dpTolerance = (short) (prefDPTolerance * 10);
 
       } else {
@@ -2429,39 +2452,39 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
          altiUpDown = computeAltitudeUpDown_30_Algorithm_9_08(null, prefDPTolerance);
       }
 
-      if (altiUpDown == null) {
-         return false;
+      if (altiUpDown != null) {
+         setTourAltUp(altiUpDown.altitudeUp);
+         setTourAltDown(altiUpDown.altitudeDown);
       }
 
-      setTourAltUp(altiUpDown.altitudeUp);
-      setTourAltDown(altiUpDown.altitudeDown);
-
-      return true;
-   }
-
-   public AltitudeUpDown computeAltitudeUpDown(final ArrayList<AltitudeUpDownSegment> segmentSerieIndexParameter,
-                                               final float selectedMinAltiDiff) {
-
-      return computeAltitudeUpDown_30_Algorithm_9_08(segmentSerieIndexParameter, selectedMinAltiDiff);
+      return altiUpDown;
    }
 
    /**
-    * Compute altitude up/down with Douglas Peuker algorithm.
+    * Compute altitude up/down with Douglas Peucker algorithm.
     *
     * @param dpTolerance
+    *           The Douglas-Peucker tolerance value
+    * @param startIndex
+    *           The start of the section for which to compute the elevation gain/loss
+    * @param endIndex
+    *           The end of the section for which to compute the elevation gain/loss
     * @return Returns <code>null</code> when altitude up/down cannot be computed
     */
-   private AltitudeUpDown computeAltitudeUpDown_20_Algorithm_DP(final float dpTolerance) {
+   private AltitudeUpDown computeAltitudeUpDown_20_Algorithm_DP(final float dpTolerance, final int startIndex, final int endIndex) {
 
       // check if all necessary data are available
-      if (altitudeSerie == null || altitudeSerie.length < 2) {
+      if (altitudeSerie == null || altitudeSerie.length < 2 ||
+            startIndex > altitudeSerie.length || endIndex >= altitudeSerie.length) {
          return null;
       }
 
       // convert data series into DP points
-      final DPPoint dpPoints[] = new DPPoint[distanceSerie.length];
-      for (int serieIndex = 0; serieIndex < dpPoints.length; serieIndex++) {
-         dpPoints[serieIndex] = new DPPoint(distanceSerie[serieIndex], altitudeSerie[serieIndex], serieIndex);
+      final DPPoint dpPoints[] = new DPPoint[endIndex - startIndex];
+      int dpPointsIndex = 0;
+      for (int serieIndex = startIndex; serieIndex < endIndex; serieIndex++) {
+         dpPoints[dpPointsIndex] = new DPPoint(distanceSerie[serieIndex], altitudeSerie[serieIndex], serieIndex);
+         dpPointsIndex++;
       }
 
       int[] forcedIndices = null;
@@ -2474,7 +2497,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
       float altitudeUpTotal = 0;
       float altitudeDownTotal = 0;
 
-      float prevAltitude = altitudeSerie[0];
+      float prevAltitude = altitudeSerie[startIndex];
 
       /*
        * Get altitude up/down from the tour altitude values which are found by DP
@@ -6728,7 +6751,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
    }
 
    /**
-    * Computes break time between start and end index when and when a break occures
+    * Computes break time between start and end index when and when a break occurs
     *
     * @param startIndex
     * @param endIndex

bundles/net.tourbook/src/net/tourbook/data/AltitudeUpDown.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2011  Wolfgang Schramm and Contributors
- * 
+ * Copyright (C) 2005, 2020  Wolfgang Schramm and Contributors
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
@@ -17,12 +17,20 @@ package net.tourbook.data;
 
 public class AltitudeUpDown {
 
-	float	altitudeUp;
-	float	altitudeDown;
+   float altitudeUp;
+   float altitudeDown;
 
-	public AltitudeUpDown(final float altiUp, final float altiDown) {
-		altitudeUp = altiUp;
-		altitudeDown = altiDown;
-	}
+   public AltitudeUpDown(final float altiUp, final float altiDown) {
+      altitudeUp = altiUp;
+      altitudeDown = altiDown;
+   }
+
+   public float getAltitudeDown() {
+      return altitudeDown;
+   }
+
+   public float getAltitudeUp() {
+      return altitudeUp;
+   }
 
 }

bundles/net.tourbook/src/net/tourbook/ui/Messages.java
@@ -210,6 +210,10 @@ public class Messages extends NLS {
    public static String        ColumnFactory_DPTolerance_Header;
    public static String        ColumnFactory_DPTolerance_Label;
    public static String        ColumnFactory_DPTolerance_Tooltip;
+   public static String        ColumnFactory_ElevationGainDelta_Label;
+   public static String        ColumnFactory_ElevationLossDelta_Label;
+   public static String        ColumnFactory_ElevationGainDelta_Tooltip;
+   public static String        ColumnFactory_ElevationLossDelta_Tooltip;
    public static String        ColumnFactory_GearFrontShiftCount_Header;
    public static String        ColumnFactory_GearFrontShiftCount_Label;
    public static String        ColumnFactory_GearFrontShiftCount_Tooltip;
@@ -249,6 +253,8 @@ public class Messages extends NLS {
    public static String        ColumnFactory_NumberOfTours_Header;
    public static String        ColumnFactory_NumberOfTours_Label;
    public static String        ColumnFactory_NumberOfTours_Tooltip;
+   public static String        ColumnFactory_PaceDelta_Label;
+   public static String        ColumnFactory_PaceDelta_Tooltip;
    public static String        ColumnFactory_PhotoTimeAdjustment_Header;
    public static String        ColumnFactory_PhotoTimeAdjustment_Label;
    public static String        ColumnFactory_PhotoTimeAdjustment_Tooltip;

bundles/net.tourbook/src/net/tourbook/ui/TableColumnFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2018 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2020 Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -60,7 +60,10 @@ public abstract class TableColumnFactory {
    public static final TableColumnFactory DEVICE_NAME;
    public static final TableColumnFactory DEVICE_PROFILE;
 
+   public static final TableColumnFactory MARKER_ALTITUDE_ELEVATIONGAINDELTA;
+   public static final TableColumnFactory MARKER_ALTITUDE_ELEVATIONLOSSDELTA;
    public static final TableColumnFactory MARKER_MAP_VISIBLE;
+   public static final TableColumnFactory MARKER_PACE_DELTA;
    public static final TableColumnFactory MARKER_SERIE_INDEX;
    public static final TableColumnFactory MARKER_TIME_DELTA;
    public static final TableColumnFactory MARKER_URL;
@@ -753,6 +756,56 @@ public abstract class TableColumnFactory {
        * Marker
        */
 
+      MARKER_ALTITUDE_ELEVATIONGAINDELTA = new TableColumnFactory() {
+
+         @Override
+         public TableColumnDefinition createColumn(final ColumnManager columnManager,
+                                                   final PixelConverter pixelConverter) {
+
+            final TableColumnDefinition colDef = new TableColumnDefinition(
+                  columnManager,
+                  "MARKER_ALTITUDE_ELEVATIONGAINDELTA", //$NON-NLS-1$
+                  SWT.TRAIL);
+
+            final String unitLabel = UI.SYMBOL_DIFFERENCE_WITH_SPACE + UI.UNIT_LABEL_ALTITUDE + UI.SPACE + UI.SYMBOL_ARROW_UP;
+
+            colDef.setColumnCategory(Messages.ColumnFactory_Category_Altitude);
+            colDef.setColumnLabel(Messages.ColumnFactory_ElevationGainDelta_Label);
+            colDef.setColumnHeaderText(unitLabel);
+            colDef.setColumnHeaderToolTipText(Messages.ColumnFactory_ElevationGainDelta_Tooltip);
+            colDef.setColumnUnit(unitLabel);
+
+            colDef.setDefaultColumnWidth(pixelConverter.convertWidthInCharsToPixels(12));
+
+            return colDef;
+         }
+      };
+
+      MARKER_ALTITUDE_ELEVATIONLOSSDELTA = new TableColumnFactory() {
+
+         @Override
+         public TableColumnDefinition createColumn(final ColumnManager columnManager,
+                                                   final PixelConverter pixelConverter) {
+
+            final TableColumnDefinition colDef = new TableColumnDefinition(
+                  columnManager,
+                  "MARKER_ALTITUDE_ELEVATIONLOSSDELTA", //$NON-NLS-1$
+                  SWT.TRAIL);
+
+            final String unitLabel = UI.SYMBOL_DIFFERENCE_WITH_SPACE + UI.UNIT_LABEL_ALTITUDE + UI.SPACE + UI.SYMBOL_ARROW_DOWN;
+
+            colDef.setColumnCategory(Messages.ColumnFactory_Category_Altitude);
+            colDef.setColumnLabel(Messages.ColumnFactory_ElevationLossDelta_Label);
+            colDef.setColumnHeaderText(unitLabel);
+            colDef.setColumnHeaderToolTipText(Messages.ColumnFactory_ElevationLossDelta_Tooltip);
+            colDef.setColumnUnit(unitLabel);
+
+            colDef.setDefaultColumnWidth(pixelConverter.convertWidthInCharsToPixels(12));
+
+            return colDef;
+         }
+      };
+
       MARKER_MAP_VISIBLE = new TableColumnFactory() {
 
          @Override
@@ -772,6 +825,31 @@ public abstract class TableColumnFactory {
 
             colDef.setDefaultColumnWidth(pixelWidth);
             colDef.setColumnWeightData(new ColumnPixelData(pixelWidth, true));
+
+            return colDef;
+         }
+      };
+
+      MARKER_PACE_DELTA = new TableColumnFactory() {
+
+         @Override
+         public TableColumnDefinition createColumn(final ColumnManager columnManager,
+                                                   final PixelConverter pixelConverter) {
+
+            final TableColumnDefinition colDef = new TableColumnDefinition(
+                  columnManager,
+                  "MARKER_PACE_DELTA", //$NON-NLS-1$
+                  SWT.TRAIL);
+
+            final String unitLabel = UI.SYMBOL_DIFFERENCE_WITH_SPACE + UI.UNIT_LABEL_PACE;
+
+            colDef.setColumnCategory(Messages.ColumnFactory_Category_Motion);
+            colDef.setColumnLabel(Messages.ColumnFactory_PaceDelta_Label);
+            colDef.setColumnHeaderText(unitLabel);
+            colDef.setColumnHeaderToolTipText(Messages.ColumnFactory_PaceDelta_Tooltip);
+            colDef.setColumnUnit(unitLabel);
+
+            colDef.setDefaultColumnWidth(pixelConverter.convertWidthInCharsToPixels(12));
 
             return colDef;
          }

bundles/net.tourbook/src/net/tourbook/ui/messages.properties
@@ -51,6 +51,12 @@ ColumnFactory_Diff_Header = ||
 ColumnFactory_DistanceDelta_Label   = Distance Difference
 ColumnFactory_DistanceDelta_Tooltip = Distance difference to previous marker.
 
+ColumnFactory_ElevationGainDelta_Label   = Elevation Gain Difference
+ColumnFactory_ElevationGainDelta_Tooltip = Total elevation gain difference from previous marker.
+
+ColumnFactory_ElevationLossDelta_Label   = Elevation Loss Difference
+ColumnFactory_ElevationLossDelta_Tooltip = Total elevation loss from previous marker.
+
 ColumnFactory_Elevation_AvgChange_Label   = Elevation - Average change
 ColumnFactory_Elevation_AvgChange_Tooltip = Average elevation change per unit of distance
 
@@ -109,6 +115,9 @@ ColumnFactory_NumberOfTourPhotos_Tooltip = Number of photos which are save in th
 ColumnFactory_NumberOfTours_Header  = Tours
 ColumnFactory_NumberOfTours_Label   = Number of tours
 ColumnFactory_NumberOfTours_Tooltip = Number of tours
+
+ColumnFactory_PaceDelta_Label   = Pace difference
+ColumnFactory_PaceDelta_Tooltip = Average pace from previous marker
 
 ColumnFactory_PhotoTimeAdjustment_Header  = Adj. time
 ColumnFactory_PhotoTimeAdjustment_Label   = Time adjustment

bundles/net.tourbook/src/net/tourbook/ui/messages_fr.properties
@@ -51,6 +51,12 @@ ColumnFactory_Diff_Header = ||
 ColumnFactory_DistanceDelta_Label   = Distance de s\u00E9paration
 ColumnFactory_DistanceDelta_Tooltip = Distance de s\u00E9paration avec le marqueur pr\u00E9c\u00E9dent.
 
+ColumnFactory_ElevationGainDelta_Label   = Diff\u00E9rence de d\u00E9nivel\u00E9 positif
+ColumnFactory_ElevationGainDelta_Tooltip = D\u00E9nivel\u00E9 positif total entre le marqueur courant et le marqueur pr\u00E9c\u00E9dent.
+
+ColumnFactory_ElevationLossDelta_Label   = Diff\u00E9rence de d\u00E9nivel\u00E9 n\u00E9gatif
+ColumnFactory_ElevationLossDelta_Tooltip = D\u00E9nivel\u00E9 n\u00E9gatif total entre le marqueur courant et le marqueur pr\u00E9c\u00E9dent.
+
 ColumnFactory_Elevation_AvgChange_Label   = Variation moyenne de d\u00E9nivel\u00E9
 ColumnFactory_Elevation_AvgChange_Tooltip = Variation moyenne de d\u00E9nivel\u00E9 par unit\u00E9 de distance
 
@@ -109,6 +115,9 @@ ColumnFactory_NumberOfTourPhotos_Tooltip = Nombre de photos enregistr\u00E9es da
 ColumnFactory_NumberOfTours_Header  = Parcours
 ColumnFactory_NumberOfTours_Label   = Nombre de parcours
 ColumnFactory_NumberOfTours_Tooltip = Nombre de parcours
+
+ColumnFactory_PaceDelta_Label   = Diff\u00E9rence d'allure
+ColumnFactory_PaceDelta_Tooltip = Diff\u00E9rence d'allure entre le marqueur courant et le marqueur pr\u00E9c\u00E9dent.
 
 ColumnFactory_PhotoTimeAdjustment_Header  = Ajustement de l'heure
 ColumnFactory_PhotoTimeAdjustment_Label   = Ajustement de l'heure

bundles/net.tourbook/src/net/tourbook/ui/views/tourMarker/TourMarkerView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2019 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2020 Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -17,7 +17,10 @@ package net.tourbook.ui.views.tourMarker;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
 
 import net.tourbook.Messages;
 import net.tourbook.application.TourbookPlugin;
@@ -27,6 +30,7 @@ import net.tourbook.common.util.ColumnManager;
 import net.tourbook.common.util.IContextMenuProvider;
 import net.tourbook.common.util.ITourViewer;
 import net.tourbook.common.util.PostSelectionProvider;
+import net.tourbook.data.AltitudeUpDown;
 import net.tourbook.data.TourData;
 import net.tourbook.data.TourMarker;
 import net.tourbook.database.TourDatabase;
@@ -568,12 +572,81 @@ public class TourMarkerView extends ViewPart implements ITourProvider, ITourView
 
       defineColumn_Motion_Distance();
       defineColumn_Motion_DistanceDelta();
+      defineColumn_Motion_PaceDelta();
+
+      defineColumn_Altitude_ElevationGainDelta();
+      defineColumn_Altitude_ElevationLossDelta();
 
       defineColumn_Waypoint_Name();
       defineColumn_Waypoint_Description();
       defineColumn_Marker_Url();
 
       defineColumn_Data_SerieIndex();
+   }
+
+   /**
+    * Column: Elevation gain
+    */
+   private void defineColumn_Altitude_ElevationGainDelta() {
+
+      final ColumnDefinition colDef = TableColumnFactory.MARKER_ALTITUDE_ELEVATIONGAINDELTA.createColumn(_columnManager, _pc);
+
+      colDef.setLabelProvider(new CellLabelProvider() {
+         @Override
+         public void update(final ViewerCell cell) {
+
+            final ViewerRow lastRow = cell.getViewerRow().getNeighbor(ViewerRow.ABOVE, false);
+            int previousMarkerIndex = 0;
+            if (null != lastRow) {
+               final Object element = lastRow.getElement();
+               if (element instanceof TourMarker) {
+                  previousMarkerIndex = ((TourMarker) element).getSerieIndex();
+               }
+            }
+
+            final int currentMarkerIndex = ((TourMarker) cell.getElement()).getSerieIndex();
+
+            final AltitudeUpDown elevationGainLoss = _tourData.computeAltitudeUpDown(previousMarkerIndex, currentMarkerIndex);
+
+            final double value = elevationGainLoss.getAltitudeUp() / net.tourbook.ui.UI.UNIT_VALUE_ALTITUDE;
+
+            colDef.printValue_0(cell, value);
+         }
+      });
+
+   }
+
+   /**
+    * Column: Elevation loss
+    */
+   private void defineColumn_Altitude_ElevationLossDelta() {
+      final ColumnDefinition colDef = TableColumnFactory.MARKER_ALTITUDE_ELEVATIONLOSSDELTA.createColumn(_columnManager, _pc);
+
+      colDef.setLabelProvider(new CellLabelProvider() {
+         @Override
+         public void update(final ViewerCell cell) {
+
+            final ViewerRow lastRow = cell.getViewerRow().getNeighbor(ViewerRow.ABOVE, false);
+            int previousMarkerIndex = 0;
+            if (null != lastRow) {
+               final Object element = lastRow.getElement();
+               if (element instanceof TourMarker) {
+                  previousMarkerIndex = ((TourMarker) element).getSerieIndex();
+               }
+            }
+
+            final int currentMarkerIndex = ((TourMarker) cell.getElement()).getSerieIndex();
+
+            final AltitudeUpDown elevationGainLoss = _tourData.computeAltitudeUpDown(
+                  previousMarkerIndex,
+                  currentMarkerIndex);
+
+            final double value = elevationGainLoss.getAltitudeDown() / net.tourbook.ui.UI.UNIT_VALUE_ALTITUDE;
+
+            colDef.printValue_0(cell, value);
+         }
+      });
+
    }
 
    /**
@@ -725,6 +798,40 @@ public class TourMarkerView extends ViewPart implements ITourProvider, ITourView
                      / 1000
                      / net.tourbook.ui.UI.UNIT_VALUE_DISTANCE));
             }
+         }
+      });
+   }
+
+   /**
+    * Column: Pace delta
+    */
+   private void defineColumn_Motion_PaceDelta() {
+      final ColumnDefinition colDef = TableColumnFactory.MARKER_PACE_DELTA.createColumn(_columnManager, _pc);
+
+      colDef.setLabelProvider(new CellLabelProvider() {
+         @Override
+         public void update(final ViewerCell cell) {
+
+            final ViewerRow lastRow = cell.getViewerRow().getNeighbor(ViewerRow.ABOVE, false);
+            int previousMarkerIndex = 0;
+            if (null != lastRow) {
+               final Object element = lastRow.getElement();
+               if (element instanceof TourMarker) {
+                  previousMarkerIndex = ((TourMarker) element).getSerieIndex();
+               }
+            }
+
+            final int currentMarkerIndex = ((TourMarker) cell.getElement()).getSerieIndex();
+
+            final float[] seriePace = _tourData.getPaceSerieSeconds();
+            if (seriePace == null) {
+               return;
+            }
+
+            final double[] seriePaceDouble = IntStream.range(previousMarkerIndex, currentMarkerIndex).mapToDouble(i -> seriePace[i]).toArray();
+            final OptionalDouble averagePace = Arrays.stream(seriePaceDouble).average();
+
+            cell.setText(UI.format_mm_ss((long) averagePace.getAsDouble()));
          }
       });
    }

bundles/net.tourbook/src/net/tourbook/ui/views/tourSegmenter/TourSegmenterView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2018 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2020 Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -1427,18 +1427,18 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
 
       final float selectedMinAltiDiff = (float) (_spinnerMinAltitude.getSelection() / 10.0);
 
-      final ArrayList<AltitudeUpDownSegment> tourSegements = new ArrayList<>();
+      final ArrayList<AltitudeUpDownSegment> tourSegments = new ArrayList<>();
 
       // create segment when the altitude up/down is changing
-      _tourData.computeAltitudeUpDown(tourSegements, selectedMinAltiDiff);
+      _tourData.computeAltitudeUpDown(tourSegments, selectedMinAltiDiff);
 
       // convert segment list into array
       int serieIndex = 0;
-      final int segmentLength = tourSegements.size();
+      final int segmentLength = tourSegments.size();
       final int[] segmentSerieIndex = _tourData.segmentSerieIndex = new int[segmentLength];
       final float[] altitudeDiff = _tourData.segmentSerie_Altitude_Diff_Computed = new float[segmentLength];
 
-      for (final AltitudeUpDownSegment altitudeUpDownSegment : tourSegements) {
+      for (final AltitudeUpDownSegment altitudeUpDownSegment : tourSegments) {
 
          segmentSerieIndex[serieIndex] = altitudeUpDownSegment.serieIndex;
          altitudeDiff[serieIndex] = altitudeUpDownSegment.computedAltitudeDiff;
@@ -1733,7 +1733,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
             getTourIndices()).simplify();
 
       /*
-       * copie the data index for the simplified points into the tour data
+       * copies the data index for the simplified points into the tour data
        */
       final int[] segmentSerieIndex = _tourData.segmentSerieIndex = new int[simplePoints.length];
 
@@ -1768,7 +1768,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
             getTourIndices()).simplify();
 
       /*
-       * copie the data index for the simplified points into the tour data
+       * copies the data index for the simplified points into the tour data
        */
       final int[] segmentSerieIndex = _tourData.segmentSerieIndex = new int[simplePoints.length];
 
@@ -1830,7 +1830,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
          final float currentDistance = distanceSerie[serieIndex];
          final float currentSpeed = speedSerie[serieIndex];
 
-         // diffs to the previous time slice
+         // differences with the previous time slice
          final int timeDiff = currentTime - prevTime;
          final float distanceDiff = currentDistance - prevDistance;
 
@@ -1919,7 +1919,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
 
             if (segmentStartIndex == segmentEndIndex) {
 
-               // ignore, this occured
+               // ignore, this occurred
 
             } else {
 
@@ -4170,7 +4170,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
 
          if (tourMarker.isMarkerVisible() == false) {
 
-            // irgnore hidden marker
+            // ignore hidden marker
             continue;
          }
 

commit 09be84ca9875777dd2a5e97ace4458cecdc84f8e

Bugfix : Array index issue when computing elevation gain/loss (regression inserted in my commit 6d3c869)

bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -2442,7 +2442,7 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
 
          // DP needs distance
 
-         altiUpDown = computeAltitudeUpDown_20_Algorithm_DP(prefDPTolerance, startIndex, endIndex - 1);
+         altiUpDown = computeAltitudeUpDown_20_Algorithm_DP(prefDPTolerance, startIndex, endIndex);
 
          // keep this value to see in the UI (tour segmenter) the value and how it is computed
          dpTolerance = (short) (prefDPTolerance * 10);