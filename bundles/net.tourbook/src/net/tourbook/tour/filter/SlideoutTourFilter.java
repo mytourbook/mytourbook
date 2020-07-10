1cede6b7286081db7b051e82aee592b2f899b478
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-07-10 17:56:47 +0000

    Typos

--- bundles/net.tourbook/src/net/tourbook/tour/filter/SlideoutTourFilter.java
+++ bundles/net.tourbook/src/net/tourbook/tour/filter/SlideoutTourFilter.java
@@ -161,7 +161,7 @@ public class SlideoutTourFilter extends AdvancedSlideout {
 
    /**
     * Contains the controls which are displayed in the first column, these controls are used to get
-    * the maximum width and set the first column within the differenct section to the same width.
+    * the maximum width and set the first column within the different section to the same width.
     */
    private final ArrayList<Control>           _firstColumnControls = new ArrayList<>();
 
