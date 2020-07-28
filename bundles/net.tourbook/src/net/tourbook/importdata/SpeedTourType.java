c4ada37c1f55d0540941cd4531add37b2c0e4ab2
Ryan Thomas <7549821+rtdog@users.noreply.github.com>
2020-07-28 16:32:55 -0400

    starting changes

--- bundles/net.tourbook/src/net/tourbook/importdata/SpeedTourType.java
+++ bundles/net.tourbook/src/net/tourbook/importdata/SpeedTourType.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2015 Wolfgang Schramm and Contributors
- * 
+ * Copyright (C) 2005, 2020 Wolfgang Schramm and Contributors
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
@@ -24,15 +24,15 @@ public class SpeedTourType implements Comparable<Object>, Cloneable {
 	 * Average speed for this tour type in km/h.
 	 */
 	public float	avgSpeed;
-
+   public String cadenceType = "SPM";                           // TODO RT: test strongarm
 	public long		tourTypeId	= TourDatabase.ENTITY_IS_NOT_SAVED;
 
 	public SpeedTourType() {}
 
-	public SpeedTourType(final int value) {
-
+   // TODO RT: SpeedTourType
+   public SpeedTourType(final int value, final String cadenceType) {
 		this.avgSpeed = value;
-
+      this.cadenceType = cadenceType;
 	}
 
 	@Override
@@ -42,7 +42,7 @@ public class SpeedTourType implements Comparable<Object>, Cloneable {
 
 		try {
 
-			clonedObject = (SpeedTourType) super.clone();
+         clonedObject = (SpeedTourType) super.clone();
 
 		} catch (final CloneNotSupportedException e) {
 			StatusUtil.log(e);
@@ -51,22 +51,23 @@ public class SpeedTourType implements Comparable<Object>, Cloneable {
 		return clonedObject;
 	}
 
-	@Override
+   @Override
 	public int compareTo(final Object anotherObject) throws ClassCastException {
 
-		final float anotherValue = ((SpeedTourType) anotherObject).avgSpeed;
+      final float anotherValue = ((SpeedTourType) anotherObject).avgSpeed;
 
 		return Float.compare(avgSpeed, anotherValue);
 	}
-
 	@Override
-	public String toString() {
-		return "\nSpeedTourType [" //$NON-NLS-1$
-		//
-				+ ("avgSpeed=" + avgSpeed + ", ") //$NON-NLS-1$ //$NON-NLS-2$
-				+ ("tourTypeId=" + tourTypeId + ", ") //$NON-NLS-1$ //$NON-NLS-2$
-				//
-				+ "]"; //$NON-NLS-1$
-	}
+   public String toString() {
+      return "\nSpeedTourType [" //$NON-NLS-1$
+      //
+            + ("avgSpeed=" + avgSpeed + ", ") //$NON-NLS-1$ //$NON-NLS-2$
+            + ("tourTypeId=" + tourTypeId + ", ") //$NON-NLS-1$ //$NON-NLS-2$
+            + ("cadenceType=" + cadenceType + ", ") //$NON-NLS-1$ //$NON-NLS-2$
+            //
+            + "]"; //$NON-NLS-1$
+   }
+
 
 }
