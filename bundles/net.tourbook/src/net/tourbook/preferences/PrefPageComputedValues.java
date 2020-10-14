bc5c3510875f1121f691e685f064eaf261fad670
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-10-14 21:14:03 +0000

    Variable name

--- bundles/net.tourbook/src/net/tourbook/preferences/PrefPageComputedValues.java
+++ bundles/net.tourbook/src/net/tourbook/preferences/PrefPageComputedValues.java
@@ -1265,8 +1265,8 @@ public class PrefPageComputedValues extends PreferencePage implements IWorkbench
          return;
       }
 
-      final TourPerson ewnfkjw = TourbookPlugin.getActivePerson();
-      ewnfkjw.computePerformanceModelingData();
+      final TourPerson activePerson = TourbookPlugin.getActivePerson();
+      activePerson.computePerformanceModelingData();
       saveState();
 
       final int[] elevation = new int[] { 0, 0 };
