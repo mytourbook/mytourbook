mytourbook/mytourbook

commit ee09ccf79b8c19051ebe9be90e2896dcce409c58

[Re-import] - fixed reimport when tour time difference is < 60 seconds, old MT versions < 9.01 had no seconds - minor UI layout

bundles/net.tourbook/src/net/tourbook/Messages.java
@@ -1215,6 +1215,7 @@ public class Messages extends NLS {
    public static String        Import_Data_Checkbox_TrainingValues;
    public static String        Import_Data_Checkbox_EntireTour;
    public static String        Import_Data_Checkbox_ImportFileLocation;
+   public static String        Import_Data_Checkbox_ImportFileLocation_Tooltip;
    public static String        Import_Data_Text_AltitudeValues;
    public static String        Import_Data_Text_CadenceValues;
    public static String        Import_Data_Text_GearValues;
@@ -1238,6 +1239,7 @@ public class Messages extends NLS {
    public static String        Import_Data_Dialog_Reimport_SubTask;
    public static String        Import_Data_Dialog_Reimport_Task;
    public static String        Import_Data_Dialog_Reimport_Title;
+   public static String        Import_Data_Dialog_ReimportFile_Title;
    public static String        Import_Data_Error_CreatingFileName_Message;
    public static String        Import_Data_Error_CreatingFileName_Title;
    public static String        Import_Data_Dialog_EasyImport_InvalidBackupFolder_Message;
@@ -1289,6 +1291,7 @@ public class Messages extends NLS {
    public static String        Import_Data_Dialog_IsCancelReImport_Message;
    public static String        Import_Data_Dialog_IsCancelReImport_Title;
    public static String        Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message;
+   public static String        Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference;
    public static String        Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers;
    public static String        Import_Data_Monitor_Backup;
    public static String        Import_Data_Monitor_Backup_SubTask;

commit e9fd36e445c9564d6ee2d698b743501a87bfc594

Fixing the regression that the commit ee09ccf introduced (when trying to re-import 1 tour from a file that has a lot of tours.)

bundles/net.tourbook/src/net/tourbook/Messages.java
@@ -1296,8 +1296,6 @@ public class Messages extends NLS {
    public static String        Import_Data_Dialog_IsCancelReImport_Message;
    public static String        Import_Data_Dialog_IsCancelReImport_Title;
    public static String        Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message;
-   public static String        Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference;
-   public static String        Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent;
    public static String        Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers;
    public static String        Import_Data_Monitor_Backup;
    public static String        Import_Data_Monitor_Backup_SubTask;

bundles/net.tourbook/src/net/tourbook/importdata/RawDataManager.java
@@ -1166,8 +1166,6 @@ public class RawDataManager {
 
       TourLogManager.showLogView();
 
-      int numLoggedErrors = 0;
-
       final String oldTourDateTimeShort = TourManager.getTourDateTimeShort(oldTourData);
       String message = null;
 
@@ -1187,33 +1185,8 @@ public class RawDataManager {
           * It must be >60 seconds because db version < 7 (9.01) had no saved seconds !!!
           */
          if (timeDiff > 65_000
-
                // disabled for .fit files because they can have different tour start times (of some seconds)
-               && reImportedFile.getName().toLowerCase().endsWith(FILE_EXTENSION_FIT) == false) {
-
-            if (numLoggedErrors == 0) {
-
-               numLoggedErrors++;
-
-               TourLogManager.subLog_Error(NLS.bind(
-                     Messages.Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference,
-                     new Object[] {
-                           oldTourDateTimeShort,
-                           reImportedFile.toString(),
-                           TimeTools.Formatter_DateTime_SM.format(oldTourData.getTourStartTime()),
-                           TimeTools.Formatter_DateTime_SM.format(reimportedTourData.getTourStartTime()),
-                           timeDiff / 1000
-                     }));
-
-            } else if (numLoggedErrors == 1) {
-
-               numLoggedErrors++;
-
-               TourLogManager.subLog_Error(NLS.bind(
-                     Messages.Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent,
-                     new Object[] {
-                           oldTourDateTimeShort }));
-            }
+               && !reImportedFile.getName().toLowerCase().endsWith(FILE_EXTENSION_FIT)) {
 
             continue;
          }
@@ -1760,7 +1733,7 @@ public class RawDataManager {
 
                _importedFileNames.add(_lastImportedFileName);
 
-               if (additionalImportedFiles.size() > 0) {
+               if (!additionalImportedFiles.isEmpty()) {
                   _importedFileNamesChildren.addAll(additionalImportedFiles);
                }
             }
@@ -2135,7 +2108,7 @@ public class RawDataManager {
 
       final ImportRunState importRunState = new ImportRunState();
 
-      if (importFiles.size() == 0) {
+      if (importFiles.isEmpty()) {
          return importRunState;
       }
 

bundles/net.tourbook/src/net/tourbook/messages.properties
@@ -1361,9 +1361,6 @@ Import_Data_HTML_WatchingOff                                             = After
 Import_Data_HTML_WatchingOn                                              = After the import, device watching is kept ON.
 Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message                = Tour "{0}" cannot be re-imported from the file "{1}" because it has a different tour ID, current tour: {2} - re-import \
                                                                            file: {3}
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference            = Tour "{0}" cannot be re-imported from the file "{1}" because the time difference is too large, current tour: {2} - \
-                                                                           re-import file: {3} - time difference: {4} sec
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent = Tour "{0}" cannot be re-imported ++
 Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers                      = Tour "{0}" cannot be re-imported from the file "{1}" because it doesn't contain the same number of time slices, current \
                                                                            tour: {2} - re-import file: {3}
 Import_Data_Monitor_Backup                                               = Backup device files

bundles/net.tourbook/src/net/tourbook/messages_de.properties
@@ -1352,9 +1352,6 @@ Import_Data_HTML_WatchingOff                                             = Die S
 Import_Data_HTML_WatchingOn                                              = Die Sportcomputer \u00DCberwachung wird nach dem Import nicht ausgeschaltet.
 Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message                = Die Tour "{0}" kann von der Datei "{1}" nicht reimportiert werden da es eine andere Tour ID hat, aktuelle Tour: {2} - \
                                                                            reimportierte Datei {3}
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference            = Die Tour "{0}" kann von der Datei "{1}" nicht reimportiert werden da der Zeitunterschied zu gross ist, aktuelle Tour: {2} - \
-                                                                           reimportierte Datei: {3} - Zeitunterschied: {4} Sek
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent = Die Tour "{0}" kann von der Datei "{1}" nicht reimportiert werden ++
 Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers                      = Die Tour "{0}" kann von der Datei "{1}" nicht reimportiert werden da sie nicht die gleiche Anzahl an Datenpunkten hat, \
                                                                            aktuelle Tour: {2} - reimportierte Datei {3}
 Import_Data_Monitor_Backup                                               = Datensicherung der Sportcomputer Dateien

bundles/net.tourbook/src/net/tourbook/messages_fr.properties
@@ -1336,9 +1336,6 @@ Import_Data_HTML_WatchingOff                                             = Apr\u
 Import_Data_HTML_WatchingOn                                              = Apr\u00E8s l'import, la surveillance de l'appareil est laiss\u00E9e active.
 Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message                = Le parcours "{0}" ne peut pas \u00EAtre r\u00E9import\u00E9 depuis le fichier "{1}" car leur identificateur est \
                                                                            diff\u00E9rent, parcours actuel: {2} - fichier \u00E0 r\u00E9importer: {3}
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference            = Le parcours "{0}" ne peut pas \u00EAtre r\u00E9import\u00E9 depuis le fichier "{1}" car la diff\u00E9rence de temps est \
-                                                                           trop grande, parcours actuel: {2} - fichier \u00E0 r\u00E9importer: {3} - diff\u00E9rence de temps: {4} sec
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent = Le parcours "{0}" ne peut pas \u00EAtre r\u00E9import\u00E9 ++
 Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers                      = Le parcours "{0}" ne peut \u00EAtre r\u00E9import\u00E9 depuis le fichier "{1}" car il ne contient pas le m\u00EAme nombre \
                                                                            d''intervalles, parcours actuel: {2} - fichier \u00E0 r\u00E9importer: {3}
 Import_Data_Monitor_Backup                                               = Sauvegarder des fichiers depuis un appareil

bundles/net.tourbook/src/net/tourbook/messages_it.properties
@@ -1337,10 +1337,6 @@ Import_Data_HTML_WatchingOff                                             = Dopo 
 Import_Data_HTML_WatchingOn                                              = Dopo l'importazione, il monitoraggio del dispositivo viene mantenuto.
 Import_Data_Log_ReimportIsInvalid_DifferentTourId_Message                = Il percorso "{0}" non pu\u00F2 essere nuovamente importato dal file "{1}" perch\u00E8 ha un ID di percorso differente , \
                                                                            percorso corrente: {2} - file da importare: {3}
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference            = Il percorso "{0}" non pu\u00F2 essere importato dal file "{1}" perch\u00E8 la differenza di tempo \u00E8 troppo grande, \
-                                                                           \n\
-                                                                           percorso corrente: {2} - file reimportato: {3} - differenza di tempo: {4} secondi
-Import_Data_Log_ReimportIsInvalid_TooLargeStartTimeDifference_Subsequent = Il percorso "{0}" non pu\u00F2 essere importato ++
 Import_Data_Log_ReimportIsInvalid_WrongSliceNumbers                      = Il percorso "{0}" non pu\u00F2 essere nuovamente importato dal file "{1}" perch\u00E8 non contiene lo stesso numero di \
                                                                            campioni, percorso corrente: {2} - file da importare: {3}
 Import_Data_Monitor_Backup                                               = Backup file del dispositivo