22fc6a6f5d9e88ad86ab25b621807c553a8985d4
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-11-19 19:29:03 +0000

    Figured out how to create the preference store

--- bundles/net.tourbook.device/test/net/tourbook/trainingstress/GovssTest.java
+++ bundles/net.tourbook.device/test/net/tourbook/trainingstress/GovssTest.java
@@ -16,29 +16,41 @@ import net.tourbook.device.gpx.GPXDeviceDataReader;
 import net.tourbook.device.gpx.GPX_SAX_Handler;
 import net.tourbook.importdata.DeviceData;
 
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 
 class GovssTest {
+
    private static IPreferenceStore _prefStore;
+
    /**
     * Resource path to GPX file, generally available from net.tourbook Plugin in test/net.tourbook
     */
-   public static final String IMPORT_FILE_PATH = "/net/tourbook/trainingStress/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
+   public static final String      IMPORT_FILE_PATH = "/net/tourbook/trainingStress/Move_2017_09_30_05_36_06_Trail+running-MtWhitney.gpx"; //$NON-NLS-1$
+
+   @BeforeAll
+   static void setUp() {
+      if (_prefStore == null) {
+         _prefStore = new ScopedPreferenceStore(InstanceScope.INSTANCE, TourbookPlugin.PLUGIN_ID);
+      }
+      TimeTools.setDefaultTimeZone("UTC");
+   }
+
    @Test
    void testComputeGovss() throws SAXException, IOException, ParserConfigurationException {
+
       final InputStream gpx = GovssTest.class.getResourceAsStream(IMPORT_FILE_PATH);
 
-  final DeviceData deviceData = new DeviceData();
+      final DeviceData deviceData = new DeviceData();
       final GPXDeviceDataReader deviceDataReader = new GPXDeviceDataReader();
       final HashMap<Long, TourData> tourDataMap = new HashMap<>();
       final HashMap<Long, TourData> newlyImportedTours = new HashMap<>();
       final HashMap<Long, TourData> alreadyImportedTours = new HashMap<>();
-      if (_prefStore == null) {
-         _prefStore = TourbookPlugin.getDefault().getPreferenceStore();
-      }
-      TimeTools.setDefaultTimeZone("UTC");
+
       final GPX_SAX_Handler handler = new GPX_SAX_Handler(
             deviceDataReader,
             IMPORT_FILE_PATH,
