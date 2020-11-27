64cde16f3c6be73c8e0ea5068b30bd7ea135ba5f
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-11-27 18:05:54 +0000

    Fix for GPX tests

--- bundles/net.tourbook.tests/test/importdata/gpx/GPX_SAX_HandlerTest.java
+++ bundles/net.tourbook.tests/test/importdata/gpx/GPX_SAX_HandlerTest.java
@@ -15,9 +15,11 @@
  *******************************************************************************/
 package importdata.gpx;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
@@ -93,8 +95,7 @@ class GPX_SAX_HandlerTest {
 
       final Set<TourWayPoint> tourWayPoints = tour.getTourWayPoints();
 
-      final Integer numWayPoints = 2;
-      assert numWayPoints.equals(tourWayPoints.size());
+      assertEquals(1, tourWayPoints.size());
       final Iterator<TourWayPoint> iter = tourWayPoints.iterator();
 
       TourWayPoint waypoint1 = iter.next();
@@ -102,44 +103,33 @@ class GPX_SAX_HandlerTest {
       if (!wayPoint.equals(waypoint1.getName())) {
          waypoint1 = iter.next();
       }
-      final Double latitude = waypoint1.getLatitude();
-      assert latitude.equals(47.5674099);
-      final Double longitude = waypoint1.getLongitude();
-      assert longitude.equals(9.47741068);
-      final Float altitude = waypoint1.getAltitude();
-      assert altitude.equals(394f);
-      final Long time = waypoint1.getTime();
-      assert time.equals(1286706600000L);
-      final String name = waypoint1.getName();
-      assert name.equals(wayPoint);
-      final String comment = waypoint1.getComment();
-      assert comment.equals(wayPoint);
-      final String description = waypoint1.getDescription();
-      assert description.equals(wayPoint);
+      assertEquals(47.5674099, waypoint1.getLatitude());
+      assertEquals(9.47741068, waypoint1.getLongitude());
+      assertEquals(394f, waypoint1.getAltitude());
+      assertEquals(1286706600000L, waypoint1.getTime());
+      assertEquals(wayPoint, waypoint1.getName());
+      assertEquals(wayPoint, waypoint1.getComment());
+      assertEquals(wayPoint, waypoint1.getDescription());
 
-      final String track1 = tour.getTourTitle();
-      assert track1.equals("track1"); //$NON-NLS-1$
+      assertEquals("track1", tour.getTourTitle());//$NON-NLS-1$
 
       final float[] altitudeSerie = tour.altitudeSerie;
-      assert Arrays.equals(altitudeSerie, new float[] { 395, 394, 393, 394 });
-      final String tourDateFull = TourManager.getTourDateFull(tour);
-      assert tourDateFull.equals("2010-10-10T12:00:00.000+02:00"); //$NON-NLS-1$
+      assertArrayEquals(new float[] { 395, 394, 393, 394 }, altitudeSerie);
+      assertEquals("Sunday, October 10, 2010 - 12:00 PM", TourManager.getTourDateTimeFull(tour)); //$NON-NLS-1$
       final Integer timeSerieLength = tour.timeSerie.length;
-      assert timeSerieLength.equals(4);
+      assertEquals(4, timeSerieLength);
       final int[] timeSerie = tour.timeSerie;
-      assert Arrays.equals(timeSerie, new int[] { 0, 600, 1200, 1800 });
+      assertArrayEquals(new int[] { 0, 600, 1200, 1800 }, timeSerie);
 
       final double[] latitudeSerie = tour.latitudeSerie;
-      final double[] expectedLat = new double[] { 47.567517620, 47.585505568, 47.585397817, 47.567409937 };
-      assert Arrays.equals(latitudeSerie, expectedLat);
+      assertArrayEquals(new double[] { 47.567517620, 47.585505568, 47.585397817, 47.567409937 }, latitudeSerie);
       final double[] longitudeSerie = tour.longitudeSerie;
-      final double[] expectedLon = new double[] { 9.450832423, 9.450987056, 9.477574415, 9.477410677 };
-      assert Arrays.equals(longitudeSerie, expectedLon);
+      assertArrayEquals(new double[] { 9.450832423, 9.450987056, 9.477574415, 9.477410677 }, longitudeSerie);
       final float[] temperatureSerie = tour.temperatureSerie;
-      assert Arrays.equals(temperatureSerie, new float[] { 20, 21, 22, 21 });
+      assertArrayEquals(new float[] { 20, 21, 22, 21 }, temperatureSerie);
       final float[] pulseSerie = tour.pulseSerie;
-      assert Arrays.equals(pulseSerie, new float[] { 110, 120, 130, 120 });
+      assertArrayEquals(new float[] { 110, 120, 130, 120 }, pulseSerie);
       final float[] cadenceSerie = tour.getCadenceSerie();
-      assert Arrays.equals(cadenceSerie, new float[] { 40, 50, 60, 50 });
+      assertArrayEquals(new float[] { 40, 50, 60, 50 }, cadenceSerie);
    }
 }
