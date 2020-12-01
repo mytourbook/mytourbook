48bcae30db8244d38e210f8091a3bfe06dd64d92
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-12-01 17:57:12 +0000

    Revert "When computing the cadence zone times, we prevent from re-importing the last imported tours"
    This reverts commit 96b4958b3015836fb4041fa470e466ea1517a66f.

--- bundles/net.tourbook/src/net/tourbook/ui/action/ActionComputeCadenceZonesTimes.java
+++ bundles/net.tourbook/src/net/tourbook/ui/action/ActionComputeCadenceZonesTimes.java
@@ -24,7 +24,6 @@ import net.tourbook.application.TourbookPlugin;
 import net.tourbook.common.util.SQL;
 import net.tourbook.data.TourData;
 import net.tourbook.database.TourDatabase;
-import net.tourbook.importdata.RawDataManager;
 import net.tourbook.preferences.ITourbookPreferences;
 import net.tourbook.tour.TourEventId;
 import net.tourbook.tour.TourLogManager;
@@ -100,15 +99,10 @@ public class ActionComputeCadenceZonesTimes extends Action {
                public void run() {
 
                   TourManager.fireEvent(TourEventId.CLEAR_DISPLAYED_TOUR);
-                  // prevent re-importing in the import view
-                  RawDataManager.setIsReimportingActive(true);
-                  {
-                     // fire unique event for all changes
-                     TourManager.fireEvent(TourEventId.ALL_TOURS_ARE_MODIFIED);
-                  }
-                  RawDataManager.setIsReimportingActive(false);
+                  TourManager.fireEvent(TourEventId.ALL_TOURS_ARE_MODIFIED);
                }
             });
+
          }
       }
    }
