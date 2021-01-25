2cf4ddd9bce2c3d731967df60e2858d34831c483
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-01-25 22:05:46 +0000

    Workaround to putting the map in the clipboard

--- bundles/net.tourbook/src/net/tourbook/map2/action/ActionExportMapViewClipboard.java
+++ bundles/net.tourbook/src/net/tourbook/map2/action/ActionExportMapViewClipboard.java
@@ -15,18 +15,25 @@
  *******************************************************************************/
 package net.tourbook.map2.action;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import net.tourbook.common.UI;
+import net.tourbook.common.util.StatusUtil;
 import net.tourbook.map2.Messages;
 import net.tourbook.map2.view.Map2View;
 import net.tourbook.map2.view.PngTransfer;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IStatusLineManager;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.ImageTransfer;
 import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
 import org.eclipse.swt.widgets.Display;
 
 public class ActionExportMapViewClipboard extends Action {
@@ -43,22 +50,40 @@ public class ActionExportMapViewClipboard extends Action {
    @Override
    public void run() {
 
+      final Image mapViewImage = _map2View.getMapViewImage();
       final Clipboard clipboard = new Clipboard(Display.getCurrent());
 
-      final Image mapViewImage = _map2View.getMapViewImage();
-      final ImageData imageData = mapViewImage.getImageData();
-
       if (UI.IS_LINUX) {
+
          final PngTransfer imageTransfer = PngTransfer.getInstance();
-         clipboard.setContents(new ImageData[] { imageData },
+         clipboard.setContents(new ImageData[] { mapViewImage.getImageData() },
                new Transfer[] { imageTransfer });
       } else {
-         final ImageTransfer imageTransfer = ImageTransfer.getInstance();
-         clipboard.setContents(new ImageData[] { imageData },
-               new Transfer[] { imageTransfer });
+         //Workaround for the issue on Windows where the map is missing but the legend
+         //is displayed (GIMP and Inkscape). No issue when sing MS Paint
+
+         try {
+            final String absoluteFilePath = Files.createTempFile("map", ".jpg").toString(); //$NON-NLS-1$ //$NON-NLS-2$
+
+            //We export the image to a file as a JPEG image
+            final ImageLoader loader = new ImageLoader();
+            loader.data = new ImageData[] { mapViewImage.getImageData() };
+            loader.save(absoluteFilePath, SWT.IMAGE_JPEG);
+
+            //We reload it to put it in the clipboard
+            final Image image = new Image(Display.getCurrent(), absoluteFilePath);
+            clipboard.setContents(new ImageData[] { image.getImageData() },
+                  new Transfer[] { ImageTransfer.getInstance() });
+            image.dispose();
+
+            Files.deleteIfExists(Paths.get(absoluteFilePath));
+         } catch (final IOException e) {
+            StatusUtil.log(e);
+         }
       }
 
       mapViewImage.dispose();
+      clipboard.dispose();
 
       final IStatusLineManager statusLineManager = UI.getStatusLineManager();
       if (statusLineManager == null) {
@@ -66,8 +91,6 @@ public class ActionExportMapViewClipboard extends Action {
       }
 
       statusLineManager.setMessage(Messages.Map_Action_Export_Map_Clipboard_Copied_Info);
-
-      clipboard.dispose();
 
       Display.getCurrent().timerExec(2000, () -> statusLineManager.setMessage(null));
    }
