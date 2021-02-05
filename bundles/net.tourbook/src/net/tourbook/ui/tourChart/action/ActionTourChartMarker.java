4a1da6bb5d900b02c1143f9b3680f08c8312bdaf
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-02-05 20:51:50 +0000

    formatting

--- bundles/net.tourbook/src/net/tourbook/ui/tourChart/action/ActionTourChartMarker.java
+++ bundles/net.tourbook/src/net/tourbook/ui/tourChart/action/ActionTourChartMarker.java
@@ -159,19 +159,14 @@ public class ActionTourChartMarker extends ContributionItem implements IOpeningD
          return;
       }
 
-      final boolean isToolItemHovered = item == _actionToolItem;
-
       Rectangle itemBounds = null;
 
-      if (isToolItemHovered) {
+      itemBounds = item.getBounds();
 
-         itemBounds = item.getBounds();
+      final Point itemDisplayPosition = _toolBar.toDisplay(itemBounds.x, itemBounds.y);
 
-         final Point itemDisplayPosition = _toolBar.toDisplay(itemBounds.x, itemBounds.y);
-
-         itemBounds.x = itemDisplayPosition.x;
-         itemBounds.y = itemDisplayPosition.y;
-      }
+      itemBounds.x = itemDisplayPosition.x;
+      itemBounds.y = itemDisplayPosition.y;
 
       openSlideout(itemBounds, true);
    }
