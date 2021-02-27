33d772427c40b5401ecde7c89fbb009f0f1bebd1
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2021-02-27 08:00:20 +0100

    [Easy Import] Improved UI layout for the last column in the import file list

--- bundles/net.tourbook/src/net/tourbook/ui/views/rawData/RawDataView.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/rawData/RawDataView.java
@@ -1703,7 +1703,13 @@ public class RawDataView extends ViewPart implements ITourProviderAll, ITourView
                filePathName = filePathName.replace(tourbookFileSystem.getId(), tourbookFileSystem.getDisplayId());
             }
 
-            final String nbspFilePathName = filePathName.replace(UI.SPACE1, WEB.NONE_BREAKING_SPACE);
+            final String nbspFilePathName = UI.EMPTY_STRING
+
+                  // add additonal space before the text otherwise it is too narrow to the previous column
+                  + WEB.NONE_BREAKING_SPACE
+                  + WEB.NONE_BREAKING_SPACE
+
+                  + filePathName.replace(UI.SPACE1, WEB.NONE_BREAKING_SPACE);
 
             sb.append("<td class='column content'>"); //$NON-NLS-1$
             sb.append(nbspFilePathName);
