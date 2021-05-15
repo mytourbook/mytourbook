884c90526b96ab0285e14fc70e18779a75e5a10d
Thomas Theussing <telemaxxtower@gmail.com>
2021-05-15 10:03:26 +0200

    resolving conflicts

--- bundles/net.tourbook/src/net/tourbook/map25/Map25ConfigManager.java
+++ bundles/net.tourbook/src/net/tourbook/map25/Map25ConfigManager.java
@@ -126,7 +126,7 @@ public class Map25ConfigManager {
    public static final RGB     DEFAULT_OUTLINE_COLOR                   = new RGB(0x80, 0x0, 0x80);
    public static final int     DEFAULT_OUTLINE_OPACITY                 = 70;
    public static final float   DEFAULT_OUTLINE_WIDTH                   = 2.5f;
-   public static final boolean DEFAULT_OUTLINE_IS_SHOW_DIRECTION_ARROW = false;
+   public static final boolean DEFAULT_OUTLINE_IS_SHOW_DIRECTION_ARROW = true;
    public static final int     OUTLINE_OPACITY_MIN                     = 1;
    public static final int     OUTLINE_OPACITY_MAX                     = 100;
    public static final float   OUTLINE_WIDTH_MIN                       = 0.1f;
@@ -524,9 +524,9 @@ public class Map25ConfigManager {
          // <Outline>
          final IMemento xmlOutline = Util.setXmlRgb(xmlConfig, TAG_OUTLINE, config.outlineColor);
          {
+            xmlOutline.putBoolean(     ATTR_OUTLINE_IS_SHOW_DIRECTION_ARROW, config.isShowDirectionArrow);
             xmlOutline.putFloat(       ATTR_OUTLINE_WIDTH,                   config.outlineWidth);
             xmlOutline.putInteger(     ATTR_OUTLINE_OPACITY,                 config.outlineOpacity);
-            xmlOutline.putBoolean(     ATTR_OUTLINE_IS_SHOW_DIRECTION_ARROW, config.outlineShowDirectionArrow);
          }
 
          // <SliderPath>
@@ -724,10 +724,10 @@ public class Map25ConfigManager {
 
          case TAG_OUTLINE:
 
+            config.isShowDirectionArrow      = Util.getXmlBoolean(xmlConfigChild,      ATTR_OUTLINE_IS_SHOW_DIRECTION_ARROW, true);
             config.outlineColor              = Util.getXmlRgb(xmlConfigChild,          DEFAULT_OUTLINE_COLOR);
             config.outlineOpacity            = Util.getXmlInteger(xmlConfigChild,      ATTR_OUTLINE_OPACITY,   DEFAULT_OUTLINE_OPACITY,   OUTLINE_OPACITY_MIN, OUTLINE_OPACITY_MAX);
             config.outlineWidth              = Util.getXmlFloatFloat(xmlConfigChild,   ATTR_OUTLINE_WIDTH,     DEFAULT_OUTLINE_WIDTH,     OUTLINE_WIDTH_MIN,   OUTLINE_WIDTH_MAX);
-            config.outlineShowDirectionArrow = Util.getXmlBoolean(xmlConfigChild,      ATTR_OUTLINE_IS_SHOW_DIRECTION_ARROW, true);
             break;
 
          case TAG_SLIDER_PATH:
