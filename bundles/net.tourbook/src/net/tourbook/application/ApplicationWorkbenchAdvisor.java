5976361cbeec87786205e6f66bf2370d3ffe58df
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2021-07-14 16:03:27 +0200

    [All Views] Repaired close button in all views
    It can happen that the close button in the view tab disappear. This repair feature is performed when MyTourbook is closing.
    
    This is an experimental feature and can be activated with the command line parameter -DsetAllViewsClosable

--- bundles/net.tourbook/src/net/tourbook/application/ApplicationWorkbenchAdvisor.java
+++ bundles/net.tourbook/src/net/tourbook/application/ApplicationWorkbenchAdvisor.java
@@ -15,69 +15,48 @@
  *******************************************************************************/
 package net.tourbook.application;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.net.URISyntaxException;
 
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import net.tourbook.common.UI;
-import net.tourbook.common.util.StatusUtil;
+import net.tourbook.common.util.Util;
 import net.tourbook.preferences.PrefPageGeneral;
 import net.tourbook.tour.TourManager;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.URIUtil;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+   private static final String SYS_PROP__SET_ALL_VIEWS_CLOSABLE = "setAllViewsClosable";                                       //$NON-NLS-1$
+
+   /**
+    * When <code>true</code> then all views will be set closable in workbench.xmi
+    * <p>
+    * Sometimes the attribute "closeable=true" for views do disappear and a view cannot be closed
+    * anymore with the mouse.
+    * <p>
+    * Commandline parameter: <code>-DsetAllViewsClosable</code>
+    */
+   private static boolean      IS_SET_ALL_VIEWS_CLOSABLE        = System.getProperty(SYS_PROP__SET_ALL_VIEWS_CLOSABLE) != null;
+
+   static {
+
+      if (IS_SET_ALL_VIEWS_CLOSABLE) {
+
+         Util.logSystemProperty_IsEnabled(ApplicationWorkbenchAdvisor.class,
+               SYS_PROP__SET_ALL_VIEWS_CLOSABLE,
+               "All views will be set closeable=\"true\" in workbench.xmi when the app closes"); //$NON-NLS-1$
+      }
+   }
 
    @Override
    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(final IWorkbenchWindowConfigurer configurer) {
       return new ApplicationWorkbenchWindowAdvisor(this, configurer);
-   }
-
-   /**
-    * Copied from org.eclipse.e4.ui.internal.workbench.ResourceHandler
-    *
-    * @return
-    */
-   private File getBaseLocation() {
-
-      File baseLocation;
-      try {
-         baseLocation = new File(URIUtil.toURI(Platform.getInstanceLocation().getURL()));
-      } catch (final URISyntaxException e) {
-         throw new RuntimeException(e);
-      }
-
-      baseLocation = new File(baseLocation, ".metadata"); //$NON-NLS-1$
-      baseLocation = new File(baseLocation, ".plugins"); //$NON-NLS-1$
-
-      return new File(baseLocation, "org.eclipse.e4.workbench"); //$NON-NLS-1$
    }
 
    @Override
@@ -94,6 +73,26 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
       return PrefPageGeneral.ID;
    }
 
+   /**
+    * Copied from org.eclipse.e4.ui.internal.workbench.ResourceHandler
+    *
+    * @return
+    */
+   private File getWorkbenchFolderPath() {
+
+      File baseLocation;
+      try {
+         baseLocation = new File(URIUtil.toURI(Platform.getInstanceLocation().getURL()));
+      } catch (final URISyntaxException e) {
+         throw new RuntimeException(e);
+      }
+
+      baseLocation = new File(baseLocation, ".metadata"); //$NON-NLS-1$
+      baseLocation = new File(baseLocation, ".plugins"); //$NON-NLS-1$
+
+      return new File(baseLocation, "org.eclipse.e4.workbench"); //$NON-NLS-1$
+   }
+
    @Override
    public void initialize(final IWorkbenchConfigurer configurer) {
 
@@ -103,57 +102,14 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
    @Override
    public void postShutdown() {
 
-      final File fileWorkbenchXMI = new File(getBaseLocation(), "workbench.xmi");
-      final File fileWorkbenchXMI_Copy = new File(getBaseLocation(), "workbench-copy.xmi");
+      if (IS_SET_ALL_VIEWS_CLOSABLE) {
 
-      try (FileInputStream fromInputStream = new FileInputStream(fileWorkbenchXMI);
+         // when the timer delay is 100 ms then the task is not run
+         Display.getDefault().timerExec(0, () -> {
 
-            FileOutputStream intoFileOutputStream = new FileOutputStream(fileWorkbenchXMI_Copy);
-            OutputStreamWriter intoOutputStreamWriter = new OutputStreamWriter(intoFileOutputStream, UI.UTF_8);
-            Writer intoCopyWriter = new BufferedWriter(intoOutputStreamWriter)) {
+            ApplicationTools.fixClosable(getWorkbenchFolderPath());
+         });
 
-         // Load the document
-         final DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
-         domFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-         domFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-         final Document domDocument = domFactory.newDocumentBuilder().parse(fromInputStream);
-
-         // Select the node(s) with XPath
-         final XPath xpath = XPathFactory.newInstance().newXPath();
-         final NodeList nodes = (NodeList) xpath.evaluate(
-               "//children[@xsi:type='advanced:Placeholder']",
-               domDocument,
-               XPathConstants.NODESET);
-
-         final int numNodes = nodes.getLength();
-
-         System.out.println((System.currentTimeMillis() + " numNodes:" + numNodes));
-         // TODO remove SYSTEM.OUT.PRINTLN
-
-         // Updated the selected nodes
-         for (int nodeIndex = 0; nodeIndex < numNodes; nodeIndex++) {
-
-            System.out.println((System.currentTimeMillis() + " " + nodes.item(nodeIndex)));
-            // TODO remove SYSTEM.OUT.PRINTLN
-
-            final Element value = (Element) nodes.item(nodeIndex);
-            value.setAttribute("closable", "true");
-         }
-
-         // Get the result as a String
-         final TransformerFactory transformFactory = TransformerFactory.newInstance();
-         transformFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
-         final Transformer xformer = transformFactory.newTransformer();
-         xformer.transform(new DOMSource(domDocument), new StreamResult(intoCopyWriter));
-
-      } catch (ParserConfigurationException
-            | SAXException
-            | IOException
-            | XPathExpressionException
-            | TransformerException e) {
-
-         StatusUtil.log(e);
       }
    }
 
