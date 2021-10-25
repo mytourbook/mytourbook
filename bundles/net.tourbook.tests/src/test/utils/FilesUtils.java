357e1d24c6e9a08f9132c062c5ae96a7b7af1915
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-10-25 16:37:48 -0600

    Add the unit tests in the build process (#68)

---
+++ bundles/net.tourbook.tests/src/test/utils/FilesUtils.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (C) 2021 Frédéric Bard
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *******************************************************************************/
+package utils;
+
+import java.nio.file.Paths;
+
+public class FilesUtils {
+
+	public static final String rootPath = "src/test/"; //$NON-NLS-1$
+
+
+	public static String getAbsoluteFilePath(final String filePath) {
+
+		return Paths.get(filePath).toAbsolutePath().toString();
+   }
+}
