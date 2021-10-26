d76e6583f584b317afa8d45626fb42cb750bfebd
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-10-26 20:50:06 +0000

    New Attempt

--- bundles/net.tourbook.tests/src/test/importdata/suunto9/Suunto9Tests.java
+++ bundles/net.tourbook.tests/src/test/importdata/suunto9/Suunto9Tests.java
@@ -157,6 +157,7 @@ class Suunto9Tests {
     */
    @Test
    void testImportShoreLineWithLaps() {
+
       final String filePath = IMPORT_FILE_PATH + "1555291925128_183010004848_post_timeline-1"; //$NON-NLS-1$
 
       final String testFilePath = FilesUtils.getAbsoluteFilePath(filePath + JSON_GZ);
