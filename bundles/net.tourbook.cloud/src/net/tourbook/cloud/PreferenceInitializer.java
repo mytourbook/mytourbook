3d37aa36c31d5c04e10172d91c49ea2c4dc8100d
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-10-29 08:08:45 -0600

    final

--- bundles/net.tourbook.cloud/src/net/tourbook/cloud/PreferenceInitializer.java
+++ bundles/net.tourbook.cloud/src/net/tourbook/cloud/PreferenceInitializer.java
@@ -28,7 +28,7 @@ import org.eclipse.jface.preference.IPreferenceStore;
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
    //This is the date (01/26/2021) that Suunto forced the users to switch to Suunto App.
-   public static long SUUNTO_FILTER_SINCE_DATE = 1611619200000L;
+   public static final long SUUNTO_FILTER_SINCE_DATE = 1611619200000L;
 
    @Override
    public void initializeDefaultPreferences() {
