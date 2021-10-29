8347e14414f3309a96b635eb2d4429c143c9c16a
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-10-29 16:58:34 +0000

    Merge remote-tracking branch 'origin/main' into main

--- bundles/net.tourbook.cloud/src/net/tourbook/cloud/strava/PrefPageStrava.java
+++ bundles/net.tourbook.cloud/src/net/tourbook/cloud/strava/PrefPageStrava.java
@@ -94,6 +94,7 @@ public class PrefPageStrava extends FieldEditorPreferencePage implements IWorkbe
    private Label              _labelRefreshToken_Value;
    private Button             _chkSendDescription;
    private Button             _chkUseTourTypeMapping;
+
    private Link               _linkAthleteWebPage;
    private PreferenceLinkArea _linkTourTypeFilters;
 
