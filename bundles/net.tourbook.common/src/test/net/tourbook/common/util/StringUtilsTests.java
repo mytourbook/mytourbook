eb61358265123d0941708f51f6d1ecde08dc0e98
FJBDev <25235665+FJBDev@users.noreply.github.com>
2021-11-16 22:18:59 +0000

    Adding String utils unit tests but they are not yet picked up by the maven plugin

---
+++ bundles/net.tourbook.common/src/test/net/tourbook/common/util/StringUtilsTests.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (C) 2021 Frédéric Bard
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *******************************************************************************/
+package test.net.tourbook.common.util;
+
+import net.tourbook.common.util.StringUtils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public final class StringUtilsTests {
+
+   @Test
+   void testHasContent() {
+
+      Assertions.assertTrue(StringUtils.hasContent("string"));
+      Assertions.assertFalse(StringUtils.hasContent(" "));
+      Assertions.assertFalse(StringUtils.hasContent(""));
+      Assertions.assertFalse(StringUtils.hasContent(null));
+   }
+
+   @Test
+   void testJoin() {
+
+      final String[] stringArray = { "1", "2", "3" };
+      Assertions.assertEquals(StringUtils.join(stringArray, ","), "1,2,3");
+      Assertions.assertEquals(StringUtils.join(new String[] { "1" }, ","), "1");
+   }
+
+   @Test
+   void testSanitizeFileName() {
+
+      final String fileName = "\\$%#filename.txt";
+      Assertions.assertEquals(StringUtils.sanitizeFileName(fileName), "----filename.txt");
+   }
+}
