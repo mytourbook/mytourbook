9978949b1f9f6024f539a0fc49d416e2946a7002
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-01-14 23:41:37 +0000

    Still need to find files to use in the tests

--- bundles/net.tourbook.tests/src/test/ui/views/rawData/TourMergerTests.java
+++ bundles/net.tourbook.tests/src/test/ui/views/rawData/TourMergerTests.java
@@ -15,15 +15,14 @@
  *******************************************************************************/
 package ui.views.rawData;
 
-import java.nio.file.Paths;
-
 import net.tourbook.data.TourData;
-import net.tourbook.data.TourType;
 import net.tourbook.ui.views.rawData.TourMerger;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import utils.Comparison;
 import utils.FilesUtils;
 import utils.Initializer;
 
@@ -33,140 +32,39 @@ public class TourMergerTests {
    private static final String _testTourFilePathTcx = IMPORT_PATH + "TCXExport.tcx";         //$NON-NLS-1$
    private static final String _testTourFilePathGpx = IMPORT_PATH + "GPXExport.gpx";         //$NON-NLS-1$
 
-   private static TourData     _tour;
+   private static TourData     _targetTour;
    private TourMerger          _tourMerger;
 
    @BeforeAll
    static void initAll() {
 
-      _tour = Initializer.importTour();
-      final TourType tourType = new TourType();
-      tourType.setName("Running"); //$NON-NLS-1$
-      _tour.setTourType(tourType);
+      _targetTour = Initializer.importTour();
    }
 
    @AfterEach
    void afterEach() {
 
-      net.tourbook.common.util.FilesUtils.deleteIfExists(Paths.get(_testTourFilePathTcx));
-      net.tourbook.common.util.FilesUtils.deleteIfExists(Paths.get(_testTourFilePathGpx));
    }
 
-//   private void executeGpxTest(final String controlTourFileName) {
-//
-//      _tourMerger.computeMergedData(_testTourFilePathGpx);
-//
-//      final List<String> nodesToFilter = Arrays.asList("Cadence", "mt:tourType", "mt:tourDistance"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//      final List<String> attributesToFilter = Arrays.asList("creator"); //$NON-NLS-1$
-//      Comparison.compareXmlAgainstControl(IMPORT_PATH + controlTourFileName,
-//            _testTourFilePathGpx,
-//            nodesToFilter,
-//            attributesToFilter);
-//   }
-//
-//   private void executeTcxTest(final String controlTourFileName) {
-//
-//      _tourExporter.export(_testTourFilePathTcx);
-//
-//      final List<String> nodesToFilter = Arrays.asList("Cadence", "Author", "Creator"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//      final List<String> attributesToFilter = new ArrayList<>();
-//      Comparison.compareXmlAgainstControl(IMPORT_PATH + controlTourFileName,
-//            _testTourFilePathTcx,
-//            nodesToFilter,
-//            attributesToFilter);
-//   }
-//
-//   private void initializeTourExporterGpx() {
-//
-//      _tourExporter = new TourExporter(ExportTourGPX.GPX_1_0_TEMPLATE).useTourData(_tour);
-//      _tourExporter.setActivityType(_tour.getTourType().getName());
-//   }
-//
-//   private void initializeTourExporterTcx() {
-//
-//      _tourExporter = new TourExporter(ExportTourTCX.TCX_2_0_TEMPLATE).useTourData(_tour);
-//      _tourExporter.setActivityType(_tour.getTourType().getName());
-//   }
-//
-//   @Test
-//   void testGpxExportAllOptions() {
-//
-//      initializeTourExporterGpx();
-//
-//      final String controlTourFileName = "LongsPeak-AllOptions-RelativeDistance.gpx"; //$NON-NLS-1$
-//
-//      _tourExporter.setUseDescription(true);
-//      _tourExporter.setIsExportAllTourData(true);
-//      _tourExporter.setIsExportSurfingWaves(true);
-//      _tourExporter.setIsExportWithBarometer(true);
-//      _tourExporter.setIsCamouflageSpeed(true);
-//      _tourExporter.setCamouflageSpeed(15 / 3.6f); // 15km/h
-//      _tour.setBodyWeight(77.7f); // 77.7kg
-//      _tour.setCalories(1282000); // 1282 kcal / 1282000 calories
-//
-//      executeGpxTest(controlTourFileName);
-//   }
-//
-//   @Test
-//   void testGpxExportDescriptionAndActivity() {
-//
-//      initializeTourExporterGpx();
-//
-//      final String controlTourFileName = "LongsPeak-AbsoluteDistance.gpx"; //$NON-NLS-1$
-//
-//      _tourExporter.setUseAbsoluteDistance(true);
-//
-//      executeGpxTest(controlTourFileName);
-//   }
-//
-//   @Test
-//   void testTcxExportCamouflage15KmHBikingActivity() {
-//
-//      initializeTourExporterTcx();
-//
-//      _tourExporter.setUseActivityType(true);
-//      _tourExporter.setActivityType("Biking"); //$NON-NLS-1$
-//      _tourExporter.setIsCamouflageSpeed(true);
-//      _tourExporter.setCamouflageSpeed(15 / 3.6f);
-//
-//      final String controlTourFileName = "LongsPeak-CamouflageSpeed-15kmh-BikingActivity.tcx"; //$NON-NLS-1$
-//      executeTcxTest(controlTourFileName);
-//   }
-//
-//   @Test
-//   void testTcxExportCourse() {
-//
-//      initializeTourExporterTcx();
-//
-//      _tourExporter.setIsCourse(true);
-//      _tourExporter.setCourseName("Longs Peak"); //$NON-NLS-1$
-//
-//      final String controlTourFileName = "LongsPeak-Course.tcx"; //$NON-NLS-1$
-//      executeTcxTest(controlTourFileName);
-//   }
-//
-//   @Test
-//   void testTcxExportDescriptionAndActivity() {
-//
-//      initializeTourExporterTcx();
-//
-//      _tourExporter.setUseDescription(true);
-//      _tourExporter.setUseActivityType(true);
-//      _tourExporter.setIsExportAllTourData(true);
-//
-//      final String controlTourFileName = "LongsPeak-Description-RunningActivity.tcx"; //$NON-NLS-1$
-//      executeTcxTest(controlTourFileName);
-//   }
-//
-//   @Test
-//   void testTcxExportHikingActivity() {
-//
-//      initializeTourExporterTcx();
-//
-//      _tourExporter.setUseActivityType(true);
-//      _tourExporter.setActivityType("Hiking"); //$NON-NLS-1$
-//
-//      final String controlTourFileName = "LongsPeak-HikingActivity.tcx"; //$NON-NLS-1$
-//      executeTcxTest(controlTourFileName);
-//   }
+   @Test
+   void testMergeTours() {
+
+      _tourMerger = new TourMerger(
+            new TourData(), // _sourceTour,
+            _targetTour,
+            false, //_chkSynchStartTime.getSelection(),
+            0, //_tourStartTimeSynchOffset,
+            true, //_chkMergeCadence.getSelection(),
+            true, //_chkMergePulse.getSelection(),
+            true, //_chkMergeSpeed.getSelection(),
+            true, //_chkMergeTemperature.getSelection(),
+            false, //_chkAdjustAltiFromStart.getSelection(),
+            false, //_chkAdjustAltiFromSource.getSelection(),
+            false, //_chkAdjustAltiSmoothly.getSelection(),
+            0);// _tourChart.getXSliderPosition().getLeftSliderValueIndex());
+
+      final TourData mergedTour = _tourMerger.computeMergedData_NEWWIP();
+
+      Comparison.compareTourDataAgainstControl(mergedTour, "filePath");
+   }
 }
