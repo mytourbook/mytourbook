ffe5f099a762554d0ed5e9027cdfeb6956b4d5d8
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-02-28 20:13:58 +0000

    WIP

--- bundles/net.tourbook/src/net/tourbook/ui/views/WeatherProvider_OpenWeatherMap.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/WeatherProvider_OpenWeatherMap.java
@@ -15,15 +15,42 @@
  *******************************************************************************/
 package net.tourbook.ui.views;
 
+import static org.eclipse.swt.events.SelectionListener.widgetSelectedAdapter;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
+import net.tourbook.Messages;
+import net.tourbook.common.util.StatusUtil;
+import net.tourbook.weather.worldweatheronline.WorldWeatherOnlineRetriever;
+import net.tourbook.web.WEB;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 public class WeatherProvider_OpenWeatherMap implements IWeatherProvider {
+
+   private static HttpClient httpClient = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build();
 
    /*
     * UI controls
     */
    private FormToolkit _tk;
+   private Button      _btnTestConnection;
 
    public WeatherProvider_OpenWeatherMap() {}
 
@@ -32,13 +59,85 @@ public class WeatherProvider_OpenWeatherMap implements IWeatherProvider {
                              final Composite parent,
                              final FormToolkit tk) {
       _tk = tk;
-      final Composite container = _tk.createComposite(parent);
+      final int defaultHIndent = 16;
 
+      final Composite container = _tk.createComposite(parent);
+      {
+         /*
+          * WWO Api Sign-up page
+          */
+
+         // Link - see http(s)://www.worldweatheronline.com/developer/signup.aspx
+         final Link linkApiSignup = new Link(container, SWT.PUSH);
+         linkApiSignup.setText(Messages.Pref_Weather_Link_ApiSignup);
+         linkApiSignup.setEnabled(true);
+         linkApiSignup.addListener(
+               SWT.Selection,
+               event -> WEB.openUrl(Messages.External_Link_Weather_ApiSignup));
+         GridDataFactory.fillDefaults()
+               .span(2, 1)
+               .indent(defaultHIndent, 0)
+               .applyTo(linkApiSignup);
+      }
+      {
+         /*
+          * Button: test connection
+          */
+         _btnTestConnection = new Button(container, SWT.NONE);
+         _btnTestConnection.setText(Messages.Pref_Weather_Button_TestHTTPConnection);
+         _btnTestConnection.addSelectionListener(widgetSelectedAdapter(
+               selectionEvent -> onCheckConnection()));
+         GridDataFactory.fillDefaults()
+               .indent(defaultHIndent, 0)
+               .align(SWT.BEGINNING, SWT.FILL)
+               .span(2, 1)
+               .applyTo(_btnTestConnection);
+      }
       return container;
    }
 
    @Override
    public void dispose() {}
+
+   /**
+    * This method checks the connection to the API
+    */
+   private void onCheckConnection() {
+
+      BusyIndicator.showWhile(Display.getCurrent(), () -> {
+
+         try {
+
+            final HttpRequest request = HttpRequest
+                  .newBuilder(URI.create("HEROKU URL"))
+                  .GET()
+                  .build();
+
+            final HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+
+            final int statusCode = response.statusCode();
+            final String responseMessage = response.body();
+
+            final String message = statusCode == HttpURLConnection.HTTP_OK
+                  ? NLS.bind(Messages.Pref_Weather_CheckHTTPConnection_OK_Message, WorldWeatherOnlineRetriever.getBaseApiUrl())
+                  : NLS.bind(
+                        Messages.Pref_Weather_CheckHTTPConnection_FAILED_Message,
+                        new Object[] {
+                              WorldWeatherOnlineRetriever.getBaseApiUrl(),
+                              statusCode,
+                              responseMessage });
+
+            MessageDialog.openInformation(
+                  Display.getCurrent().getActiveShell(),
+                  Messages.Pref_Weather_CheckHTTPConnection_Message,
+                  message);
+
+         } catch (final IOException | InterruptedException e) {
+            StatusUtil.log(e);
+            Thread.currentThread().interrupt();
+         }
+      });
+   }
 
    @Override
    public void performDefaults() {}
