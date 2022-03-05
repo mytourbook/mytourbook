b8af48ce2da39727026feed7cb8ffa45795b6cbf
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-03-04 22:08:25 -0700

    Space

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -533,9 +533,9 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Cloneable
     */
    private boolean               isWeatherDataFromProvider = false;
 
-   private int                   weather_Wind_Direction  = -1;                                 // db-version 8
+   private int                   weather_Wind_Direction  = -1;                         // db-version 8
    private int                   weather_Wind_Speed;                                       // db-version 8
-   private String                weather_Clouds;                                        // db-version 8
+   private String                weather_Clouds;                                       // db-version 8
 
    private String                weather;                                              // db-version 13
 
