1db4b89372f2c7f23f29a5a95679eb1a146c9f94
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-06-02 18:09:41 +0000

    Improving the tests

--- bundles/net.tourbook.ui.tests/src/test/basic/BasicTests.java
+++ bundles/net.tourbook.ui.tests/src/test/basic/BasicTests.java
@@ -17,20 +17,22 @@ package basic;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 public class BasicTests {
 
-   private SWTBot bot = new SWTBot();
+   private SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
    @BeforeClass
    public static void beforeClass() {
 
       SWTBotPreferences.TIMEOUT = 10000;
+      SWTBotPreferences.PLAYBACK_DELAY = 5000;
    }
 
    @Test
@@ -40,30 +42,119 @@ public class BasicTests {
       bot.button("Apply and Close").click();
    }
 
-//   @Test
-//   void testStatisticsView() {
-//
-//      final SWTBotMenu statisticsView = bot.menu("Statistics");
-//      assertNotNull(statisticsView);
-//   }
+   @Test
+   void testOpenViews() {
+
+      //Select a tour so that the selected views contain information
+      final SWTBotView tourBookView = bot.viewByTitle("Tour Book");
+      assertNotNull(tourBookView);
+      tourBookView.show();
+
+      final String twentyThirteen = "2013   1";
+      bot.tree().getTreeItem(twentyThirteen).expand();
+      final String may = "May   1";
+      final SWTBotTreeItem mayNode = bot.tree().getTreeItem(twentyThirteen).getNode(may);
+      mayNode.expand();
+      mayNode.select();
+      mayNode.getNode("18").select();
+
+      final SWTBotView tourTourImportView = bot.viewByTitle("Tour Import");
+      assertNotNull(tourTourImportView);
+      tourTourImportView.show();
+
+      final SWTBotView tourStatisticsView = bot.viewByTitle("Statistics");
+      assertNotNull(tourStatisticsView);
+      tourStatisticsView.show();
+
+      final SWTBotView tourCalendarView = bot.viewByTitle("Calendar");
+      assertNotNull(tourCalendarView);
+      tourCalendarView.show();
+
+      final SWTBotView tour2DMapView = bot.viewByTitle("2D Tour Map");
+      assertNotNull(tour2DMapView);
+      tour2DMapView.show();
+      //Sleeping 3 seconds as the 2D map can be slow to display
+      bot.sleep(3000);
+
+      final SWTBotView tourLogView = bot.viewByTitle("Tour Log");
+      assertNotNull(tourLogView);
+      tourLogView.show();
+
+      final SWTBotView tourWaypointsView = bot.viewByTitle("Waypoints");
+      assertNotNull(tourWaypointsView);
+      tourWaypointsView.show();
+
+      final SWTBotView tourEditorView = bot.viewByTitle("Tour Editor");
+      assertNotNull(tourEditorView);
+      tourEditorView.show();
+
+      bot.cTabItem("Time Slices").activate();
+      bot.cTabItem("Swim Slices").activate();
+   }
 
    @Test
    void testTourMarkerView() {
 
-      // This is the equivalent of
-      // bot.viewByTitle("Calendar").show();
-      // for SWTBot.
-      bot.menu("Tour Editor");
+      final SWTBotView tourBookView = bot.viewByTitle("Tour Book");
+      assertNotNull(tourBookView);
+      tourBookView.show();
 
       final String march2020 = "2020   3";
       bot.tree().getTreeItem(march2020).expand();
       final String maySecond = "May   2";
-      bot.tree().getTreeItem(march2020).getNode(maySecond).expand();
-      bot.tree().getTreeItem(march2020).getNode(maySecond).select();
-      bot.tree().getTreeItem(march2020).getNode(maySecond).getNode("23").select();
-      final SWTBotMenu tourMarkerView = bot.menu("Tour Markers");
+      final SWTBotTreeItem marchNode = bot.tree().getTreeItem(march2020).getNode(maySecond);
+      marchNode.expand();
+      marchNode.select();
+      marchNode.getNode("23").select();
+
+      final SWTBotView tourMarkerView = bot.viewByTitle("Tour Markers");
       assertNotNull(tourMarkerView);
+      tourMarkerView.show();
+
       bot.table().select(0);
       bot.table().select(1);
+   }
+
+   @Test
+   void testTourPausesView() {
+
+      final SWTBotView tourBookView = bot.viewByTitle("Tour Book");
+      assertNotNull(tourBookView);
+      tourBookView.show();
+
+      final String march2020 = "2021   2";
+      bot.tree().getTreeItem(march2020).expand();
+      final String january = "Jan   2";
+      final SWTBotTreeItem januaryNode = bot.tree().getTreeItem(march2020).getNode(january);
+      januaryNode.expand();
+      januaryNode.select();
+      januaryNode.getNode("31").select();
+
+      final SWTBotView tourPausesView = bot.viewByTitle("Tour Pauses");
+      assertNotNull(tourPausesView);
+      tourPausesView.show();
+
+      bot.table().select("15:40");
+   }
+
+   @Test
+   void testTourTagsView() {
+
+      final SWTBotView tourBookView = bot.viewByTitle("Tour Book");
+      assertNotNull(tourBookView);
+      tourBookView.show();
+
+      final String twentyTwentyOne = "2021   2";
+      bot.tree().getTreeItem(twentyTwentyOne).expand();
+      final String january = "Jan   2";
+      final SWTBotTreeItem januaryNode = bot.tree().getTreeItem(twentyTwentyOne).getNode(january);
+      januaryNode.expand();
+      januaryNode.select();
+      januaryNode.getNode("31").select();
+
+      final SWTBotView tourTagsView = bot.viewByTitle("Tour Tags");
+      assertNotNull(tourTagsView);
+      tourTagsView.show();
+      // bot.tree().getTreeItem("Shoes 2").select();
    }
 }
