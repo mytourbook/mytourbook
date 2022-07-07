da52d693ee7691a20a1bb76d23f51d68fb03cb7c
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-07-07 18:08:28 +0000

    WIP

--- bundles/net.tourbook.ui.tests/src/test/dialogs/DialogPrintTourTests.java
+++ bundles/net.tourbook.ui.tests/src/test/dialogs/DialogPrintTourTests.java
@@ -15,7 +15,7 @@
  *******************************************************************************/
 package dialogs;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
