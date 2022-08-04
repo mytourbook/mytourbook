28d1da0361f200ceeb7a5d11004b2e50619aea2b
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-08-04 14:43:11 +0000

    I think that's it!

--- bundles/net.tourbook.ui.tests/src/test/views/StatisticsViewTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/StatisticsViewTests.java
@@ -39,9 +39,6 @@ public class StatisticsViewTests {
    @ExtendWith(CaptureScreenshotOnFailureExtension.class)
    public void testStatisticsView() {
 
-      Utils.showViewFromMenu(_bot, "Help", "Error Log"); //$NON-NLS-1$
-      Utils.showView(_bot, "Error Log"); //$NON-NLS-1$
-
       final SWTBot tourEditorViewBot = Utils.showView(_bot, STATISTICS_VIEW_NAME).bot();
       _bot.sleep(3000);
 
