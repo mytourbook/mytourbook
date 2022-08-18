7e71862d849bfde02ff1f9833483d039531d91ba
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-08-18 19:19:04 +0000

    Revert

--- bundles/net.tourbook.device/src/net/tourbook/device/suunto/SuuntoQuestDeviceDataReader.java
+++ bundles/net.tourbook.device/src/net/tourbook/device/suunto/SuuntoQuestDeviceDataReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2021, 2021 Frédéric Bard and Contributors
+ * Copyright (C) 2021 Frédéric Bard and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
