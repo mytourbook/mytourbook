8d26a98cd94caa21b2386bb2f4c9a7e5133c0cd6
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-09-02 20:37:12 +0000

    Fix

--- bundles/net.tourbook.ui.tests/src/test/views/WorkbenchTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/WorkbenchTests.java
@@ -15,10 +15,7 @@
  *******************************************************************************/
 package views;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
@@ -49,9 +46,7 @@ public class WorkbenchTests extends UITest {
       //Select a tour so that the selected views contain information
       Utils.showTourBookView(bot);
 
-      final SWTBotTreeItem tour = bot.tree().getTreeItem("2013   1").expand() //$NON-NLS-1$
-            .getNode("May   1").expand().select().getNode("18").select(); //$NON-NLS-1$ //$NON-NLS-2$
-      assertNotNull(tour);
+      Utils.getTour(bot);
 
       Utils.showView(bot, "Tour Import"); //$NON-NLS-1$
 
