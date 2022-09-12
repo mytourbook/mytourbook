158d612aec206efbb9551fdae0a8d2a3b325b754
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-09-12 22:36:19 +0000

    Misc

--- bundles/net.tourbook.ui.tests/src/test/views/TourBookViewTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/TourBookViewTests.java
@@ -89,7 +89,7 @@ public class TourBookViewTests extends UITest {
 
       // Set a different date than today's date
       bot.dateTime(0).setDate(new Date(1420117200000L));
-      // Set a different time than today's date
+      // Set a different time than the current's time
       bot.dateTime(1).setDate(new Date(1420117200000L));
 
       //Save the tour
