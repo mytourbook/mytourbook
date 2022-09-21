b5d2435a1c54ea9ebe1bf137a52b050572dc5cc0
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-09-21 04:15:06 +0000

    Fix

--- bundles/net.tourbook.ui.tests/src/test/views/TourSegmenterViewTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/TourSegmenterViewTests.java
@@ -31,18 +31,22 @@ public class TourSegmenterViewTests extends UITest {
    @Test
    void testSegmenterAlgorithms_All() {
 
+      Utils.getTour(bot);
+
       Utils.showViewFromMenu(bot, Utils.TOOLS, "Tour Segmenter"); //$NON-NLS-1$
       final SWTBot tourSegmenterViewBot = Utils.showView(bot, "Tour Segmenter").bot(); //$NON-NLS-1$
 
       final SWTBotTable tableSegments = tourSegmenterViewBot.table();
 
+      tourSegmenterViewBot.comboBox(0).setSelection(Messages.tour_segmenter_type_byAltitude);
+      assertEquals("0:01", tableSegments.cell(0, 0));
+
       tourSegmenterViewBot.comboBox(0).setSelection(Messages.Tour_Segmenter_Type_ByAltitude_Merged);
-      assertEquals("0:17", tableSegments.cell(0, 0));
+      assertEquals("0:35", tableSegments.cell(0, 0));
 
       tourSegmenterViewBot.comboBox(0).setSelection(Messages.tour_segmenter_type_byDistance);
-      assertEquals("0:07", tableSegments.cell(0, 0));
+      assertEquals("0:47", tableSegments.cell(0, 0));
 
-      tourSegmenterViewBot.comboBox(0).setSelection(Messages.tour_segmenter_type_byAltitude);
 //      bot.comboBox().setSelection("By Break Time");
 //      bot.comboBox().setSelection("By Power (with DP)");
 //      bot.comboBox().setSelection("With Ascending/Descending");
