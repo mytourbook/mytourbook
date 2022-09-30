0d6eef0056b239732856a2616987d598831025c5
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-09-30 23:31:57 +0000

    Daum Ergo bike

---
+++ bundles/net.tourbook.tests/src/test/device/daum/ergobike/DaumErgoBikeDataReaderTests.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Frédéric Bard
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *******************************************************************************/
+package device.daum.ergobike;
+
+import net.tourbook.device.daum.ergobike.DaumErgoBikeDataReader;
+
+import org.junit.jupiter.api.Test;
+
+import utils.DeviceDataReaderTester;
+import utils.FilesUtils;
+
+/**
+ * Samples provided by WRPSoft
+ * todo fb
+ * https://github.com/mytourbook/mytourbook/discussions/924#discussioncomment-3585694
+ */
+public class DaumErgoBikeDataReaderTests extends DeviceDataReaderTester {
+
+   public static final String FILES_PATH       = FilesUtils.rootPath + "device/crp/files/"; //$NON-NLS-1$
+
+   private DaumErgoBikeDataReader deviceDataReader = new DaumErgoBikeDataReader();
+
+   @Test
+   void testDaumErgoBikeImport_20071109() {
+
+      testImportFile(deviceDataReader, FILES_PATH + "0009  09_11_2007 21_30_07   34min   14_7km  Manuelles Training (Watt)", ".csv");
+   }
+
+}
