8d703000350cd8c19e39658a8905af215e076c36
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-10-01 18:51:43 -0600

    revert

--- bundles/net.tourbook.device/src/net/tourbook/device/cm4xxm/CM4XXMDeviceData.java
+++ bundles/net.tourbook.device/src/net/tourbook/device/cm4xxm/CM4XXMDeviceData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2006, 2022 Wolfgang Schramm, Markus Stipp
+ * Copyright (C) 2006, 2020  Wolfgang Schramm, Markus Stipp
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
