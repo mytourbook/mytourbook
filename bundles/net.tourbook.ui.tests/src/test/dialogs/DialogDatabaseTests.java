2b8f2c312750cee88d0fb92777c750baa0a673f4
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-10-07 18:53:13 +0000

    test

--- bundles/net.tourbook.ui.tests/src/test/dialogs/DialogDatabaseTests.java
+++ bundles/net.tourbook.ui.tests/src/test/dialogs/DialogDatabaseTests.java
@@ -20,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import net.tourbook.Messages;
 import net.tourbook.ui.UI;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.jupiter.api.Test;
 
@@ -36,7 +37,7 @@ public class DialogDatabaseTests extends UITest {
       bot.button(Messages.App_Db_Compress_Button_CompressDatabase).click();
       bot.sleep(5000);
 
-      bot.button("Close").click(); //$NON-NLS-1$
+      bot.button(IDialogConstants.CLOSE_LABEL).click();
    }
 
    @Test
