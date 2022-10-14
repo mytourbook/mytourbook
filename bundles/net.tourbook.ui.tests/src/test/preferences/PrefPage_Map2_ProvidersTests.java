30c0d7cffd40fa098c813e47942f8212ec79c155
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-10-14 15:11:08 +0000

    basic test

---
+++ bundles/net.tourbook.ui.tests/src/test/preferences/PrefPage_Map2_ProvidersTests.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (C) 2022 Frédéric Bard
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ *******************************************************************************/
+package preferences;
+
+import org.junit.jupiter.api.Test;
+
+import utils.UITest;
+
+public class PrefPage_Map2_ProvidersTests extends UITest {
+
+   @Test
+   void openPreferencePage() {
+
+      bot.toolbarButtonWithTooltip("Preferences (Ctrl+Shift+P)").click(); //$NON-NLS-1$
+      bot.tree().getTreeItem("2D Map").expand().getNode("Map Provider").select(); //$NON-NLS-1$
+
+      bot.button("Apply and Close").click(); //$NON-NLS-1$
+   }
+}
