bfded33e7ca9da467c6c54dcb0849992e3f42a70
FJBDev <25235665+FJBDev@users.noreply.github.com>
2022-11-08 15:03:14 +0000

    prettier

--- bundles/net.tourbook.tests/src/test/data/weather/weatherapi/WeatherApiRetrieverTests.java
+++ bundles/net.tourbook.tests/src/test/data/weather/weatherapi/WeatherApiRetrieverTests.java
@@ -15,6 +15,7 @@
  *******************************************************************************/
 package data.weather.weatherapi;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -55,40 +56,47 @@ public class WeatherApiRetrieverTests {
       field.set(null, httpClientMock);
    }
 
-   @Test
-   void testWeatherRetrieval_July2022() {
+   private void configureRetriever(final TourData tour, final String vendorResponseFileName, final String expectedRequestParameters) {
 
       final String weatherApiResponse = Comparison.readFileContent(WEATHERAPI_FILE_PATH
-            + "LongsPeak-Manual-WeatherApiResponse-July2022.json"); //$NON-NLS-1$
-      final String url = WeatherUtils.OAUTH_PASSEUR_APP_URL
-            + "/weatherapi?lat=40.263996&lon=-105.58854099999999&lang=en&dt=2022-07-02"; //$NON-NLS-1$
-      httpClientMock.onGet(url)
-            .doReturn(weatherApiResponse);
+            + vendorResponseFileName);
+      final String url = WeatherUtils.OAUTH_PASSEUR_APP_URL + expectedRequestParameters;
+      httpClientMock.onGet(url).doReturn(weatherApiResponse);
+
+      weatherApiRetriever = new WeatherApiRetriever(tour);
+
+      assertTrue(weatherApiRetriever.retrieveHistoricalWeatherData());
+      httpClientMock.verify().get(url).called();
+   }
+
+   @Test
+   void testWeatherRetrieval_July2022() {
 
       final TourData tour = Initializer.importTour();
       //Tuesday, July 02, 2022 12:00:00 PM
       tour.setTourStartTime(2022, 7, 2, 12, 0, 0);
       //We set the current time elapsed to trigger the computation of the new end time
       tour.setTourDeviceTime_Elapsed(tour.getTourDeviceTime_Elapsed());
+      final String weatherApiResponse = "LongsPeak-Manual-WeatherApiResponse-July2022.json"; //$NON-NLS-1$
+      final String requestParameters = "/weatherapi?lat=40.263996&lon=-105.58854099999999&lang=en&dt=2022-07-02"; //$NON-NLS-1$
 
-      weatherApiRetriever = new WeatherApiRetriever(tour);
-
-      assertTrue(weatherApiRetriever.retrieveHistoricalWeatherData());
-      httpClientMock.verify().get(url).called();
+      configureRetriever(tour, weatherApiResponse, requestParameters);
 
 // SET_FORMATTING_OFF
 
-      assertEquals(16.6f,                         tour.getWeather_Temperature_Average());
-      assertEquals(3,                             tour.getWeather_Wind_Speed());
-      assertEquals(171,                           tour.getWeather_Wind_Direction());
-      assertEquals("Thundery outbreaks possible", tour.getWeather()); //$NON-NLS-1$
-      assertEquals("weather-drizzle",             tour.getWeather_Clouds()); //$NON-NLS-1$
-      assertEquals(51,                            tour.getWeather_Humidity());
-      assertEquals(6.13f,                         tour.getWeather_Precipitation());
-      assertEquals(1016.0,                        tour.getWeather_Pressure());
-      assertEquals(22.0f,                         tour.getWeather_Temperature_Max());
-      assertEquals(7.6f,                          tour.getWeather_Temperature_Min());
-      assertEquals(16.22f,                        tour.getWeather_Temperature_WindChill());
+      assertAll(
+            () ->  assertEquals(16.6f,                         tour.getWeather_Temperature_Average()),
+            () ->  assertEquals(3,                             tour.getWeather_Wind_Speed()),
+            () ->  assertEquals(171,                           tour.getWeather_Wind_Direction()),
+            () ->  assertEquals("Thundery outbreaks possible", tour.getWeather()),
+            () ->  assertEquals("weather-drizzle",             tour.getWeather_Clouds()),
+            () ->  assertEquals(51,                            tour.getWeather_Humidity()),
+            () ->  assertEquals(6.13f,                         tour.getWeather_Precipitation()),
+            () ->  assertEquals(0,                             tour.getWeather_Snowfall()),
+            () ->  assertEquals(1016.0,                        tour.getWeather_Pressure()),
+            () ->  assertEquals(22.0f,                         tour.getWeather_Temperature_Max()),
+            () ->  assertEquals(7.6f,                          tour.getWeather_Temperature_Min()),
+            () ->  assertEquals(16.22f,                        tour.getWeather_Temperature_WindChill()));
 
 // SET_FORMATTING_ON
    }
@@ -96,37 +104,32 @@ public class WeatherApiRetrieverTests {
    @Test
    void testWeatherRetrieval_May2022() {
 
-      final String weatherApiResponse = Comparison.readFileContent(WEATHERAPI_FILE_PATH
-            + "LongsPeak-Manual-WeatherApiResponse-May2022.json"); //$NON-NLS-1$
-      final String url = WeatherUtils.OAUTH_PASSEUR_APP_URL
-            + "/weatherapi?lat=40.263996&lon=-105.58854099999999&lang=en&dt=2022-05-10"; //$NON-NLS-1$
-      httpClientMock.onGet(url)
-            .doReturn(weatherApiResponse);
-
       final TourData tour = Initializer.importTour();
       //Tuesday, May 10, 2022 12:00:00 PM
       tour.setTourStartTime(2022, 5, 10, 12, 0, 0);
       //We set the current time elapsed to trigger the computation of the new end time
       tour.setTourDeviceTime_Elapsed(tour.getTourDeviceTime_Elapsed());
 
-      weatherApiRetriever = new WeatherApiRetriever(tour);
+      final String weatherApiResponse = "LongsPeak-Manual-WeatherApiResponse-May2022.json"; //$NON-NLS-1$
+      final String requestParameters = "/weatherapi?lat=40.263996&lon=-105.58854099999999&lang=en&dt=2022-05-10"; //$NON-NLS-1$
 
-      assertTrue(weatherApiRetriever.retrieveHistoricalWeatherData());
-      httpClientMock.verify().get(url).called();
+      configureRetriever(tour, weatherApiResponse, requestParameters);
 
 // SET_FORMATTING_OFF
 
-      assertEquals(13.92f,          tour.getWeather_Temperature_Average());
-      assertEquals(11,              tour.getWeather_Wind_Speed());
-      assertEquals(121,             tour.getWeather_Wind_Direction());
-      assertEquals("Ensoleille",    tour.getWeather()); //$NON-NLS-1$
-      assertEquals("weather-sunny", tour.getWeather_Clouds()); //$NON-NLS-1$
-      assertEquals(29,              tour.getWeather_Humidity());
-      assertEquals(0.0,             tour.getWeather_Precipitation());
-      assertEquals(1012.0,          tour.getWeather_Pressure());
-      assertEquals(22.1f,           tour.getWeather_Temperature_Max());
-      assertEquals(3.4f,            tour.getWeather_Temperature_Min());
-      assertEquals(13.32f,          tour.getWeather_Temperature_WindChill());
+      assertAll(
+            () ->  assertEquals(13.92f,          tour.getWeather_Temperature_Average()),
+            () ->  assertEquals(11,              tour.getWeather_Wind_Speed()),
+            () ->  assertEquals(121,             tour.getWeather_Wind_Direction()),
+            () ->  assertEquals("Ensoleille",    tour.getWeather()),
+            () ->  assertEquals("weather-sunny", tour.getWeather_Clouds()),
+            () ->  assertEquals(29,              tour.getWeather_Humidity()),
+            () ->  assertEquals(0,               tour.getWeather_Precipitation()),
+            () ->  assertEquals(0,               tour.getWeather_Snowfall()),
+            () ->  assertEquals(1012.0,          tour.getWeather_Pressure()),
+            () ->  assertEquals(22.1f,           tour.getWeather_Temperature_Max()),
+            () ->  assertEquals(3.4f,            tour.getWeather_Temperature_Min()),
+            () ->  assertEquals(13.32f,          tour.getWeather_Temperature_WindChill()));
 
 // SET_FORMATTING_ON
    }
