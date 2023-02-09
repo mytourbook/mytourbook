7d09e64f668d855412342ec98c33fbbb4c4b3b8a
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-02-09 15:49:54 +0000

    misc

--- bundles/net.tourbook/src/net/tourbook/ui/UI.java
+++ bundles/net.tourbook/src/net/tourbook/ui/UI.java
@@ -561,11 +561,11 @@ public class UI {
 
    private static Map<Long, String> fetchTourTagsAccumulatedValues() {
 
-      final String sql = "SELECT" + NEW_LINE //                                                                        //$NON-NLS-1$
+      final String sqlQuery = "SELECT" + NEW_LINE //                                                                        //$NON-NLS-1$
             + "jTdataTtag.TOURTAG_TAGID," + NEW_LINE // //$NON-NLS-1$
             + "SUM(tourData.TOURDISTANCE) AS TOTALDISTANCE," + NEW_LINE // //$NON-NLS-1$
             + "SUM(tourData.TOURDEVICETIME_RECORDED) AS TOTALRECORDEDTIME" + NEW_LINE //                                                                   //$NON-NLS-1$
-            + "FROM " +  TourDatabase.JOINTABLE__TOURDATA__TOURTAG + " jTdataTtag" + NEW_LINE //                                                                        //$NON-NLS-1$ //$NON-NLS-2$
+            + "FROM " + TourDatabase.JOINTABLE__TOURDATA__TOURTAG + " jTdataTtag" + NEW_LINE //                                                                        //$NON-NLS-1$ //$NON-NLS-2$
             + "INNER JOIN " + TourDatabase.TABLE_TOUR_DATA + NEW_LINE //                                                       //$NON-NLS-1$
             + "ON jTdataTtag.TOURDATA_TOURID = tourData.TOURID" + NEW_LINE //                                                                       //$NON-NLS-1$
             + "GROUP BY jTdataTtag.TOURTAG_TAGID"; //$NON-NLS-1$
@@ -573,8 +573,8 @@ public class UI {
       final Map<Long, String> tourTagsAccumulatedValues = new HashMap<>();
 
       try (Connection connection = TourDatabase.getInstance().getConnection();
-           final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-  
+            final PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+
          final ResultSet result = preparedStatement.executeQuery();
 
          while (result.next()) {
@@ -599,7 +599,7 @@ public class UI {
 
       } catch (final SQLException e) {
 
-         SQL.showException(e, sql);
+         SQL.showException(e, sqlQuery);
       }
       return tourTagsAccumulatedValues;
    }
