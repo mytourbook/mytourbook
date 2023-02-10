3d97159ca21fe41db66ae11e46614a7f2623674f
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-02-10 18:34:03 +0000

    revert

--- bundles/net.tourbook/src/net/tourbook/ui/UI.java
+++ bundles/net.tourbook/src/net/tourbook/ui/UI.java
@@ -627,7 +627,6 @@ public class UI {
    }
 
    public static ColumnPixelData getColumnPixelWidth(final PixelConverter pixelConverter, final int width) {
-
       return new ColumnPixelData(pixelConverter.convertWidthInCharsToPixels(width), false);
    }
 
