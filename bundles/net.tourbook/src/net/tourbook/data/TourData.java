50fd35e4a9945201e17bc5cf16b6241eb30e8d72
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-02-23 21:11:10 -0700

    Update TourData.java

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -14,7 +14,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  *******************************************************************************/
 package net.tourbook.data;
-
+//todo fb reduce the number of lines in the quick edit tour dialog
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.EAGER;
 
