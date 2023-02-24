bc3f35de4be2147513f13b0cd8cdca941a89e967
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-02-24 00:00:08 +0000

    year

--- bundles/net.tourbook.common/src/net/tourbook/common/util/FilesUtils.java
+++ bundles/net.tourbook.common/src/net/tourbook/common/util/FilesUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2021, 2022 Frédéric Bard
+ * Copyright (C) 2021, 2023 Frédéric Bard
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
