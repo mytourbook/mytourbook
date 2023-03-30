18256d2cbca8e4ade12128e0d95dda5f1a9c1f1c
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-03-30 19:47:47 +0000

    fix

--- bundles/net.tourbook.tests/src/test/cloud/suunto/SuuntoCloudDownloaderTests.java
+++ bundles/net.tourbook.tests/src/test/cloud/suunto/SuuntoCloudDownloaderTests.java
@@ -37,6 +37,7 @@ import net.tourbook.cloud.Messages;
 import net.tourbook.cloud.Preferences;
 import net.tourbook.cloud.oauth2.OAuth2Utils;
 import net.tourbook.cloud.suunto.SuuntoCloudDownloader;
+import net.tourbook.cloud.suunto.SuuntoTokensRetrievalHandler;
 import net.tourbook.tour.TourLogManager;
 
 import org.eclipse.jface.preference.IPreferenceStore;
