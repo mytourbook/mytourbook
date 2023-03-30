71fe39dfbb628f02d0ad95dc54f2c3ac0eec2706
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-03-30 20:41:05 +0000

    fix

--- bundles/net.tourbook.tests/src/test/cloud/strava/StravaUploaderTests.java
+++ bundles/net.tourbook.tests/src/test/cloud/strava/StravaUploaderTests.java
@@ -73,7 +73,7 @@ public class StravaUploaderTests {
             .doReturn(passeurResponse)
             .withStatus(201);
 
-      final Field field = StravaUploader.class.getDeclaredField("httpClient"); //$NON-NLS-1$
+      final Field field = OAuth2Utils.class.getDeclaredField("httpClient"); //$NON-NLS-1$
       field.setAccessible(true);
       field.set(null, httpClientMock);
 
