fcae846dde556311ed190eaa2d66e318c1f9b729
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-04-10 22:22:35 +0000

    revert and WIP

--- bundles/net.tourbook.export/src/net/tourbook/export/fit/FitExporter.java
+++ bundles/net.tourbook.export/src/net/tourbook/export/fit/FitExporter.java
@@ -18,24 +18,20 @@ package net.tourbook.export.fit;
 import com.garmin.fit.ActivityMesg;
 import com.garmin.fit.DateTime;
 import com.garmin.fit.DeveloperDataIdMesg;
-import com.garmin.fit.DeveloperField;
 import com.garmin.fit.DeviceIndex;
 import com.garmin.fit.DeviceInfoMesg;
 import com.garmin.fit.Event;
 import com.garmin.fit.EventMesg;
 import com.garmin.fit.EventType;
-import com.garmin.fit.FieldDescriptionMesg;
 import com.garmin.fit.File;
 import com.garmin.fit.FileEncoder;
 import com.garmin.fit.FileIdMesg;
 import com.garmin.fit.Fit;
-import com.garmin.fit.FitBaseType;
 import com.garmin.fit.FitRuntimeException;
 import com.garmin.fit.Gender;
 import com.garmin.fit.LapMesg;
 import com.garmin.fit.Manufacturer;
 import com.garmin.fit.Mesg;
-import com.garmin.fit.MesgNum;
 import com.garmin.fit.RecordMesg;
 import com.garmin.fit.SessionMesg;
 import com.garmin.fit.Sport;
@@ -46,8 +42,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import net.tourbook.application.TourbookPlugin;
+import net.tourbook.common.time.TimeTools;
 import net.tourbook.common.util.StatusUtil;
 import net.tourbook.data.TourData;
+import net.tourbook.data.TourPerson;
 
 //todo fb: Add unit test -> convert to csv and use it for comparison
 public class FitExporter {
@@ -63,7 +62,8 @@ public class FitExporter {
       final short productId = 0;
       final float softwareVersion = 1.0f;
 
-      final int serialNumber = 123; //todo fb
+      //todo fb
+      final int serialNumber = 123;
 
       // Every FIT file MUST contain a File ID message
       final FileIdMesg fileIdMesg = new FileIdMesg();
@@ -93,12 +93,7 @@ public class FitExporter {
          return;
       }
 
-      //todo fb get from the people data
-      final UserProfileMesg userProfileMesg = new UserProfileMesg();
-      userProfileMesg.setGender(Gender.FEMALE);
-      userProfileMesg.setWeight(63.1F);
-      userProfileMesg.setAge((short) 99);
-      userProfileMesg.setFriendlyName("TestUser");
+      final UserProfileMesg userProfileMesg = createUserProfile();
       fileEncoder.write(userProfileMesg);
 
       fileEncoder.write(fileIdMesg);
@@ -112,6 +107,23 @@ public class FitExporter {
       } catch (final FitRuntimeException e) {
          StatusUtil.log(e);
       }
+   }
+
+   private static UserProfileMesg createUserProfile() {
+
+      final TourPerson activePerson = TourbookPlugin.getActivePerson();
+      if (activePerson == null) {
+         return null;
+      }
+
+      final UserProfileMesg userProfileMesg = new UserProfileMesg();
+      userProfileMesg.setGender(activePerson.getGender() == 0 ? Gender.MALE : Gender.FEMALE);
+      userProfileMesg.setWeight(activePerson.getWeight());
+      final int age = TimeTools.now().getYear() - TimeTools.getZonedDateTime(activePerson.getBirthDay()).getYear();
+      userProfileMesg.setAge((short) age);
+      userProfileMesg.setFriendlyName(activePerson.getName());
+
+      return userProfileMesg;
    }
 
    private void convertTourDataToFit(final TourData tourData, final String exportFilePath) {
@@ -138,31 +150,12 @@ public class FitExporter {
             (byte) 0xE9, 0x79, 0x62, (byte) 0xDB
       };
 
-      for (final byte element : appId) {
-         developerIdMesg.setApplicationId(0,  (byte) 0x90);
+      for (int i = 0; i < appId.length; i++) {
+         developerIdMesg.setApplicationId(i, appId[i]);
       }
 
       developerIdMesg.setDeveloperDataIndex((short) 0);
       messages.add(developerIdMesg);
-
-      // Create the Developer Data Field Descriptions
-      final FieldDescriptionMesg doughnutsFieldDescMesg = new FieldDescriptionMesg();
-      doughnutsFieldDescMesg.setDeveloperDataIndex((short) 0);
-      doughnutsFieldDescMesg.setFieldDefinitionNumber((short) 0);
-      doughnutsFieldDescMesg.setFitBaseTypeId(FitBaseType.FLOAT32);
-      doughnutsFieldDescMesg.setUnits(0, "doughnuts");
-      doughnutsFieldDescMesg.setNativeMesgNum(MesgNum.SESSION);
-      messages.add(doughnutsFieldDescMesg);
-
-      final FieldDescriptionMesg hrFieldDescMesg = new FieldDescriptionMesg();
-      hrFieldDescMesg.setDeveloperDataIndex((short) 0);
-      hrFieldDescMesg.setFieldDefinitionNumber((short) 1);
-      hrFieldDescMesg.setFitBaseTypeId(FitBaseType.UINT8);
-      hrFieldDescMesg.setFieldName(0, "Heart Rate");
-      hrFieldDescMesg.setUnits(0, "bpm");
-      hrFieldDescMesg.setNativeFieldNum((short) RecordMesg.HeartRateFieldNum);
-      hrFieldDescMesg.setNativeMesgNum(MesgNum.RECORD);
-      messages.add(hrFieldDescMesg);
 
       // Every FIT ACTIVITY file MUST contain Record messages
       final int previousTimeSerieValue = 0;
@@ -179,16 +172,7 @@ public class FitExporter {
          final RecordMesg recordMesg = new RecordMesg();
          recordMesg.setTimestamp(timestamp);
 
-         // Fake Record Data of Various Signal Patterns
          exportDataSeries(tourData, index, recordMesg);
-
-         recordMesg.setPositionLat(SemicirclesConverter.degreesToSemicircles(tourData.latitudeSerie[index]));
-         recordMesg.setPositionLong(SemicirclesConverter.degreesToSemicircles(tourData.longitudeSerie[index]));
-
-         // Add a Developer Field to the Record Message
-         final DeveloperField hrDevField = new DeveloperField(hrFieldDescMesg, developerIdMesg);
-         recordMesg.addDeveloperField(hrDevField);
-         hrDevField.setValue((short) (Math.sin((.01 * index + 10)) + 1.0) * 127.0);
 
          // Write the Record message to the output stream
          messages.add(recordMesg);
@@ -229,19 +213,10 @@ public class FitExporter {
       sessionMesg.setNumLaps(tourData.getTourMarkers().size());
       messages.add(sessionMesg);
 
-      // Add a Developer Field to the Session message
-      final DeveloperField doughnutsEarnedDevField = new DeveloperField(doughnutsFieldDescMesg, developerIdMesg);
-      doughnutsEarnedDevField.setValue(sessionMesg.getTotalElapsedTime() / 1200.0f);
-      sessionMesg.addDeveloperField(doughnutsEarnedDevField);
-
       // Every FIT ACTIVITY file MUST contain EXACTLY one Activity message
       final ActivityMesg activityMesg = new ActivityMesg();
       // activityMesg.setTimestamp(timestamp);
       activityMesg.setNumSessions(1);
-      // final TimeZone timeZone = TimeZone.getTimeZone(tourData.getTimeZoneId());
-      //  final long timezoneOffset = (timeZone.getRawOffset() + timeZone.getDSTSavings()) / 1000;
-      //  activityMesg.setLocalTimestamp(timestamp.getTimestamp() + timezoneOffset);
-      //  activityMesg.setTotalTimerTime((float) (timestamp.getTimestamp() - startTime.getTimestamp()));
       messages.add(activityMesg);
 
       createFitFile(messages, exportFilePath, startTime);
@@ -253,6 +228,16 @@ public class FitExporter {
    }
 
    private void exportDataSeries(final TourData tourData, final int index, final RecordMesg recordMesg) {
+
+      final double[] latitudeSerie = tourData.latitudeSerie;
+      if (latitudeSerie != null) {
+         recordMesg.setPositionLat(SemicirclesConverter.degreesToSemicircles(latitudeSerie[index]));
+      }
+
+      final double[] longitudeSerie = tourData.longitudeSerie;
+      if (latitudeSerie != null) {
+         recordMesg.setPositionLong(SemicirclesConverter.degreesToSemicircles(longitudeSerie[index]));
+      }
 
       final float[] distanceSerie = tourData.distanceSerie;
       if (distanceSerie != null) {
@@ -284,5 +269,4 @@ public class FitExporter {
          recordMesg.setAltitude(altitudeSerie[index]);
       }
    }
-
 }
