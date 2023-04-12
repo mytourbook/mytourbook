16112d193fe17e4f05cfe0e739d4c7e20ae54084
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-04-12 18:56:57 +0000

    misc

--- bundles/net.tourbook.tests/src/test/cloud/suunto/SuuntoWorkoutUploaderTests.java
+++ bundles/net.tourbook.tests/src/test/cloud/suunto/SuuntoWorkoutUploaderTests.java
@@ -109,8 +109,10 @@ public class SuuntoWorkoutUploaderTests {
             OAuth2Utils.createOAuthPasseurUri("/suunto/workout/upload").toString()) //$NON-NLS-1$
             .doReturn(workoutsResponse)
             .withStatus(HttpURLConnection.HTTP_OK);
+      final String blobUrl =
+            "https://askoworkout001.blob.core.windows.net/fit6/642c5admtced4c09af1c49e6?sv=2019-02-02&se=2023-04-05T05%3A14%3A03Z&sr=b&sp=racwd&sig=lLSJzHaMa6EEN%2FYdFQJyCDEBzO0LuM%2BTyWVt4Bf%2BmoU%3D";
       httpClientMock.onPut(
-            "https://askoworkout001.blob.core.windows.net/fit6/642c5admtced4c09af1c49e6?sv=2019-02-02&se=2023-04-05T05%3A14%3A03Z&sr=b&sp=racwd&sig=lLSJzHaMa6EEN%2FYdFQJyCDEBzO0LuM%2BTyWVt4Bf%2BmoU%3D") //$NON-NLS-1$
+            blobUrl)
             .doReturn(UI.EMPTY_STRING)
             .withStatus(HttpURLConnection.HTTP_CREATED);
 
@@ -118,9 +120,7 @@ public class SuuntoWorkoutUploaderTests {
       suuntoWorkoutsUploader.uploadTours(Arrays.asList(tour));
 
       httpClientMock.verify().post(OAuth2Utils.createOAuthPasseurUri("/suunto/workout/upload").toString()).called(); //$NON-NLS-1$
-      httpClientMock.verify().put(
-            "https://askoworkout001.blob.core.windows.net/fit6/642c5admtced4c09af1c49e6?sv=2019-02-02&se=2023-04-05T05%3A14%3A03Z&sr=b&sp=racwd&sig=lLSJzHaMa6EEN%2FYdFQJyCDEBzO0LuM%2BTyWVt4Bf%2BmoU%3D" //$NON-NLS-1$
-                  .toString()).called();
+      httpClientMock.verify().put(blobUrl).called();
 
       final List<?> logs = TourLogManager.getLogs();
       //todo fb
