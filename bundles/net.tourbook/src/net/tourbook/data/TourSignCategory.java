1e422e755f17fd036955b6087f2006e5f15d0cae
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-05-05 13:48:10 +0000

    wip

--- bundles/net.tourbook/src/net/tourbook/data/TourSignCategory.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourSignCategory.java
@@ -1,26 +1,26 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2014  Wolfgang Schramm and Contributors
- * 
+ * Copyright (C) 2005, 2023 Wolfgang Schramm and Contributors
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
  *******************************************************************************/
 package net.tourbook.data;
 
+import net.tourbook.database.TourDatabase;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import net.tourbook.database.TourDatabase;
 
 @Entity
 public class TourSignCategory implements Comparable<Object> {
