54fd79d06d7baf330d8f47dc99a4b8985d6f893a
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2023-07-08 15:36:18 +0200

    [TourDatabase] Fixed "Invalid thread access"
    org.eclipse.swt.SWTException: Invalid thread access
            at org.eclipse.swt.SWT.error(SWT.java:4918)
            at org.eclipse.swt.SWT.error(SWT.java:4833)
            at org.eclipse.swt.SWT.error(SWT.java:4804)
            at org.eclipse.swt.widgets.Widget.error(Widget.java:450)
            at org.eclipse.swt.widgets.Shell.<init>(Shell.java:288)
            at org.eclipse.swt.widgets.Shell.<init>(Shell.java:381)
            at org.eclipse.jface.window.Window.createShell(Window.java:487)
            at org.eclipse.jface.window.Window.create(Window.java:430)
            at org.eclipse.jface.dialogs.Dialog.create(Dialog.java:1094)
            at org.eclipse.jface.window.Window.open(Window.java:788)
            at net.tourbook.database.TourDatabase.updateDb__1_Design(TourDatabase.java:5582)
            at net.tourbook.database.TourDatabase.sqlStartup_50_IsDesignVersionValid(TourDatabase.java:5336)
            at net.tourbook.database.TourDatabase.lambda$6(TourDatabase.java:5003)
            at net.tourbook.database.TourDatabase.sqlStartup_10_IsSqlServerUpAndRunning(TourDatabase.java:5035)
            at net.tourbook.database.TourDatabase.getConnection(TourDatabase.java:4724)
            at net.tourbook.search.FTSearchManager$2.run(FTSearchManager.java:1609)
            at org.eclipse.jface.operation.ModalContext$ModalContextThread.run(ModalContext.java:122)

--- bundles/net.tourbook/src/net/tourbook/database/TourDatabase.java
+++ bundles/net.tourbook/src/net/tourbook/database/TourDatabase.java
@@ -435,6 +435,7 @@ public class TourDatabase {
 
    private boolean                               _isDbInitialized;
    private boolean                               _isDbInDataUpdate;
+   private boolean                               _isInDesignUpdate;
    private boolean                               _isTableChecked;
    private boolean                               _isDataVersionChecked;
    private boolean                               _isDesignVersionChecked;
@@ -4963,7 +4964,7 @@ public class TourDatabase {
     */
    private boolean sqlStartup_10_IsSqlServerUpAndRunning() {
 
-      if (_isDbInitialized || _isDbInDataUpdate) {
+      if (_isDbInitialized || _isDbInDataUpdate || _isInDesignUpdate) {
          return true;
       }
 
@@ -5556,7 +5557,8 @@ public class TourDatabase {
     */
    private boolean updateDb__1_Design(int currentDbVersion, final SplashManager splashManager) {
 
-      if (!_isSilentDatabaseUpdate) {
+      if (_isSilentDatabaseUpdate == false) {
+
          /*
           * Confirm update
           */
@@ -5582,11 +5584,14 @@ public class TourDatabase {
                1).open()) != Window.OK) {
 
             // the user will not update -> close application
+
             PlatformUI.getWorkbench().close();
 
             return false;
          }
       }
+
+      _isInDesignUpdate = true;
 
       /*
        * Do an additional check because version 20 is restructuring the data series
@@ -5880,6 +5885,11 @@ public class TourDatabase {
          _isSQLDesignUpdateError = true;
 
          return false;
+
+      } finally {
+
+         _isInDesignUpdate = false;
+
       }
 
       return true;
