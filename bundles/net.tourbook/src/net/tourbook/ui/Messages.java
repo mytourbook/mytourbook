be07fcc7bb09169e4129b4117688b61a81134df2
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2023-08-08 10:48:00 +0200

    [Tour Tooltip] Keep 3 different tooltip widths, small, medium and large which makes it easier to switch between different sizes

--- bundles/net.tourbook/src/net/tourbook/ui/Messages.java
+++ bundles/net.tourbook/src/net/tourbook/ui/Messages.java
@@ -564,6 +564,7 @@ public class Messages extends NLS {
    public static String        Tour_Marker_Column_IsVisibleNoEdit_Tooltip;
 
    public static String        Tour_Tooltip_Action_EditFormatPreferences;
+   public static String        Tour_Tooltip_Combo_UIWidthSize_Tooltip;
    public static String        Tour_Tooltip_Format_DateWeekTime;
    public static String        Tour_Tooltip_Format_HistoryDateTime;
    public static String        Tour_Tooltip_Format_Pace;
