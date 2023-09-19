ec2da9b4b0c461e9193462c6f8f21076eb9f556d
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-09-19 23:48:13 +0000

    Not here anymore

--- bundles/net.tourbook/src/net/tourbook/tour/TourChartContextProvider.java
+++ bundles/net.tourbook/src/net/tourbook/tour/TourChartContextProvider.java
@@ -66,7 +66,6 @@ public class TourChartContextProvider implements IChartContextProvider, ITourPro
 
    /**
     * @param tourEditor
-    * @param tourChartView
     */
    TourChartContextProvider(final TourEditor tourEditor) {
 
