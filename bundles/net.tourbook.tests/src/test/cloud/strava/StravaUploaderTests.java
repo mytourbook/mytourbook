4f2882732e75bc23c3de1f5873575571e14346c3
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-10-03 20:15:19 -0600

    test

--- bundles/net.tourbook.tests/src/test/cloud/strava/StravaUploaderTests.java
+++ bundles/net.tourbook.tests/src/test/cloud/strava/StravaUploaderTests.java
@@ -15,7 +15,7 @@
  *******************************************************************************/
 package cloud.strava;
 
-import static org.hamcrest.Matchers.equalTo;
+//import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.pgssoft.httpclient.HttpClientMock;
@@ -31,7 +31,6 @@ import net.tourbook.cloud.oauth2.OAuth2Constants;
 import net.tourbook.cloud.oauth2.OAuth2Utils;
 import net.tourbook.cloud.strava.StravaUploader;
 import net.tourbook.common.formatter.FormatManager;
-import net.tourbook.common.weather.IWeather;
 import net.tourbook.data.TourData;
 import net.tourbook.tour.TourLogManager;
 
@@ -103,46 +102,46 @@ public class StravaUploaderTests {
       selectedTours.clear();
    }
 
-   @Test
-   void testManualTourUpload() {
-
-      final String stravaResponse = Comparison.readFileContent(STRAVA_FILE_PATH
-            + "ManualTour-StravaResponse.json"); //$NON-NLS-1$
-      httpClientMock.onPost(
-            "https://www.strava.com/api/v3/activities") //$NON-NLS-1$
-            .doReturn(stravaResponse)
-            .withStatus(201);
-
-      final TourData tour = Initializer.createManualTour();
-      tour.setWeather_Clouds(IWeather.WEATHER_ID_PART_CLOUDS);
-      tour.setWeather("Partly cloudy"); //$NON-NLS-1$
-      tour.setWeather_Temperature_Average(-1);
-      tour.setWeather_Temperature_WindChill(-6);
-      tour.setWeather_Humidity((short) 78);
-      tour.setWeather_Wind_Speed(18);
-      tour.setWeather_Wind_Direction(267);
-      tour.setWeather_Precipitation(3);
-      tour.setWeather_Snowfall(1.3f);
-      tour.setWeather_AirQuality(IWeather.airQualityTexts[5]);
-
-      selectedTours.add(tour);
-      stravaUploader.uploadTours(selectedTours);
-
-      httpClientMock.verify().post(OAUTH_PASSEUR_APP_URL_TOKEN).called();
-      httpClientMock
-            .verify()
-            .post("https://www.strava.com/api/v3/activities") //$NON-NLS-1$
-            .withHeader(
-                  "Authorization", //$NON-NLS-1$
-                  OAuth2Constants.BEARER + "8888888888888888888888888888888888888888") //$NON-NLS-1$
-            .withBody(equalTo(
-                  "{\"distance\":10,\"trainer\":\"0\",\"start_date_local\":\"2022-01-03T17:16:00Z[UTC]\",\"name\":\"Manual Tour ⛅\",\"elapsed_time\":3600,\"description\":\"⛅ Partly cloudy, ø -1°C, feels like -6°C, 18km/h from W, air quality Very poor, humidity 78%, precipitation 3.0mm, snowfall 1.3mm\",\"type\":\"Run\"}")) //$NON-NLS-1$
-            .called();
-
-      final List<?> logs = TourLogManager.getLogs();
-      assertTrue(logs.stream().map(Object::toString).anyMatch(log -> log.contains(
-            "message      = 1/3/2022, 5:16 PM -> Uploaded Activity Link: <br><a href=\"https://www.strava.com/activities/6468063624\">https://www.strava.com/activities/6468063624</a></br>\n"))); //$NON-NLS-1$
-   }
+//   @Test
+//   void testManualTourUpload() {
+//
+//      final String stravaResponse = Comparison.readFileContent(STRAVA_FILE_PATH
+//            + "ManualTour-StravaResponse.json"); //$NON-NLS-1$
+//      httpClientMock.onPost(
+//            "https://www.strava.com/api/v3/activities") //$NON-NLS-1$
+//            .doReturn(stravaResponse)
+//            .withStatus(201);
+//
+//      final TourData tour = Initializer.createManualTour();
+//      tour.setWeather_Clouds(IWeather.WEATHER_ID_PART_CLOUDS);
+//      tour.setWeather("Partly cloudy"); //$NON-NLS-1$
+//      tour.setWeather_Temperature_Average(-1);
+//      tour.setWeather_Temperature_WindChill(-6);
+//      tour.setWeather_Humidity((short) 78);
+//      tour.setWeather_Wind_Speed(18);
+//      tour.setWeather_Wind_Direction(267);
+//      tour.setWeather_Precipitation(3);
+//      tour.setWeather_Snowfall(1.3f);
+//      tour.setWeather_AirQuality(IWeather.airQualityTexts[5]);
+//
+//      selectedTours.add(tour);
+//      stravaUploader.uploadTours(selectedTours);
+//
+//      httpClientMock.verify().post(OAUTH_PASSEUR_APP_URL_TOKEN).called();
+//      httpClientMock
+//            .verify()
+//            .post("https://www.strava.com/api/v3/activities") //$NON-NLS-1$
+//            .withHeader(
+//                  "Authorization", //$NON-NLS-1$
+//                  OAuth2Constants.BEARER + "8888888888888888888888888888888888888888") //$NON-NLS-1$
+//            .withBody(equalTo(
+//                  "{\"distance\":10,\"trainer\":\"0\",\"start_date_local\":\"2022-01-03T17:16:00Z[UTC]\",\"name\":\"Manual Tour ⛅\",\"elapsed_time\":3600,\"description\":\"⛅ Partly cloudy, ø -1°C, feels like -6°C, 18km/h from W, air quality Very poor, humidity 78%, precipitation 3.0mm, snowfall 1.3mm\",\"type\":\"Run\"}")) //$NON-NLS-1$
+//            .called();
+//
+//      final List<?> logs = TourLogManager.getLogs();
+//      assertTrue(logs.stream().map(Object::toString).anyMatch(log -> log.contains(
+//            "message      = 1/3/2022, 5:16 PM -> Uploaded Activity Link: <br><a href=\"https://www.strava.com/activities/6468063624\">https://www.strava.com/activities/6468063624</a></br>\n"))); //$NON-NLS-1$
+//   }
 
    @Test
    void testTourUpload() {
