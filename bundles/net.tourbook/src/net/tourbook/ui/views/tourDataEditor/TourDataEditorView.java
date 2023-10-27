15a09d7e85f7209cb175c4ee486b48b53a91eb18
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2023-10-27 08:55:44 +0200

    [Tour Duplication] Fixed: Markers from duplicated tours have their date equal to the one from the original tour
    https://github.com/mytourbook/mytourbook/issues/1221

--- bundles/net.tourbook/src/net/tourbook/ui/views/tourDataEditor/TourDataEditorView.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/tourDataEditor/TourDataEditorView.java
@@ -8449,6 +8449,35 @@ public class TourDataEditorView extends ViewPart implements ISaveablePart, ISave
          _tourData.computeComputedValues();
 
          /*
+          * Update absolute tour marker time when this time is not set correctly. This can happen
+          * when the tour start time is modified, e.g. when a tour is copied and the tour start
+          * date/time is adjusted
+          */
+         final long absoluteTourStartTime = _tourData.getTourStartTimeMS();
+
+         for (final TourMarker tourMarker : _tourData.getTourMarkers()) {
+
+            final int relativeMarkerTime = tourMarker.getTime();
+
+            if (relativeMarkerTime == -1) {
+
+               // relative time is not available
+
+               continue;
+            }
+
+            final long absoluteFromRelativeMarkerTime = absoluteTourStartTime + relativeMarkerTime * 1000;
+            final long absoluteMarkerTime = tourMarker.getTourTime();
+
+            if (absoluteMarkerTime != absoluteFromRelativeMarkerTime) {
+
+               // adjust marker time
+
+               tourMarker.setTime(relativeMarkerTime, absoluteFromRelativeMarkerTime);
+            }
+         }
+
+         /*
           * saveTour() will check the tour editor dirty state, but when the tour is saved, the dirty
           * flag can be set before to prevent an out of sync error
           */
@@ -8519,7 +8548,7 @@ public class TourDataEditorView extends ViewPart implements ISaveablePart, ISave
    }
 
    /**
-    * saves the tour in the {@link TourDataEditorView}
+    * Saves the tour in the {@link TourDataEditorView}
     *
     * @return Returns <code>true</code> when the tour is saved or <code>false</code> when the tour
     *         could not saved because the user canceled saving
