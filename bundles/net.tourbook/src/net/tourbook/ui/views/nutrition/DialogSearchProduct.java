4a369959fc585913332358725c2105a31bcc7bf0
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-12-15 16:45:08 +0000

    me

--- bundles/net.tourbook/src/net/tourbook/ui/views/nutrition/DialogSearchProduct.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/nutrition/DialogSearchProduct.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2024 Wolfgang Schramm and Contributors
+ * Copyright (C) 2024 Frédéric Bard
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
