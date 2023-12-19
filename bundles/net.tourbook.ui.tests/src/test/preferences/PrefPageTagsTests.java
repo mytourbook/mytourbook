a725e588d5d3c2d6f7f3f47c7505be9ab60acce8
FJBDev <25235665+FJBDev@users.noreply.github.com>
2023-12-19 22:00:19 +0000

    fix

--- bundles/net.tourbook.ui.tests/src/test/preferences/PrefPageTagsTests.java
+++ bundles/net.tourbook.ui.tests/src/test/preferences/PrefPageTagsTests.java
@@ -34,8 +34,8 @@ public class PrefPageTagsTests extends UITest {
       Utils.openPreferences(bot);
       bot.tree().getTreeItem("Tagging").select(); //$NON-NLS-1$
 
-      //assert that there is 1 tag
-      assertEquals(1, bot.tree(1).rowCount());
+      //assert that there are 2 tags
+      assertEquals(2, bot.tree(1).rowCount());
 
       //Create a new tag
       bot.button(Messages.pref_tourtag_btn_new_tag).click();
@@ -45,15 +45,15 @@ public class PrefPageTagsTests extends UITest {
       final SWTBotTreeItem newTag = bot.tree(1).getTreeItem(newTagName).select();
       assertNotNull(newTag);
 
-      //assert that there are 2 tags
-      assertEquals(2, bot.tree(1).rowCount());
+      //assert that there are 3 tags
+      assertEquals(3, bot.tree(1).rowCount());
 
       //Delete the new tag
       newTag.contextMenu(Messages.Action_Tag_Delete).click();
       bot.button(Messages.Tag_Manager_Action_DeleteTag).click();
 
-      //assert that there is 1 tag
-      assertEquals(1, bot.tree(1).rowCount());
+      //assert that there are 2 tags
+      assertEquals(2, bot.tree(1).rowCount());
 
       Utils.clickApplyAndCloseButton(bot);
    }
