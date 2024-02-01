7939aa687a705a09100ce27047658ea7e5035302
FJBDev <25235665+FJBDev@users.noreply.github.com>
2024-02-01 00:03:41 +0000

    Fix

--- bundles/net.tourbook.ui.tests/src/test/views/TourNutritionViewTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/TourNutritionViewTests.java
@@ -87,8 +87,8 @@ public class TourNutritionViewTests extends UITest {
          dialogSearchProductShell.get().close();
       }
 
-      // Make sure that the table now contains 2 products
-      assertEquals(initialTableRowCount + 2, productsTable.rowCount());
+      // Make sure that the table now contains 3 products
+      assertEquals(initialTableRowCount + 3, productsTable.rowCount());
 
       // Act - Add a manual product
       bot.button(Messages.Tour_Nutrition_Button_AddCustomProduct).click();
@@ -98,15 +98,15 @@ public class TourNutritionViewTests extends UITest {
       Utils.clickOkButton(bot);
 
       // Assert
-      // Make sure that the table now contains 3 products
-      assertEquals(initialTableRowCount + 3, productsTable.rowCount());
+      // Make sure that the table now contains 4 products
+      assertEquals(initialTableRowCount + 4, productsTable.rowCount());
 
       // Sort the products by name
       productsTable.header(Messages.Tour_Nutrition_Column_Name).click();
       productsTable.header(Messages.Tour_Nutrition_Column_Name).click();
 
       // Act - Delete all the products
-      for (int index = 0; index < 3; ++index) {
+      for (int index = 0; index < 4; ++index) {
 
          productsTable.click(0, 2);
          bot.button(Messages.Tour_Nutrition_Button_DeleteProduct).click();
