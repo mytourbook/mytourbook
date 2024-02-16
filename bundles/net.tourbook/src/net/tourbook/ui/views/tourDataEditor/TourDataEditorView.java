b0c4665bd2da54163cbc523d8df851793ee8d5ab
FJBDev <25235665+FJBDev@users.noreply.github.com>
2024-02-16 23:54:05 +0000

    private

--- bundles/net.tourbook/src/net/tourbook/ui/views/tourDataEditor/TourDataEditorView.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/tourDataEditor/TourDataEditorView.java
@@ -333,7 +333,7 @@ public class TourDataEditorView extends ViewPart implements
     */
    private static final int              _hintTextColumnWidth                             = IS_OSX ? 200 : 150;
    //
-   DecimalFormat                         _temperatureFormat                               = new DecimalFormat("###.0");                       //$NON-NLS-1$
+   private DecimalFormat                 _temperatureFormat                               = new DecimalFormat("###.0");                       //$NON-NLS-1$
    //
    private ZonedDateTime                 _tourStartTime;
    //
