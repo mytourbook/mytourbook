272ba6573081cbb286cad89e4a4d19cbb7ba334d
FJBDev <25235665+FJBDev@users.noreply.github.com>
2024-03-03 17:17:58 -0700

    revert

--- bundles/net.tourbook.common/src/net/tourbook/common/Messages.java
+++ bundles/net.tourbook.common/src/net/tourbook/common/Messages.java
@@ -81,6 +81,7 @@ public class Messages extends NLS {
 
    public static String        Column_Annotation_Formatting;
    public static String        Column_Annotation_Sorting;
+
    public static String        Column_Profile_Dialog_ShowAllColumns_Message;
    public static String        Column_Profile_Dialog_ShowDefaultColumns_Message;
    public static String        Column_Profile_Dialog_Title;
