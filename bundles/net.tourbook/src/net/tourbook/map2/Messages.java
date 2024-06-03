c57fcb43df9dde74fc319a4b699e9981e8825358
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2024-06-03 14:15:51 +0200

    [2D Map] Removed unused text

--- bundles/net.tourbook/src/net/tourbook/map2/Messages.java
+++ bundles/net.tourbook/src/net/tourbook/map2/Messages.java
@@ -68,7 +68,6 @@ public class Messages extends NLS {
 
    public static String Map_Action_CopyLocation;
    public static String Map_Action_CreateTourMarkerFromMap;
-   public static String Map_Action_Edit2DMapPreferences;
    public static String Map_Action_Export_Map_Clipboard_Copied_Info;
    public static String Map_Action_Export_Map_View;
    public static String Map_Action_Export_Map_View_Clipboard;
