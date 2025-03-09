cca9990ed35fa0d5fe73ec8413acf4c9483fa64a
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2025-03-09 08:06:57 +0100

    [Tour Segments] Fixed: Douglas Peucker Tolerance Altitude recalculation
    https://github.com/mytourbook/mytourbook/issues/1465

--- bundles/net.tourbook/src/net/tourbook/ui/views/tourSegmenter/TourSegmenterView.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/tourSegmenter/TourSegmenterView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2005, 2024 Wolfgang Schramm and Contributors
+ * Copyright (C) 2005, 2025 Wolfgang Schramm and Contributors
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -6573,7 +6573,7 @@ public class TourSegmenterView extends ViewPart implements ITourViewer {
 
          if (selectedSegmenter.segmenterType == SegmenterType.ByElevationWithMarker) {
 
-            _elevationLoss = _tourData.segmentSerieTotal_Elevation_Loss;
+            _elevationLoss = -_tourData.segmentSerieTotal_Elevation_Loss;
             _elevationGain = _tourData.segmentSerieTotal_Elevation_Gain;
 
          } else {
