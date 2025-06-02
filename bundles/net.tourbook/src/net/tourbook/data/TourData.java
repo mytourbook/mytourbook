dc1f6f0de78982558125e9fe961505c12b65957e
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2025-06-02 14:33:27 +0200

    [Tour Editor] Fixed truncated integer value when deleting time slices

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -12407,8 +12407,18 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Serializa
 
       final long tourStartTimeMS = tourStartTime;
 
-      final long firstTimeMS = tourStartTimeMS + (timeSerie[firstSerieIndex] * 1000);
-      final long lastTimeMS = tourStartTimeMS + (timeSerie[lastSerieIndex] * 1000);
+      /*
+       * 1000L ist VERY important otherwise the value is truncated to an integer :-(((
+       */
+      final long firstTimeMS = tourStartTimeMS + (timeSerie[firstSerieIndex] * 1000L);
+      final long lastTimeMS = tourStartTimeMS + (timeSerie[lastSerieIndex] * 1000L);
+
+//      final String dtStart = TimeTools.Formatter_DateTime_SM.format(TimeTools.getZonedDateTime(tourStartTimeMS));
+//      final String dtFirst = TimeTools.Formatter_DateTime_SM.format(TimeTools.getZonedDateTime(firstTimeMS));
+//      final String dtLast = TimeTools.Formatter_DateTime_SM.format(TimeTools.getZonedDateTime(lastTimeMS));
+//
+//      System.out.println(dtStart + " ### " + firstSerieIndex + ":" + dtFirst + " - " + lastSerieIndex + ":" + dtLast);
+//// TODO remove SYSTEM.OUT.PRINTLN
 
       final Set<TourPhoto> allTourPhotos = tourPhotos;
       final ArrayList<TourPhoto> allSortedPhotos = new ArrayList<>(allTourPhotos);
@@ -12423,21 +12433,31 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Serializa
 
          final long photoTimeMS = tourPhoto.getAdjustedTime();
 
+//         final String dtPhoto = TimeTools.Formatter_DateTime_SM.format(TimeTools.getZonedDateTime(photoTimeMS));
+
          final boolean isInRange_Begin = photoTimeMS >= firstTimeMS;
          final boolean isInRange_End = photoTimeMS <= lastTimeMS;
 
          if (isInRange_Begin && isInRange_End) {
 
+//            System.out.println(" removed " + dtPhoto);
+//// TODO remove SYSTEM.OUT.PRINTLN
+
             allRemovedPhotos.add(tourPhoto);
 
          } else {
 
+//            System.out.println(" not removed " + dtPhoto);
+//// TODO remove SYSTEM.OUT.PRINTLN
          }
       }
 
-      final int numPhotos = allRemovedPhotos.size();
+      final int numRemovedPhotos = allRemovedPhotos.size();
 
-      if (numPhotos > 0) {
+      if (numRemovedPhotos > 0) {
+
+//         System.out.println(" removed " + numRemovedPhotos);
+//// TODO remove SYSTEM.OUT.PRINTLN
 
          tourPhotos.removeAll(allRemovedPhotos);
 
