248d91234eb239aed38834bc7d31731c23c25cfa
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2025-06-20 14:49:36 +0200

    [Eclipse 4.36] Use svg images to fix a scaling issue when using scaling of e.g. 110%

--- bundles/net.tourbook.photo/src/net/tourbook/photo/PhotoImages.java
+++ bundles/net.tourbook.photo/src/net/tourbook/photo/PhotoImages.java
@@ -25,9 +25,9 @@ public class PhotoImages {
 
    public static final String Photo_ShowAnnotation                = "photo-action-show-annotation.png";             //$NON-NLS-1$
    public static final String Photo_ShowAnnotation_Disabled       = "photo-action-show-annotation-disabled.png";    //$NON-NLS-1$
-   public static final String PhotoAnnotation_Cropped             = "photo-annotation-cropped.png";                 //$NON-NLS-1$
+   public static final String PhotoAnnotation_Cropped             = "photo-annotation-cropped.svg";                 //$NON-NLS-1$
    public static final String PhotoAnnotation_ExifGPS             = "photo-annotation-exif-gps.png";                //$NON-NLS-1$
-   public static final String PhotoAnnotation_Tonality            = "photo-annotation-tonality.png";                //$NON-NLS-1$
+   public static final String PhotoAnnotation_Tonality            = "photo-annotation-tonality.svg";                //$NON-NLS-1$
    public static final String PhotoAnnotation_TourGPS             = "photo-annotation-tour-gps.png";                //$NON-NLS-1$
    public static final String PhotoAnnotation_SavedInTour         = "photo-annotation-saved-in-tour.png";           //$NON-NLS-1$
    public static final String PhotoAnnotation_SavedInTour_Hovered = "photo-annotation-saved-in-tour-hovered.png";   //$NON-NLS-1$
@@ -54,7 +54,7 @@ public class PhotoImages {
    public static final String PhotoInfo_Name_Disabled             = "photo-info-name-disabled.png";                 //$NON-NLS-1$
    public static final String PhotoInvalid_PhotoImage             = "photo-invalid-photo-image.png";                //$NON-NLS-1$
    public static final String PhotoInvalid_PhotoImage_Hovered     = "photo-invalid-photo-image-hovered.png";        //$NON-NLS-1$
-   public static final String PhotoRatingStar                     = "photo-rating-star.png";                        //$NON-NLS-1$
+   public static final String PhotoRatingStar                     = "photo-rating-star.svg";                        //$NON-NLS-1$
    public static final String PhotoRatingStar_AndHovered          = "photo-rating-star-and-hovered.png";            //$NON-NLS-1$
    public static final String PhotoRatingStar_Delete              = "photo-rating-star-delete.png";                 //$NON-NLS-1$
    public static final String PhotoRatingStar_Disabled            = "photo-rating-star-disabled.png";               //$NON-NLS-1$
