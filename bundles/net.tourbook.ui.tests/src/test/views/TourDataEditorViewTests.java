7a1e9627801a1d813a4a85a0329be07cb66cb339
FJBDev <25235665+FJBDev@users.noreply.github.com>
2025-07-20 22:28:31 -0600

    Build updates and fixes (#1522)

--- bundles/net.tourbook.ui.tests/src/test/views/TourDataEditorViewTests.java
+++ bundles/net.tourbook.ui.tests/src/test/views/TourDataEditorViewTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2022, 2023 Frédéric Bard
+ * Copyright (C) 2022, 2025 Frédéric Bard
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
@@ -18,6 +18,7 @@ package views;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.Arrays;
 import java.util.GregorianCalendar;
 
 import net.tourbook.Messages;
@@ -26,6 +27,7 @@ import net.tourbook.common.UI;
 import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotCombo;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotDateTime;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotSpinner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
@@ -98,10 +100,13 @@ public class TourDataEditorViewTests extends UITest {
       timeSlicesTable.select(3);
 
       timeSlicesTable.contextMenu(Messages.Tour_Editor_Action_DeleteTimeSlices_KeepTime).click();
-      Utils.clickOkButton(bot);
+      final SWTBotShell[] currentShells = bot.shells();
+      if (Arrays.stream(currentShells).anyMatch(shell -> shell.getText().equals(Messages.tour_editor_dlg_delete_rows_title))) {
 
-      timeSlicesTable = tourEditorViewBot.table();
-      timeSlicesTable.contextMenu(Messages.Tour_Editor_Action_DeleteTimeSlices_KeepTime).click();
+         Utils.clickOkButton(bot);
+         timeSlicesTable = tourEditorViewBot.table();
+         timeSlicesTable.contextMenu(Messages.Tour_Editor_Action_DeleteTimeSlices_KeepTime).click();
+      }
 
       bot.toolbarButtonWithTooltip(Utils.SAVE_MODIFIED_TOUR).click();
 
