444d9ae3b505ca5e68e44872f9132c130402d091
FJBDev <25235665+FJBDev@users.noreply.github.com>
2025-10-07 21:46:41 -0600

    revetrt

--- bundles/net.tourbook.tests/src/test/utils/Comparison.java
+++ bundles/net.tourbook.tests/src/test/utils/Comparison.java
@@ -22,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
