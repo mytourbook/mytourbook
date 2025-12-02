f4fd72d5f65550a8c87c86ec624222f0d9bc1b9a
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2025-12-02 15:51:02 +0100

    [Tour Segmenter] Update tour moving/break time immediately when the segments are modified

--- bundles/net.tourbook/src/net/tourbook/data/TourData.java
+++ bundles/net.tourbook/src/net/tourbook/data/TourData.java
@@ -4231,6 +4231,12 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Serializa
       }
    }
 
+   /**
+    * @param startIndex
+    * @param endIndex
+    *
+    * @return Returns the break time in seconds between start and end index
+    */
    private int computeBreakTime(final int startIndex, int endIndex) {
 
       int totalBreakTime = 0;
@@ -13225,7 +13231,26 @@ public class TourData implements Comparable<Object>, IXmlSerializable, Serializa
    }
 
    public void setBreakTimeSerie(final boolean[] breakTimeSerie) {
+
       this.breakTimeSerie = breakTimeSerie;
+   }
+
+   /**
+    * @param breakTimeSerie
+    * @param tourBreakTime
+    *           Break time in seconds for the whole tour
+    */
+   public void setBreakTimeSerie(final boolean[] breakTimeSerie, final long tourBreakTime) {
+
+      this.breakTimeSerie = breakTimeSerie;
+
+      // update moving time
+      if (timeSerie != null && timeSerie.length > 0) {
+
+         final int tourMovingTime = (int) (timeSerie[timeSerie.length - 1] - tourBreakTime);
+
+         tourComputedTime_Moving = Math.max(0, tourMovingTime);
+      }
    }
 
    public void setCadenceMultiplier(final float cadenceMultiplier) {
