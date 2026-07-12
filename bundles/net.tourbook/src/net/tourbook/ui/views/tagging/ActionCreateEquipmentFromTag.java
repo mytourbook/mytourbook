eacaa7e904de6e80e1b3d88f5fc90f9ce0dccda2
wolfgang-ch <wolfgang-ch@users.sourceforge.net>
2026-07-12 14:51:38 +0200

    [Tag -> Equipment] Removed action to create equipment parts from tags

--- bundles/net.tourbook/src/net/tourbook/ui/views/tagging/ActionCreateEquipmentFromTag.java
+++ bundles/net.tourbook/src/net/tourbook/ui/views/tagging/ActionCreateEquipmentFromTag.java
@@ -18,115 +18,35 @@ package net.tourbook.ui.views.tagging;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import net.tourbook.common.time.TimeTools;
-import net.tourbook.common.ui.SubMenu;
 import net.tourbook.data.Equipment;
-import net.tourbook.data.EquipmentPart;
-import net.tourbook.data.TourData;
 import net.tourbook.data.TourTag;
 import net.tourbook.database.TourDatabase;
 import net.tourbook.equipment.EquipmentManager;
 import net.tourbook.equipment.EquipmentMenuManager;
 import net.tourbook.tour.TourEventId;
 import net.tourbook.tour.TourManager;
-import net.tourbook.ui.UI;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.custom.BusyIndicator;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Menu;
 
 /**
  * Submenu for exporting tours
  */
-public class ActionCreateEquipmentFromTag extends SubMenu {
+public class ActionCreateEquipmentFromTag extends Action {
 
-   private TaggingView               _taggingView;
-
-   private ActionCreateEquipment     _actionCreateEquipment;
-   private ActionCreateEquipmentPart _actionCreateEquipmentPart;
-
-   private class ActionCreateEquipment extends Action {
-
-      public ActionCreateEquipment() {
-
-         super("&Equipment");
-      }
-
-      @Override
-      public void run() {
-
-         onCreateEquipment();
-      }
-
-   }
-
-   private class ActionCreateEquipmentPart extends SubMenu {
-
-      public ActionCreateEquipmentPart() {
-
-         super("Equipment &Part for", AS_DROP_DOWN_MENU);
-      }
-
-      @Override
-      public void enableActions() {
-
-      }
-
-      @Override
-      public void fillMenu(final Menu menu) {
-
-         fillMenu_10_EquipmentActions(menu);
-      }
-   }
-
-   private class ActionEquipmentWithParts extends Action {
-
-      private final Equipment __equipment;
-
-      public ActionEquipmentWithParts(final Equipment equipment) {
-
-         super(equipment.getName(), AS_CHECK_BOX);
-
-         final Image eqImage = EquipmentManager.getEquipmentImage(equipment);
-
-         if (eqImage != null) {
-            setImageDescriptor(ImageDescriptor.createFromImage(eqImage));
-         }
-
-         __equipment = equipment;
-      }
-
-      @Override
-      public void run() {
-
-         onCreateEquipmentPart(__equipment);
-      }
-   }
+   private TaggingView _taggingView;
 
    /**
     * @param taggingView
-    * @param isAddMode
-    * @param isSaveTour
-    *           when <code>true</code> the tour will be saved and a {@link TourManager#TOUR_CHANGED}
-    *           event is fired, otherwise the {@link TourData} from the tour provider is only
-    *           updated
     */
    public ActionCreateEquipmentFromTag(final TaggingView taggingView) {
 
-      super(UI.IS_NOT_INITIALIZED, AS_DROP_DOWN_MENU);
+      super("&Create Equipment from Tag", AS_PUSH_BUTTON);
 
       _taggingView = taggingView;
-
-      setText("&Create Equipment from Tag");
-
-      createActions();
    }
 
    /**
@@ -176,55 +96,7 @@ public class ActionCreateEquipmentFromTag extends SubMenu {
 // SET_FORMATTING_ON
    }
 
-   private void createActions() {
-
-      _actionCreateEquipment = new ActionCreateEquipment();
-      _actionCreateEquipmentPart = new ActionCreateEquipmentPart();
-   }
-
-   @Override
-   public void enableActions() {}
-
-   @Override
-   public void fillMenu(final Menu menu) {
-
-      addActionToMenu(_actionCreateEquipment);
-      addActionToMenu(_actionCreateEquipmentPart);
-   }
-
-   private void fillMenu_10_EquipmentActions(final Menu menu) {
-
-      final List<Equipment> allAvailableEquipment = EquipmentManager.getAllEquipment_Name();
-
-      // Preload the equipment images
-      // Note that the hourglass is only displayed on Windows (it doesn't seem
-      // to work on Linux)
-      BusyIndicator.showWhile(Display.getCurrent(),
-            () -> allAvailableEquipment
-                  .forEach(equipment -> EquipmentManager.getEquipmentImage(equipment)));
-
-      for (final Equipment availableEquipment : allAvailableEquipment) {
-
-         if (availableEquipment.isRetired()) {
-
-            // skip retired equipment https://github.com/mytourbook/mytourbook/issues/1660
-            continue;
-         }
-
-         if (availableEquipment.canCollate()) {
-
-            // parts can be created only for not collated equipment
-
-            continue;
-         }
-
-         final ActionEquipmentWithParts equipmentAction = new ActionEquipmentWithParts(availableEquipment);
-
-         addActionToMenu(menu, equipmentAction);
-      }
-   }
-
-   private void onCreateEquipment() {
+   private void createEquipment() {
 
       final IStructuredSelection structuredSelection = _taggingView.getViewer().getStructuredSelection();
 
@@ -285,75 +157,10 @@ public class ActionCreateEquipmentFromTag extends SubMenu {
       }
    }
 
-   private void onCreateEquipmentPart(final Equipment equipment) {
+   @Override
+   public void run() {
 
-      final IStructuredSelection structuredSelection = _taggingView.getViewer().getStructuredSelection();
-
-      boolean isPartCreated = false;
-
-      for (final Object selection : structuredSelection) {
-
-         if (selection instanceof final TVITaggingView_Tag tagItem) {
-
-            final TourTag tourTag = tagItem.getTourTag();
-
-            final LocalDateTime now = LocalDateTime.now();
-            final String collateID = "From tag - %s".formatted(TimeTools.Formatter_DateTime_SM.format(now));
-
-            // convert expand type
-            final int partExpandType = convertExpandType(tourTag);
-
-            final EquipmentPart newPart = new EquipmentPart();
-
-// SET_FORMATTING_OFF
-
-            newPart.setEquipment       (equipment);
-
-            newPart.setBrand           (tourTag.getTagName());
-            newPart.setDescription     (tourTag.getNotes());
-            newPart.setImageFilePath   (tourTag.getImageFilePath());
-
-            newPart.setModel           (collateID);
-
-            newPart.setIsCollate       (true);
-            newPart.setPartCollateID   (collateID);
-            newPart.setCollateBetween  (EquipmentPart.COLLATED_WITH_NEXT);
-
-            newPart.setExpandType      ((short) partExpandType);
-
-// SET_FORMATTING_ON
-
-            // save new part
-            final EquipmentPart savedPart = TourDatabase.saveEntity(newPart, newPart.getPartId(), EquipmentPart.class);
-
-            // set equipment into all tours with the same tag
-            final long firstUseDate = EquipmentManager.setEquipmentFromTag(equipment, tourTag);
-
-            // set equipment first use date which is the oldest tour which contained the tag
-            savedPart.setDateUsed(firstUseDate);
-            savedPart.setDateBuilt(firstUseDate);
-
-            // resave part
-            TourDatabase.saveEntity(savedPart, savedPart.getPartId(), EquipmentPart.class);
-
-            /*
-             * Add part to the equipment to correctly update the from/until dates, but this
-             * equipment is not saved, it is reloaded when the UI is updated
-             */
-            equipment.getParts().add(savedPart);
-
-            final HashSet<String> allCollateIDs = new HashSet<>(Arrays.asList(collateID));
-
-            // update part from/until collate dates
-            EquipmentManager.updateUntilDate_Parts(equipment, allCollateIDs, savedPart.getCollateBetween());
-
-            isPartCreated = true;
-         }
-      }
-
-      if (isPartCreated) {
-         updateUI();
-      }
+      createEquipment();
    }
 
    /**
