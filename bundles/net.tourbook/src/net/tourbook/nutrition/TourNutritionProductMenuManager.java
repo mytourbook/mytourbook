0a1f7fd1e996b118f404248e0217c87fc63114be
FJBDev <25235665+FJBDev@users.noreply.github.com>
2026-08-19 22:45:10 -0600

    not sure why

--- bundles/net.tourbook/src/net/tourbook/nutrition/TourNutritionProductMenuManager.java
+++ bundles/net.tourbook/src/net/tourbook/nutrition/TourNutritionProductMenuManager.java
@@ -363,7 +363,6 @@ public class TourNutritionProductMenuManager {
 
                // update the tour nutrition product in the list
                recentTourNutritionProduct.setValue(tourNutritionProduct);
-
                break;
             }
          }
